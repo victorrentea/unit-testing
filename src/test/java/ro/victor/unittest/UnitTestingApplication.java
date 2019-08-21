329e85b7dde440c46e8597c63e7a12a4bf0ba887
Victor Rentea <victorrentea@gmail.com>
2019-08-21 10:07:42 +0100

    START MHR session

--- src/test/java/ro/victor/unittest/UnitTestingApplication.java
+++ src/test/java/ro/victor/unittest/UnitTestingApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UnitTestingApplication {
+	// nothing
 	
 	public static void main(String[] args) {
 		SpringApplication.run(UnitTestingApplication.class, args);
