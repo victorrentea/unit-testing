1aaed4f297a099419521c3d56d0567a57cd8ace4
Victor Rentea <victorrentea@gmail.com>
2020-05-05 11:16:45 +0300

    revert Tennis implem. Prep for TDD

--- src/test/java/ro/victor/unittest/tdd/TennisGameFeatureSteps.java
+++ src/test/java/ro/victor/unittest/tdd/TennisGameFeatureSteps.java
@@ -12,25 +12,4 @@ import static org.junit.Assert.assertEquals;
 public class TennisGameFeatureSteps {
     private TennisGame tennisGame;
 
-    @Given("^A new tennis game$")
-    public void a_new_tennis_game() throws Throwable {
-        tennisGame = new TennisGame();
-    }
-
-    @Then("^Score is \"([^\"]*)\"$")
-    public void score_is(String expected) throws Throwable {
-        assertEquals(expected, tennisGame.score());
-    }
-
-    @When("^Player(\\d+) scores$")
-    public void playerScores(int playerNo) {
-        tennisGame.addPoint(playerNo);
-    }
-
-    @And("^Player(\\d+) scores (\\d+) points$")
-    public void playerScoresPoints(int playerNo, int points) {
-        for (int i = 0; i < points; i++) {
-            tennisGame.addPoint(playerNo);
-        }
-    }
 }
