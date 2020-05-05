b47d49f6d1be8c5f6e5e131ca21ed796ba90d4d7
Victor Rentea <victorrentea@gmail.com>
2020-05-05 08:31:29 +0300

    Prep for PIT

--- src/test/java/ro/victor/unittest/builder/CustomerValidatorShould.java
+++ src/test/java/ro/victor/unittest/builder/CustomerValidatorShould.java
@@ -1,16 +1,13 @@
 package ro.victor.unittest.builder;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class CustomerValidatorShould {
 
 	private CustomerValidator validator = new CustomerValidator();
-	
-	@Test
-	public void beOK() {
-	}
 
+	@Test
+	public void yesSir() {
+	}
 
 }
