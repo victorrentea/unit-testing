7c36a8d926f408961832e9cb7a89135b9960eb18
Victor Rentea <victorrentea@gmail.com>
2020-05-25 13:34:33 +0300

    Add Spring Integration Testing examples + WEB

--- src/main/java/ro/victor/unittest/spring/web/ProductController.java
+++ src/main/java/ro/victor/unittest/spring/web/ProductController.java
@@ -1,10 +1,8 @@
 package ro.victor.unittest.spring.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import ro.victor.unittest.spring.facade.ProductFacade;
 import ro.victor.unittest.spring.facade.ProductSearchCriteria;
 import ro.victor.unittest.spring.facade.ProductSearchResult;
@@ -12,15 +10,29 @@ import ro.victor.unittest.spring.facade.ProductSearchResult;
 import java.util.List;
 
 @RestController
-@RequestMapping("product")
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductFacade facade;
 
-    @PostMapping("search")
+    @PostMapping("product/search")
     public List<ProductSearchResult> search(@RequestBody ProductSearchCriteria criteria) {
         return facade.searchProduct(criteria);
+    }
+
+    @GetMapping("unsecured")
+    public int unsecured() {
+        return 1; // dummy endpoint to test security
+    }
+
+    @GetMapping("secured")
+    public int secured() {
+        return 99; // dummy endpoint to test security
+    }
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("secured/method")
+    public int securedByAnnotation() {
+        return 99; // dummy endpoint to test security
     }
 
 }
--- src/test/java/ro/victor/unittest/spring/web/ProductControllerMvcTest.java
+++ src/test/java/ro/victor/unittest/spring/web/ProductControllerMvcTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -25,30 +26,49 @@ import java.util.Arrays;
 
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest // Start up only web part. Requires @MockBean for deeper layers
-//@SpringBootTest @AutoConfigureMockMvc
-public class ProductControllerTest {
+//@WebMvcTest(value=ProductController.class) // - Even more fine-grained
+//@SpringBootTest @AutoConfigureMockMvc // deeper e2e tests
+public class ProductControllerMvcTest {
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private ProductFacade facade;
 
+    // TODO read about Standalone setup:
+    // https://docs.spring.io/spring-framework/docs/current/spring-framework-reference/testing.html#spring-mvc-test-server-setup-options
 
+    @WithMockUser("spring")
     @Test
-    public void test() throws Exception {
-        when(facade.searchProduct(any())).thenReturn(Arrays.asList(new ProductSearchResult(1L,"dummyProduct1")));
+    public void testSearch() throws Exception {
+        when(facade.searchProduct(any())).thenReturn(Arrays.asList(new ProductSearchResult(1L, "dummyProduct1")));
         mockMvc.perform(post("/product/search")
-                    .content("{}")
-                    .contentType(MediaType.APPLICATION_JSON))
+                .content("{}")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
                 .andExpect(status().isOk())
                 .andExpect(header().string("Head-Shot", "true"))
 //                .andExpect(content().string(contains("dummyProduct1")))
                 .andExpect(jsonPath("$[0].name").value("dummyProduct1"));
+    }
+
+    @Test
+    public void testSearchUnauthorized() throws Exception {
+        when(facade.searchProduct(any())).thenReturn(Arrays.asList(new ProductSearchResult(1L, "dummyProduct1")));
+        mockMvc.perform(post("/product/search")).andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void testUnsecured() throws Exception {
+        mockMvc.perform(get("/unsecured"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("1"));
     }
 
 }
