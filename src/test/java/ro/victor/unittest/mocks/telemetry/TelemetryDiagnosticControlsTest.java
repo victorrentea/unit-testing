e2b4684b2a051f15928e4ee1a259367c1293232e
Victor Rentea <victorrentea@gmail.com>
2020-05-27 11:14:02 +0300

    MockitoRunner + validarea exceptiei

--- src/test/java/ro/victor/unittest/mocks/telemetry/TelemetryDiagnosticControlsTest.java
+++ src/test/java/ro/victor/unittest/mocks/telemetry/TelemetryDiagnosticControlsTest.java
@@ -1,20 +1,35 @@
 package ro.victor.unittest.mocks.telemetry;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 
+@RunWith(MockitoJUnitRunner.class)
 public class TelemetryDiagnosticControlsTest {
+    @Mock
+    private TelemetryClient client;
+    @InjectMocks
+    private TelemetryDiagnosticControls controls;
 
     @Test
-    public void test() {
+    public void disconnects() {
         // Intentia default ar trebui sa fie sa testtezi si clientul in joc cu Controls, eventual decupland sub client, cat mai aproape de exterior.
-        TelemetryClient client = new TelemetryClient();
-        TelemetryDiagnosticControls controls = new TelemetryDiagnosticControls(client);
+        when(client.getOnlineStatus()).thenReturn(true);
         controls.checkTransmission();
-
+        verify(client).disconnect();
+    }
+    @Test(expected = IllegalStateException.class)
+    public void throwsWhenNotOnline() {
+        when(client.getOnlineStatus()).thenReturn(false);
+        controls.checkTransmission();
     }
 
 }
