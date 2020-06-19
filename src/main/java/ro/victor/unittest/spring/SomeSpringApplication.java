771616c5f5c3ce556ee16fea1503c02a77f01ff9
Victor Rentea <victorrentea@gmail.com>
2020-06-19 09:56:31 +0300

    Init mock data at startup

--- src/main/java/ro/victor/unittest/spring/SomeSpringApplication.java
+++ src/main/java/ro/victor/unittest/spring/SomeSpringApplication.java
@@ -1,11 +1,16 @@
 package ro.victor.unittest.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import ro.victor.unittest.spring.domain.Product;
+import ro.victor.unittest.spring.repo.ProductRepo;
 import ro.victor.unittest.spring.web.CustomHeaderInterceptor;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -13,7 +18,7 @@ import java.time.Clock;
 
 @EnableSwagger2
 @SpringBootApplication
-public class SomeSpringApplication implements WebMvcConfigurer {
+public class SomeSpringApplication implements WebMvcConfigurer, CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(SomeSpringApplication.class);
     }
@@ -30,4 +35,12 @@ public class SomeSpringApplication implements WebMvcConfigurer {
         return Clock.systemDefaultZone(); // from: LocalDateTime.now();
     }
 
+
+    @Autowired
+    private ProductRepo productRepo;
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+        productRepo.save(new Product("Scaun la cap"));
+    }
 }
