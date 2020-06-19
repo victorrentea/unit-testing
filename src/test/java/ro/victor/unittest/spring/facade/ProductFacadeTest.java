ed8925e299e651ac5b1b7c9a42b6a4f358efb3a5
Victor Rentea <victorrentea@gmail.com>
2020-06-19 09:42:24 +0300

    undo solution

--- src/test/java/ro/victor/unittest/spring/facade/ProductFacadeTest.java
+++ src/test/java/ro/victor/unittest/spring/facade/ProductFacadeTest.java
@@ -25,51 +25,5 @@ import java.time.ZoneId;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-@RunWith(SpringRunner.class)
-@Transactional
-@SpringBootTest
 public class ProductFacadeTest {
-    @MockBean
-    public WhoServiceClient externalServiceClient;
-
-    @Autowired
-    private EntityManager em;
-    @Autowired
-    private ProductFacade productFacade;
-
-    @MockBean
-    private Clock clock;
-
-    private LocalDateTime currentTime = LocalDateTime.now();
-
-    @Before
-    public void setupTime() {
-        when(clock.instant()).thenAnswer(call -> currentTime.toInstant(ZoneId.systemDefault().getRules().getOffset(currentTime)));
-        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void testWar() {
-        when(externalServiceClient.covidVaccineExists()).thenReturn(true);
-        Product product = new Product();
-        em.persist(product);
-        productFacade.getProduct(product.getId());
-    }
-
-    @Test
-    public void testWholeWorkflow() {
-        when(externalServiceClient.covidVaccineExists()).thenReturn(false);
-        Product product = new Product().setName("Prod");
-        Supplier supplier = new Supplier().setActive(true);
-        product.setSupplier(supplier);
-        em.persist(product);
-        em.persist(supplier);
-        currentTime = LocalDateTime.parse("2020-01-01T20:00:00");
-
-        ProductDto dto = productFacade.getProduct(product.getId());
-
-        assertThat(dto.productName).isEqualTo("Prod");
-        System.out.println(dto.sampleDate);
-        assertThat(dto.sampleDate).isEqualTo("2020-01-01");
-    }
 }
