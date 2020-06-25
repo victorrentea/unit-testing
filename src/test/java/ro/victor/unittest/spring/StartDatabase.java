6efab58e308ef29557a91acd466046f8198c8889
Victor Rentea <victorrentea@gmail.com>
2020-06-26 00:49:07 +0300

    added MockMvc

--- src/test/java/ro/victor/unittest/spring/StartDatabase.java
+++ src/test/java/ro/victor/unittest/spring/StartDatabase.java
@@ -4,13 +4,11 @@ import java.sql.SQLException;
 
 public class StartDatabase {
 	public static void main(String[] args) throws SQLException {
-		System.out.println("Check out folder: ~/source/db/database/db");
 		System.out.println("Started DB...");
-		//hsqldb does not support Nested Transactions (REQUIRES_NEW).
-//		org.hsqldb.server.Server.main("--database.0 mem:test --dbname.0 test".split(" "));
+		System.out.println("Connecting to 'jdbc:h2:tcp://localhost:9092/~/test' will auto-create a database file 'test.mv.db' in user home (~)...");
 
-		// H2 does :)
-		org.h2.tools.Server.createTcpServer().start();
+		// Allow auto-creating new databases on disk at first connection
+		org.h2.tools.Server.createTcpServer("-ifNotExists").start();
 
 	}
 }
--- src/test/java/ro/victor/unittest/spring/web/ProductControllerMvcTest.java
+++ src/test/java/ro/victor/unittest/spring/web/ProductControllerMvcTest.java
@@ -3,13 +3,18 @@ package ro.victor.unittest.spring.web;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import ro.victor.unittest.spring.domain.Product;
 import ro.victor.unittest.spring.facade.ProductFacade;
 import ro.victor.unittest.spring.facade.ProductSearchResult;
+import ro.victor.unittest.spring.repo.ProductRepo;
 
 import java.util.Arrays;
 
@@ -30,33 +35,18 @@ public class ProductControllerMvcTest {
     @MockBean
     private ProductFacade facade;
 
-    // TODO read about Standalone setup:
-    // https://docs.spring.io/spring-framework/docs/current/spring-framework-reference/testing.html#spring-mvc-test-server-setup-options
-
     @Test
     public void testSearch() throws Exception {
-        when(facade.searchProduct(any())).thenReturn(Arrays.asList(new ProductSearchResult(1L, "dummyProduct1")));
+        when(facade.searchProduct(any())).thenReturn(Arrays.asList(new ProductSearchResult(1L, "Tree")));
         mockMvc.perform(post("/product/search")
                 .content("{}")
                 .contentType(MediaType.APPLICATION_JSON)
         )
                 .andExpect(status().isOk())
-                .andExpect(header().string("Head-Shot", "true"))
-//                .andExpect(content().string(contains("dummyProduct1")))
-                .andExpect(jsonPath("$[0].name").value("dummyProduct1"));
+                .andExpect(header().string("Custom-Header", "true"))
+//                .andExpect(content().string(contains("Tree")))
+                .andExpect(jsonPath("$[0].name").value("Tree"));
     }
 
-    @Test
-    public void testSearchUnauthorized() throws Exception {
-        when(facade.searchProduct(any())).thenReturn(Arrays.asList(new ProductSearchResult(1L, "dummyProduct1")));
-        mockMvc.perform(post("/product/search")).andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    public void testUnsecured() throws Exception {
-        mockMvc.perform(get("/unsecured"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("1"));
-    }
 
 }
