6c4e580feeaad2143ffd8ba77dc40dd74f589ea0
Victor Rentea <victorrentea@gmail.com>
2020-06-26 15:24:25 +0300

    DirtiesContext kills spring contexts between tests

--- src/test/java/ro/victor/unittest/spring/feed/FeedProcessorTest.java
+++ src/test/java/ro/victor/unittest/spring/feed/FeedProcessorTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.annotation.DirtiesContext.MethodMode;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,17 +16,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 public class FeedProcessorTest {
-
    @Autowired
    private FeedProcessor feedProcessor;
-
    @Autowired
    private FileRepoForTest repoForTest;
-   @Before
-   public void initialize() {
-      repoForTest.reset();
-   }
    @Test
    public void test1() throws IOException {
       //if one file containing 1 line
