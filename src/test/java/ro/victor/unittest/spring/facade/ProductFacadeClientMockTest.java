418291dbb06262916dab86b005e5812f2a890f68
Victor Rentea <victorrentea@gmail.com>
2020-07-08 22:46:14 +0300

    cleanu

--- src/test/java/ro/victor/unittest/spring/facade/ProductFacadeClientMockTest.java
+++ src/test/java/ro/victor/unittest/spring/facade/ProductFacadeClientMockTest.java
@@ -4,20 +4,19 @@ package ro.victor.unittest.spring.facade;
 //import org.junit.Test;
 //import org.junit.Test;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import ro.victor.unittest.spring.domain.Product;
 import ro.victor.unittest.spring.domain.Supplier;
 import ro.victor.unittest.spring.infra.SafetyServiceClient;
+import ro.victor.unittest.spring.repo.ProductRepo;
 import ro.victor.unittest.spring.web.ProductDto;
 
 import javax.persistence.EntityManager;
@@ -25,20 +24,22 @@ import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+// DONE
 
 @RunWith(SpringRunner.class)
 @Transactional
-@SpringBootTest(properties = "safety.service.url.base=http://localhost:8089")
-public class ProductFacade2Test {
+@SpringBootTest
+@ActiveProfiles("db-mem")
+public class ProductFacadeClientMockTest {
+    @MockBean
+    public SafetyServiceClient mockSafetyClient;
+
     @Autowired
-    private EntityManager em;
+    private ProductRepo productRepo;
     @Autowired
     private ProductFacade productFacade;
-
     @MockBean
     private Clock clock;
 
@@ -50,42 +51,33 @@ public class ProductFacade2Test {
         when(clock.getZone()).thenReturn(ZoneId.systemDefault());
     }
 
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule(8089);
-
     @Test(expected = IllegalStateException.class)
-    public void throwsWhenNotSafe() {
-        Product product = new Product().setSupplier(new Supplier().setActive(true));
-
-        em.persist(product);
-
-        WireMock.stubFor(get(urlEqualTo("/product/"+product.getId()+"/safety"))
-            .willReturn(aResponse()
-                .withStatus(200)
-                .withHeader("Content-Type", "application/json")
-                .withBody("[{\"category\":\"DETERMINED\", \"safeToSell\":false}]")));
+    public void throwsForUnsafeProduct() {
+        Product product = new Product().setExternalRef("EXTREF");
+        productRepo.save(product);
+        when(mockSafetyClient.isSafe("EXTREF")).thenReturn(false);
         productFacade.getProduct(product.getId());
     }
 
     @Test
-    public void success() {
-        Product product = new Product().setName("Prod");
+    public void fullOk() {
+        Product product = new Product().setExternalRef("EXTREF").setName("Prod");
         Supplier supplier = new Supplier().setActive(true);
         product.setSupplier(supplier);
-        em.persist(product);
-//        em.persist(supplier);
+        productRepo.save(product);
+        when(mockSafetyClient.isSafe("EXTREF")).thenReturn(true);
         currentTime = LocalDateTime.parse("2020-01-01T20:00:00");
-
-        WireMock.stubFor(get(urlEqualTo("/product/"+product.getId()+"/safety"))
-            .willReturn(aResponse()
-                .withStatus(200)
-                .withHeader("Content-Type", "application/json")
-                .withBody("[{\"category\":\"DETERMINED\", \"safeToSell\":true}]")));
 
         ProductDto dto = productFacade.getProduct(product.getId());
 
         assertThat(dto.productName).isEqualTo("Prod");
         System.out.println(dto.sampleDate);
-        assertThat(dto.sampleDate).isEqualTo("2020-01-01");
+        assertThat(dto.sampleDate).isEqualTo("2020-01-01T19:59:55");
     }
+
+    // TIP: sample response from Safety Service: "[{\"category\":\"DETERMINED\", \"safeToSell\":true}]"
+
+    // TIP2:
+    // when(clock.instant()).thenAnswer(call -> currentTime.toInstant(ZoneId.systemDefault().getRules().getOffset(currentTime)));
+    // when(clock.getZone()).thenReturn(ZoneId.systemDefault());
 }
