0b3e5f2ca7b357c538d8994aae903ddc310056b8
Victor Rentea <victorrentea@gmail.com>
2020-07-08 10:23:48 +0300

    compilation

--- src/main/java/ro/victor/unittest/spring/feed/FeedProcessor.java
+++ src/main/java/ro/victor/unittest/spring/feed/FeedProcessor.java
@@ -5,7 +5,8 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Set;
 
