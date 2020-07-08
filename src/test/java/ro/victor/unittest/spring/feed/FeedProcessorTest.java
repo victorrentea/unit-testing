2173a4dbadc58b94b322c3de2439c24ccc4e2e1c
Victor Rentea <victorrentea@gmail.com>
2020-07-08 11:07:34 +0300

    DirtiesContext iti da la gunoi viteza testelor. recreeaza

--- src/test/java/ro/victor/unittest/spring/feed/FeedProcessorTest.java
+++ src/test/java/ro/victor/unittest/spring/feed/FeedProcessorTest.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.annotation.DirtiesContext.MethodMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest // cauta in sus primul pachet care sa contina o @SpringBootApplication
 @RunWith(SpringRunner.class)
+//@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class FeedProcessorTest {
    @Autowired
    private FeedProcessor processor;
@@ -22,22 +26,25 @@ public class FeedProcessorTest {
    @Autowired
    private FileRepoDummy dummyRepo;
 
-   @After
-   public void mamaMiaZisSaCuratDupaMine() {
-      dummyRepo.cleanFiles();
-   }
+//   @After
+//   public void mamaMiaZisSaCuratDupaMine() {
+//      dummyRepo.cleanFiles();
+//   }
+   @DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
    @Test
    public void oneXLine() throws IOException {
       dummyRepo.addTestFile("one.txt", "one line");
       assertThat(processor.countPendingLines()).isEqualTo(1);
       // 2: ce tre sa faci musai aici<<<<<<<<<<
    }
+   @DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
    @Test
    public void testYTwo() throws IOException {
       dummyRepo.addTestFile("one.txt", "one line");
       dummyRepo.addTestFile("two.txt", "one line");
       assertThat(processor.countPendingLines()).isEqualTo(2);
    }
+   @DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
    @Test
    public void testZTwoFilesWithThreeLines() throws IOException {
       dummyRepo.addTestFile("one.txt", "one line");
