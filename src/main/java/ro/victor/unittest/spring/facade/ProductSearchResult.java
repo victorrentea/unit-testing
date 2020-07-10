42abb341597fc52451a93a0bf06b769cd437090c
Victor Rentea <victorrentea@gmail.com>
2020-07-10 13:11:17 +0300

    fixes

--- src/main/java/ro/victor/unittest/spring/facade/ProductSearchResult.java
+++ src/main/java/ro/victor/unittest/spring/facade/ProductSearchResult.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductSearchResult {
+    // Tip: final fields if using JPA
     public Long id;
     public String name;
 
