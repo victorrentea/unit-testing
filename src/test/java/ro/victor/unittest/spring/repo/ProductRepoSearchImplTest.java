77cc945df15886215cc80c79042ab09d2f33f95b
Victor Rentea <victorrentea@gmail.com>
2020-07-10 12:55:00 +0300

    mismatch case - gasit cu mutation testing

--- src/test/java/ro/victor/unittest/spring/repo/ProductRepoSearchImplTest.java
+++ src/test/java/ro/victor/unittest/spring/repo/ProductRepoSearchImplTest.java
@@ -6,9 +6,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.transaction.TestTransaction;
 import org.springframework.transaction.annotation.Transactional;
 import ro.victor.unittest.spring.domain.Product;
 import ro.victor.unittest.spring.domain.Supplier;
@@ -28,32 +30,43 @@ public class ProductRepoSearchImplTest {
 
    @Autowired
    private ProductRepo repo;
+   private final ProductSearchCriteria criteria = new ProductSearchCriteria();
 
    @Before // foarte utila in app mari, cand nu stii cine lasa gunoi in DB
    public void checkDbIsEmpty() {
       assertThat(repo.findAll()).isEmpty();
    }
 
+
    @Test
    public void emptyCriteria() {
-      ProductSearchCriteria criteria = new ProductSearchCriteria();
       repo.save(new Product().setSupplier(new Supplier()));
       List<ProductSearchResult> results = repo.search(criteria);
 //      assertEquals(1, results.size()); // are mesaj de failure enervant
       assertThat(results).hasSize(1);
-      repo.cevaUrat();
+//      repo.cevaUrat();
 
    }
    @Test
 //   @Commit // pt debug - iti lasa testu sa comita
    public void byName() {
-      ProductSearchCriteria criteria = new ProductSearchCriteria();
-      criteria.name = "a";
-      repo.save(new Product().setName("A"));
-      List<ProductSearchResult> results = repo.search(criteria);
-      assertThat(results).hasSize(1);
+      repo.save(new Product().setName("xAy"));
 
-      repo.cevaUrat();
+      criteria.name = "xAy";
+      assertThat(repo.search(criteria)).hasSize(1);
+
+      criteria.name = "a";
+      assertThat(repo.search(criteria)).hasSize(1);
+
+      criteria.name = "notPresentInDB";
+      assertThat(repo.search(criteria)).isEmpty();
+
+//      repo.cevaUrat();
+
+      // TransactionTemplate --<
+//      repo.deleteAll();
+       //daca vreun test acoper cod de prod care face COMMITURI, e politicos ca EL sa curete gunoiul in After
    }
+
 
 }
