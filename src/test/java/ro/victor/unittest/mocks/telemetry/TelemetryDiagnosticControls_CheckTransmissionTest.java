29da3df627d5248c85dbe1101d8459487bfe9cee
Victor Rentea <victorrentea@gmail.com>
2020-07-14 12:13:48 +0300

    when.thenReturn cu impact in starea obiectului testat

--- src/test/java/ro/victor/unittest/mocks/telemetry/TelemetryDiagnosticControls_CheckTransmissionTest.java
+++ src/test/java/ro/victor/unittest/mocks/telemetry/TelemetryDiagnosticControls_CheckTransmissionTest.java
@@ -23,9 +23,7 @@ public class TelemetryDiagnosticControls_CheckTransmissionTest {
     @Test
     public void disconnectsAndSends() {
         when(clientMock.getOnlineStatus()).thenReturn(true);
-
         controls.checkTransmission();
-
         verify(clientMock).disconnect();
 
         verify(clientMock).send(anyString()); // orice string e bun -> error prome
@@ -38,12 +36,22 @@ public class TelemetryDiagnosticControls_CheckTransmissionTest {
 
     @Test(expected = IllegalStateException.class)
     public void throwsWhenOffline() {
-
         when(clientMock.getOnlineStatus()).thenReturn(false);
-
         controls.checkTransmission();
     }
 
+    @Test
+    public void receives() {
+        when(clientMock.getOnlineStatus()).thenReturn(true);
+        when(clientMock.receive()).thenReturn("RECEIVE_VALUE");
+        controls.checkTransmission();
+
+//        verify(clientMock).receive();
+        // daca ai programat o metoda cu when.thenReturn atunci nu prea are sens sa ii faci verify
+        // ci codul testat ar trebui sa ia cumva deczii/ sa expuna datele citite din metoda mockuite
+
+        assertEquals("RECEIVE_VALUE", controls.getDiagnosticInfo());
+    }
 
 
 }
