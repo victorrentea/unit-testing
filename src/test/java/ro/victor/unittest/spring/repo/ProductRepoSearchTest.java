eccf89b6f114a3f0d79757625cd6d6e4a70c3ef0
Victor Rentea <victorrentea@gmail.com>
2020-07-15 14:37:27 +0300

    golirea tabelelor in cazuri reale e o provocare (dupa FK)...

--- src/test/java/ro/victor/unittest/spring/repo/ProductRepoSearchTest.java
+++ src/test/java/ro/victor/unittest/spring/repo/ProductRepoSearchTest.java
@@ -1,6 +1,7 @@
 package ro.victor.unittest.spring.repo;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import ro.victor.unittest.spring.domain.Product;
+import ro.victor.unittest.spring.domain.Supplier;
 import ro.victor.unittest.spring.facade.ProductSearchCriteria;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,12 +20,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ProductRepoSearchTest {
    @Autowired
    private ProductRepo repo;
+   @Autowired
+   private SupplierRepo srepo;
 
    private ProductSearchCriteria criteria = new ProductSearchCriteria();
 
+   @Before
+   public void initialize() {
+      srepo.deleteAll();
+       repo.deleteAll();
+   }
+
    @Test
    public void primuTest_cuOLumanarePeMasa() {
-      repo.save(new Product());
+      repo.save(new Product().setSupplier(new Supplier()));
+      assertThat(repo.search(criteria)).hasSize(1);
+   }
+   @Test
+   public void primuTest_cuOLumanarePeMasa2() {
+      repo.save(new Product().setSupplier(new Supplier()));
       assertThat(repo.search(criteria)).hasSize(1);
    }
 }
