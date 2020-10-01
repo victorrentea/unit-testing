eed92e9e2ec9fbe3e4d2251b131f7e80064b33b2
Victor Rentea <victorrentea@gmail.com>
2020-10-01 15:41:10 +0300

    Mockito equivalent

--- src/test/java/ro/victor/unittest/mocks/telemetry/TelemetryDiagnosticControlsTest.java
+++ src/test/java/ro/victor/unittest/mocks/telemetry/TelemetryDiagnosticControlsTest.java
@@ -1,6 +1,7 @@
 package ro.victor.unittest.mocks.telemetry;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -9,20 +10,25 @@ import static org.mockito.Mockito.verify;
 
 public class TelemetryDiagnosticControlsTest {
 
-   @Test
+   @Test(expected = IllegalStateException.class)
    public void test() {
-      TelemetryDiagnosticControls controls =
-          new TelemetryDiagnosticControls(new TelemetryClient(){
-             @Override
-             public void disconnect() {
-                //nada
-             }
+//      TelemetryClient client = new TelemetryClient() {
+//         @Override
+//         public void disconnect() {
+//            //nada
+//         }
+//
+//         @Override
+//         public boolean getOnlineStatus() {
+//            return false;
+//         }
+//      };
+      TelemetryClient client = Mockito.mock(TelemetryClient.class);
 
-             @Override
-             public boolean getOnlineStatus() {
-                return false;
-             }
-          });
+      Mockito.when(client.getOnlineStatus()).thenReturn(false);
+
+      TelemetryDiagnosticControls controls =
+          new TelemetryDiagnosticControls(client);
       controls.checkTransmission();
    }
 }
