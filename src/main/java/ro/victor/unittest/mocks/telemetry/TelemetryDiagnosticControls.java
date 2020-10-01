fa9f029ee2ca41f384b3443ac462a5e4f5f5761b
Victor Rentea <victorrentea@gmail.com>
2020-10-01 16:10:54 +0300

    cod cu mockuri compact frumos

--- src/main/java/ro/victor/unittest/mocks/telemetry/TelemetryDiagnosticControls.java
+++ src/main/java/ro/victor/unittest/mocks/telemetry/TelemetryDiagnosticControls.java
@@ -7,43 +7,46 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class TelemetryDiagnosticControls {
-	public static final String DIAGNOSTIC_CHANNEL_CONNECTION_STRING = "*111#";
+   public static final String DIAGNOSTIC_CHANNEL_CONNECTION_STRING = "*111#";
 
-	private final TelemetryClient telemetryClient;
-	private String diagnosticInfo = "";
+   private final TelemetryClient telemetryClient;
+   private String diagnosticInfo = "";
 
-	public TelemetryDiagnosticControls(TelemetryClient telemetryClient) {
-		this.telemetryClient = telemetryClient;
-	}
+   public TelemetryDiagnosticControls(TelemetryClient telemetryClient) {
+      this.telemetryClient = telemetryClient;
+   }
 
-	public String getDiagnosticInfo() {
-		return diagnosticInfo;
-	}
-	public void setDiagnosticInfo(String diagnosticInfo) {
-		this.diagnosticInfo = diagnosticInfo;
-	}
+   public String getDiagnosticInfo() {
+      return diagnosticInfo;
+   }
 
-	public void checkTransmission() {
-		 telemetryClient.disconnect();
+   public void setDiagnosticInfo(String diagnosticInfo) {
+      this.diagnosticInfo = diagnosticInfo;
+   }
 
-		int currentRetry = 1;
-		while (! telemetryClient.getOnlineStatus() && currentRetry <= 3) {
-			telemetryClient.connect(DIAGNOSTIC_CHANNEL_CONNECTION_STRING);
-			currentRetry ++;
-		}
+   public void checkTransmission() {
+      // TODO extract reconnect()
 
-		if (! telemetryClient.getOnlineStatus()) {
-			throw new IllegalStateException("Unable to connect.");
-		}
+      telemetryClient.disconnect();
 
-		ClientConfiguration config = new ClientConfiguration();
-		config.setSessionId(telemetryClient.getVersion() + "-" + UUID.randomUUID().toString());
-		config.setSessionStart(LocalDateTime.now());
-		config.setAckMode(AckMode.NORMAL);
-		telemetryClient.configure(config);
+      int currentRetry = 1;
+      while (!telemetryClient.getOnlineStatus() && currentRetry <= 3) {
+         telemetryClient.connect(DIAGNOSTIC_CHANNEL_CONNECTION_STRING);
+         currentRetry++;
+      }
 
-		telemetryClient.send(TelemetryClient.DIAGNOSTIC_MESSAGE);
-		diagnosticInfo = telemetryClient.receive();
-	}
+      if (!telemetryClient.getOnlineStatus()) {
+         throw new IllegalStateException("Unable to connect.");
+      }
+
+      ClientConfiguration config = new ClientConfiguration();
+      config.setSessionId(telemetryClient.getVersion() + "-" + UUID.randomUUID().toString());
+      config.setSessionStart(LocalDateTime.now());
+      config.setAckMode(AckMode.NORMAL);
+      telemetryClient.configure(config);
+
+      telemetryClient.send(TelemetryClient.DIAGNOSTIC_MESSAGE);
+      diagnosticInfo = telemetryClient.receive();
+   }
 
 }
--- src/test/java/ro/victor/unittest/mocks/telemetry/TelemetryDiagnosticControlsTest.java
+++ src/test/java/ro/victor/unittest/mocks/telemetry/TelemetryDiagnosticControlsTest.java
@@ -1,36 +1,44 @@
 package ro.victor.unittest.mocks.telemetry;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 
+@RunWith(MockitoJUnitRunner.class)
 public class TelemetryDiagnosticControlsTest {
+   @Mock
+   private TelemetryClient client;
+   @InjectMocks
+   private TelemetryDiagnosticControls controls;
+
+   @Before
+   public void initialize() {
+      when(client.getOnlineStatus()).thenReturn(true);
+   }
 
    @Test(expected = IllegalStateException.class)
    public void test() {
-      TelemetryClient client = Mockito.mock(TelemetryClient.class);
-
-      Mockito.when(client.getOnlineStatus()).thenReturn(false);
-
-      TelemetryDiagnosticControls controls =
-          new TelemetryDiagnosticControls(client);
+      when(client.getOnlineStatus()).thenReturn(false);
       controls.checkTransmission();
    }
 
    @Test
    public void disconnects() {
-      TelemetryClient client = Mockito.mock(TelemetryClient.class);
-
-      Mockito.when(client.getOnlineStatus()).thenReturn(true);
-
-      TelemetryDiagnosticControls controls = new TelemetryDiagnosticControls(client);
-
       controls.checkTransmission();
-
-      Mockito.verify(client).disconnect();
+      verify(client).disconnect();
+   }
+   @Test
+   public void sends() {
+      controls.checkTransmission();
+      verify(client).send(TelemetryClient.DIAGNOSTIC_MESSAGE);
    }
 }
