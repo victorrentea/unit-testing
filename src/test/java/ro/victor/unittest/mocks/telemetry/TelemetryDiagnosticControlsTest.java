fd2eb1fd473d9c152f3747f63ea92d4acba66ebb
Victor Rentea <victorrentea@gmail.com>
2020-10-01 15:31:30 +0300

    start testing with real client

--- src/test/java/ro/victor/unittest/mocks/telemetry/TelemetryDiagnosticControlsTest.java
+++ src/test/java/ro/victor/unittest/mocks/telemetry/TelemetryDiagnosticControlsTest.java
@@ -1,22 +1,17 @@
 package ro.victor.unittest.mocks.telemetry;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 
 public class TelemetryDiagnosticControlsTest {
-   private TelemetryDiagnosticControls controls = new TelemetryDiagnosticControls();
-   
+
    @Test
    public void test() {
-       
+      TelemetryDiagnosticControls controls = new TelemetryDiagnosticControls(new TelemetryClient());
+      controls.checkTransmission();
    }
 }
