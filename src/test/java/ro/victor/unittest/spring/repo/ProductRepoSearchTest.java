49ed7c2f593c6873aeabcac04da6e70a9c279198
Victor Rentea <victorrentea@gmail.com>
2020-10-02 12:42:21 +0300

    clear tables BEFORE tests

--- src/test/java/ro/victor/unittest/spring/repo/ProductRepoSearchTest.java
+++ src/test/java/ro/victor/unittest/spring/repo/ProductRepoSearchTest.java
@@ -2,12 +2,16 @@ package ro.victor.unittest.spring.repo;
 
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.annotation.DirtiesContext.MethodMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -20,6 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles({"db-mem", "test"})
+//@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD) // brutal, don't do it
 //@Category(IntegrationTest.class)
 //@ContextConfiguration(initializers = WaitForDBInitializer.class)
 public class ProductRepoSearchTest {
@@ -28,10 +33,23 @@ public class ProductRepoSearchTest {
 
     private ProductSearchCriteria criteria = new ProductSearchCriteria();
 
+    @Before
+    public void initialize() {
+        repo.deleteAll();// probleme pt ca tre sa stergi din tabele intr-o anumita ordine.
+        // (Dupa FK)
+        // Singura solutie daca lucrezi fara spring.
+
+        //
+    }
+
     @Test
     public void noCriteria() {
         repo.save(new Product());
-//        Assert.assertEquals(1, repo.search(criteria).size());
+        assertThat(repo.search(criteria)).hasSize(1);
+    }
+    @Test
+    public void noCriteria2() {
+        repo.save(new Product());
         assertThat(repo.search(criteria)).hasSize(1);
     }
 
