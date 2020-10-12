937d7f7fb39dd950d1eac0349fa7ee97b9e42061
Victor Rentea <victorrentea@gmail.com>
2020-10-12 11:05:39 +0300

    Junit5 ! :D

--- src/test/java/victor/testing/builder/CustomerValidatorTest.java
+++ src/test/java/victor/testing/builder/CustomerValidatorTest.java
@@ -2,51 +2,59 @@ package victor.testing.builder;
 
 
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import victor.testing.builder.MyException.ErrorCode;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class CustomerValidatorTest {
 
-	private CustomerValidator validator = new CustomerValidator();
+// Object Mother F.... (monoliti)
+class DummyData {
 
-	private Customer aValidCustomer() {
+	public static Customer aValidCustomer() {
 		return new Customer()
 			.setName("John")
+			.addLabel("a","b")
+			.addLabel("a","b")
+			.addLabel("a","b")
 			.addLabel("a","b")
 			.setAddress(aValidAddress());
 	}
 
-	private Address aValidAddress() {
+	public static Address aValidAddress() {
 		return new Address()
 			.setCity("Bucharest")
 			.setCountry("Romania");
 	}
+}
+
+public class CustomerValidatorTest {
+
+	private CustomerValidator validator = new CustomerValidator();
 
 	@Test
 	public void passForValidCustomer() {
-		validator.validate(aValidCustomer());
+		validator.validate(DummyData.aValidCustomer());
 	}
 
 	@Test
 //	public void whenCustomerHasNullName_throws() {
 	public void throwsForCustomerWithNullName() {
 		assertThrows(IllegalArgumentException.class,
-			() -> validator.validate(aValidCustomer().setName(null)));
+			() -> validator.validate(DummyData.aValidCustomer().setName(null)));
 	}
 
 	@Test
 	public void throwsForCustomerWithAddressWithNullCity() {
 		MyException ex = assertThrows(MyException.class,
-			() -> validator.validate(aValidCustomer().setAddress(aValidAddress().setCity(null))));
+			() -> validator.validate(DummyData.aValidCustomer().setAddress(DummyData.aValidAddress().setCity(null))));
 		assertEquals(ErrorCode.NO_ADDRESS_CITY, ex.getCode());
 	}
 	@Test
 	public void throwsForCustomerWithAddressNull() {
 		assertThrows(IllegalArgumentException.class,
-			() -> validator.validate(aValidCustomer().setAddress(null)));
+			() -> validator.validate(DummyData.aValidCustomer().setAddress(null)));
 	}
 
 }
