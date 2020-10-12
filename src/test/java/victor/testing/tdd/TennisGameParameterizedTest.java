e2af18c6820998f50f8205a3fc54440902d34fd0
Victor Rentea <victorrentea@gmail.com>
2020-10-12 12:30:51 +0300

    Foarte tare: teste Parameterized atunci cand tot ceea ce difera intre 4-5 teste de-ale tale sunt doar cativa literali (String, ints, etc)

---
+++ src/test/java/victor/testing/tdd/TennisGameParameterizedTest.java
@@ -0,0 +1,50 @@
+package victor.testing.tdd;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import victor.testing.tdd.TennisGame.Player;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TennisGameParameterizedTest {
+   public TennisGameParameterizedTest() {
+//      System.out.println("O instanta noua per @Test!");
+   }
+
+   private String translateScore(int points1, int points2) {
+      TennisGame tennisGame = new TennisGame();
+      markPoints(Player.ONE, points1, tennisGame);
+      markPoints(Player.TWO, points2, tennisGame);
+      return tennisGame.score();
+   }
+
+   private void markPoints(Player player, int points, TennisGame tennisGame) {
+      for (int i = 0; i < points; i++) {
+         tennisGame.markPoint(player);
+      }
+   }
+
+   private static Stream<Object[]> toateDatele() {
+      return Stream.of(
+          new Object[]{0, 0, "Love - Love"},
+          new Object[]{0, 1, "Love - Fifteen"},
+          new Object[]{1, 0, "Fifteen - Love"},
+          new Object[]{1, 1, "Fifteen - Fifteen"},
+          new Object[]{6, 6, "Deuce"}
+      );
+   }
+
+   @ParameterizedTest
+   @MethodSource("toateDatele")
+   public void loveLove(int points1, int points2, String expectedScore) {
+      assertThat(translateScore(points1, points2)).isEqualTo(expectedScore);
+   }
+
+
+}
