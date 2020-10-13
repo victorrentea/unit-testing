4d1413661cc75b4979c39c724e90f56b1bd39d77
Victor Rentea <victorrentea@gmail.com>
2020-10-13 16:36:43 +0300

    Black box testing: teste end to end prin toata app pana in DB in mem, tranzactate. Poti refactora tot in app, fara sa impactezi aceste teste

--- src/test/java/victor/testing/spring/web/ProductMvcTest.java
+++ src/test/java/victor/testing/spring/web/ProductMvcTest.java
@@ -1,6 +1,8 @@
 package victor.testing.spring.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,29 +11,51 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import victor.testing.spring.domain.Product;
+import victor.testing.spring.domain.ProductCategory;
+import victor.testing.spring.domain.Supplier;
 import victor.testing.spring.repo.ProductRepo;
+import victor.testing.spring.repo.SupplierRepo;
+import victor.testing.spring.tools.WireMockExtension;
+import victor.testing.spring.web.dto.ProductDto;
+
+import javax.annotation.RegEx;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Transactional
-@SpringBootTest
-@AutoConfigureMockMvc
+//@Sql
+@SpringBootTest(properties = "safety.service.url.base=http://localhost:9999")
 @ActiveProfiles("db-mem")
+@Transactional
+@AutoConfigureMockMvc // emuleaza requesturi HTTP **FARA** sa porneasca Tomcat.
 public class ProductMvcTest {
     @Autowired
     private MockMvc mockMvc;
-
     @Autowired
-    private ProductRepo productRepo;
+    private SupplierRepo supplierRepo;
+
+    @RegisterExtension
+    public WireMockExtension wireMock = new WireMockExtension(9999);
 
     @Test
     public void testSearch() throws Exception {
-        productRepo.save(new Product().setName("Tree"));
+        // date de referinta
+        Long supplierId = supplierRepo.save(new Supplier()).getId();
+//        productRepo.save(new Product().setName("Tree"));
+
+        ProductDto dto = new ProductDto("Tree", "SAFE", supplierId, ProductCategory.ME);
+        String createJson = new ObjectMapper().writeValueAsString(dto);
+        mockMvc.perform(post("/product/create")
+            .content(createJson)
+            .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+
 
         mockMvc.perform(post("/product/search")
-            .content("{}")
+            .content("{\n" +
+                     "  \"name\": \"R\"\n" +
+                     "}")
             .contentType(MediaType.APPLICATION_JSON)
         )
             .andExpect(status().isOk())
@@ -39,27 +63,4 @@ public class ProductMvcTest {
             .andExpect(jsonPath("$", hasSize(1)));
 //            .andExpect(jsonPath("$[0].name").value("Tree"));
     }
-
-    // The MockMvc EMULATES a HTTP call, w/o Tomcat, w/o any HTTP Worker Thread Pool,
-    // I'm running the COntroller in the same thread as the test
-    // and since the Transaction in Spring is bound to the current thread ->
-    // I can make sure the .search repo works in the same Tx as the test one,
-    // in which I INSERTed the Product
-
-    @Test
-    public void testSearc2h() throws Exception {
-        productRepo.save(new Product().setName("Tree"));
-
-        mockMvc.perform(post("/product/search")
-            .content("{}")
-            .contentType(MediaType.APPLICATION_JSON)
-        )
-            .andExpect(status().isOk())
-            .andExpect(header().string("Custom-Header", "true"))
-
-
-            .andExpect(jsonPath("$[0].name").value("Tree"));
-    }
-
-
 }
