bdb9657d4b4bc521b4ef5d4a7b292dac5d791c53
Victor Rentea <victorrentea@gmail.com>
2020-10-15 15:54:31 +0300

    Purify de design by returning the value, not verify() the side effect

--- src/test/java/victor/testing/mocks/telemetry/TelemetryDiagnosticControlsTest.java
+++ src/test/java/victor/testing/mocks/telemetry/TelemetryDiagnosticControlsTest.java
@@ -1,59 +1,90 @@
 package victor.testing.mocks.telemetry;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import victor.testing.mocks.telemetry.TelemetryClient.ClientConfiguration;
+import victor.testing.mocks.telemetry.TelemetryClient.ClientConfiguration.AckMode;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TelemetryDiagnosticControlsTest {
-   @Mock
-   private TelemetryClient client;
-   @InjectMocks
-   private TelemetryDiagnosticControls controls;
+	@Mock
+	private TelemetryClient client;
+	@InjectMocks
+	private TelemetryDiagnosticControls controls;
 
-   @Test
-   public void disconnects() {
-      when(client.getOnlineStatus()).thenReturn(true);
-      controls.checkTransmission();
-      verify(client).disconnect();
-   }
+//	@Before
+//	public void before() {
+//		controls = new TelemetryDiagnosticControls(client, "STUFF");
+//	}
 
-   @Test(expected = IllegalStateException.class)
-   public void throwsWhenNotOnline() {
-      when(client.getOnlineStatus()).thenReturn(false);
-      controls.checkTransmission();
-   }
+	
+	@Test(expected = IllegalStateException.class)
+	public void throwsWhenNotOnline() {
+		when(client.getOnlineStatus()).thenReturn(false);
+		controls.checkTransmission();
+	}
 
-   @Test
-   public void sendsDiagnosticInfo() {
-      when(client.getOnlineStatus()).thenReturn(true);
-      controls.checkTransmission();
-      verify(client).send(TelemetryClient.DIAGNOSTIC_MESSAGE);
-   }
+	
+	@Test
+	public void disconnects() {
+		when(client.getOnlineStatus()).thenReturn(true);
+		controls.checkTransmission();
+		verify(client).disconnect();
+	}
 
-   @Test
-   public void receivesDiagnosticInfo() {
-      // TODO inspect
-      when(client.getOnlineStatus()).thenReturn(true);
-      when(client.receive()).thenReturn("tataie");
-      controls.checkTransmission();
-      verify(client).receive();
-      assertThat(controls.getDiagnosticInfo()).isEqualTo("tataie");
-   }
+	@Test
+	public void sendsDiagnosticInfo() {
+		when(client.getOnlineStatus()).thenReturn(true);
+		controls.checkTransmission();
+		verify(client).send(TelemetryClient.DIAGNOSTIC_MESSAGE);
+	}
 
-   @Test
-   public void configuresClient() throws Exception {
-      when(client.getOnlineStatus()).thenReturn(true);
-      controls.checkTransmission();
-      verify(client).configure(any());
-      // TODO check config.getAckMode is NORMAL
-   }
+	// HOMEWORK: what line in the test below is useless
+	@Test
+	public void receivesDiagnosticInfo() {
+		// TODO inspect
+		when(client.getOnlineStatus()).thenReturn(true);
+		when(client.receive()).thenReturn("kalimera");
+		controls.checkTransmission();
+//		verify(client).receive(); // 99% cases is useless
+		// 1% ? == 
+//		verify(client/* , Mockito.times(1) default*/).receive(); // expensive extenal call: you might one
+//		verify(client).receive(captor); // B) when you captor
+		assertThat(controls.getDiagnosticInfo()).isEqualTo("kalimera");
+	}
+
+	@Captor
+	private ArgumentCaptor<ClientConfiguration> configCaptor;
+	
+	@Test
+	public void configuresClient() throws Exception {
+		when(client.getVersion()).thenReturn("ver");
+		ClientConfiguration config = controls.createConfig();
+		assertThat(config.getAckMode()).isEqualTo(AckMode.NORMAL);
+		assertThat(config.getSessionId()).startsWith("ver-");
+		assertThat(config.getSessionStart()).isNotNull(); // engineering
+	}
+	
+	
+	// more strict engineers
+//		LocalDateTime expectedDay = LocalDateTime.now().truncatedTo(ChronoUnit.DAYS);
+//		LocalDateTime actualDay = config.getSessionStart().truncatedTo(ChronoUnit.DAYS);
+//		assertThat(actualDay).isEqualTo(expectedDay);
 }
