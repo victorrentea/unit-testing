f88b23fed301f339bb243a41d2e30c911805d524
Victor Rentea <victorrentea@gmail.com>
2020-10-23 12:20:27 +0300

    Revert to JUNIT 4

--- src/test/java/victor/testing/spring/service/ProductServiceClientWireMockTest.java
+++ src/test/java/victor/testing/spring/service/ProductServiceClientWireMockTest.java
@@ -1,11 +1,14 @@
 package victor.testing.spring.service;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -14,6 +17,8 @@ import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.domain.ProductCategory;
 import victor.testing.spring.domain.Supplier;
@@ -24,7 +29,9 @@ import victor.testing.spring.tools.WireMockExtension;
 import victor.testing.spring.web.dto.ProductDto;
 import wiremock.org.apache.commons.io.IOUtils;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -33,101 +40,97 @@ import java.time.ZoneId;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(properties = "safety.service.url.base=http://localhost:8089")
 @ActiveProfiles("db-mem")
 public class ProductServiceClientWireMockTest {
-   @Autowired
-   public SafetyClient mockSafetyClient;
-   @Autowired
-   private ProductRepo productRepo;
-   @Autowired
-   private SupplierRepo supplierRepo;
-   @Autowired
-   private ProductService productService;
+	@Autowired
+	public SafetyClient mockSafetyClient;
+	@Autowired
+	private ProductRepo productRepo;
+	@Autowired
+	private SupplierRepo supplierRepo;
+	@Autowired
+	private ProductService productService;
 
-   @RegisterExtension
-   public WireMockExtension wireMock = new WireMockExtension(8089);
-   @Autowired
-   private CacheManager cacheManager;
+	@Rule
+	public WireMockRule wireMock = new WireMockRule(8089);
+	@Autowired
+	private CacheManager cacheManager;
 
-   @BeforeEach
-   public void initialize() {
-      // Clear manually all caches
-      cacheManager.getCacheNames().stream().map(cacheManager::getCache).forEach(Cache::clear);
-   }
+	@Before
+	public void initialize() {
+		// Clear manually all caches
+		cacheManager.getCacheNames().stream().map(cacheManager::getCache).forEach(Cache::clear);
+	}
 
-   @Test
-   public void throwsForUnsafeProduct() {
-      Assertions.assertThrows(IllegalStateException.class, () -> {
-         productService.createProduct(new ProductDto("name", "UNSAFE", -1L, ProductCategory.HOME));
-      });
-   }
+	@Test(expected = IllegalStateException.class)
+	public void throwsForUnsafeProduct() {
+		productService.createProduct(new ProductDto("name", "UNSAFE", -1L, ProductCategory.HOME));
+	}
 
-   @Test
-   public void throwsForUnsafeProductProgrammaticWireMock() {
-      Assertions.assertThrows(IllegalStateException.class, () -> {
-         WireMock.stubFor(get(urlEqualTo("/product/customXX/safety"))
-             .willReturn(aResponse()
-                 .withStatus(200)
-                 .withHeader("Content-Type", "application/json")
-                 .withBody("{\"entries\": [{\"category\": \"DETERMINED\",\"detailsUrl\": \"http://wikipedia.com\"}]}"))); // override
+	@Test(expected = IllegalStateException.class)
+	public void throwsForUnsafeProductProgrammaticWireMock() {
+		WireMock.stubFor(get(urlEqualTo("/product/customXX/safety"))
+				.willReturn(aResponse()
+						.withStatus(200)
+						.withHeader("Content-Type", "application/json")
+						.withBody(
+								"{\"entries\": [{\"category\": \"DETERMINED\",\"detailsUrl\": \"http://wikipedia.com\"}]}"))); // override
 
+		productService.createProduct(new ProductDto("name", "customXX", -1L, ProductCategory.HOME));
+	}
 
-         productService.createProduct(new ProductDto("name", "customXX", -1L, ProductCategory.HOME));
-      });
-   }
+	@Test(expected = IllegalStateException.class)
+	public void throwsForUnsafeProductProgrammaticWireMockFromFileTemplatized()
+			throws FileNotFoundException, IOException {
+		String template;
+		try (FileReader reader = new FileReader(
+				"C:\\workspace\\integration-testing-spring\\src\\test\\java\\victor\\testing\\spring\\facade\\inTemplate.json")) {
+			template = IOUtils.toString(reader);
+		}
+		template.replace("{{}}", "DYNAMIC STUFF");
+		WireMock.stubFor(get(urlEqualTo("/product/customXX/safety"))
+				.willReturn(aResponse()
+						.withStatus(200)
+						.withHeader("Content-Type", "application/json")
+						.withBody(template))); // override
 
-   @Test
-   @SneakyThrows
-   public void throwsForUnsafeProductProgrammaticWireMockFromFileTemplatized() {
-      Assertions.assertThrows(IllegalStateException.class, () -> {
-         String template;
-         try (FileReader reader = new FileReader("C:\\workspace\\integration-testing-spring\\src\\test\\java\\victor\\testing\\spring\\facade\\inTemplate.json")) {
-            template = IOUtils.toString(reader);
-         }
-         template.replace("{{}}", "DYNAMIC STUFF");
-         WireMock.stubFor(get(urlEqualTo("/product/customXX/safety"))
-             .willReturn(aResponse()
-                 .withStatus(200)
-                 .withHeader("Content-Type", "application/json")
-                 .withBody(template))); // override
+		productService.createProduct(new ProductDto("name", "customXX", -1L, ProductCategory.HOME));
+	}
 
+	@Test
+	public void fullOk() {
+		long supplierId = supplierRepo.save(new Supplier()).getId();
 
-         productService.createProduct(new ProductDto("name", "customXX", -1L, ProductCategory.HOME));
-      });
-   }
+		ProductDto dto = new ProductDto("name", "SAFE", supplierId, ProductCategory.HOME);
+		productService.createProduct(dto);
 
-   @Test
-   public void fullOk() {
-      long supplierId = supplierRepo.save(new Supplier()).getId();
+		Product product = productRepo.findAll().get(0);
+		LocalDateTime today = LocalDateTime.parse("2014-12-22T10:15:30.00");
 
-      ProductDto dto = new ProductDto("name", "SAFE", supplierId, ProductCategory.HOME);
-      productService.createProduct(dto);
+		assertThat(product.getName()).isEqualTo("name");
+		assertThat(product.getUpc()).isEqualTo("SAFE");
+		assertThat(product.getSupplier().getId()).isEqualTo(supplierId);
+		assertThat(product.getCategory()).isEqualTo(ProductCategory.HOME);
+		assertThat(product.getCreateDate()).isEqualTo(today);
+	}
 
-      Product product = productRepo.findAll().get(0);
-      LocalDateTime today = LocalDateTime.parse("2014-12-22T10:15:30.00");
+	@TestConfiguration
+	public static class TestConfig {
+		@Bean
+		@Primary
+		public Clock clockFixed() {
+			return Clock.fixed(Instant.parse("2014-12-22T10:15:30.00Z"), ZoneId.systemDefault());
+		}
+	}
 
-      assertThat(product.getName()).isEqualTo("name");
-      assertThat(product.getUpc()).isEqualTo("SAFE");
-      assertThat(product.getSupplier().getId()).isEqualTo(supplierId);
-      assertThat(product.getCategory()).isEqualTo(ProductCategory.HOME);
-      assertThat(product.getCreateDate()).isEqualTo(today);
-   }
+	// TODO Fixed Time
+	// @TestConfiguration public static class ClockConfig { @Bean @Primary public
+	// Clock fixedClock() {}}
 
-   @TestConfiguration
-   public static class TestConfig {
-      @Bean
-      @Primary
-      public Clock clockFixed() {
-         return Clock.fixed(Instant.parse("2014-12-22T10:15:30.00Z"), ZoneId.systemDefault());
-      }
-   }
-
-
-   // TODO Fixed Time
-   // @TestConfiguration public static class ClockConfig {  @Bean  @Primary  public Clock fixedClock() {}}
-
-   // TODO Variable Time
-   // when(clock.instant()).thenAnswer(call -> currentTime.toInstant(ZoneId.systemDefault().getRules().getOffset(currentTime)));
-   // when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+	// TODO Variable Time
+	// when(clock.instant()).thenAnswer(call ->
+	// currentTime.toInstant(ZoneId.systemDefault().getRules().getOffset(currentTime)));
+	// when(clock.getZone()).thenReturn(ZoneId.systemDefault());
 }
