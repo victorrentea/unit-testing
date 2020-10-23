b2c63ac95ab9af4fd492418479bc054f39c45e45
Victor Rentea <victorrentea@gmail.com>
2020-10-23 14:19:26 +0300

    End to end using test DSL (mini frameowkr) ca sa ascuzi uratenia HTTP calls

--- src/test/java/victor/testing/spring/web/ProductMvcTest.java
+++ src/test/java/victor/testing/spring/web/ProductMvcTest.java
@@ -1,12 +1,14 @@
 package victor.testing.spring.web;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -14,11 +16,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 import victor.testing.spring.domain.Product;
+import victor.testing.spring.domain.ProductCategory;
 import victor.testing.spring.domain.Supplier;
 import victor.testing.spring.repo.ProductRepo;
 import victor.testing.spring.repo.SupplierRepo;
@@ -31,6 +36,7 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -48,29 +54,31 @@ public class ProductMvcTest {
     @Rule
     public WireMockRule wireMock = new WireMockRule(8089);
 
+	private Long supplierId;
+	private ProductSearchCriteria criteria = new ProductSearchCriteria();
+
+    @Before
+    public void persistRefData() {
+    	supplierId = supplierRepo.save(new Supplier()).getId();
+    }
+    
     @Test
     public void testSearch() throws Exception {
-    	Long supplierId = supplierRepo.save(new Supplier()).getId();
-//        productRepo.save(new Product().setName("Tree"));
-        
-    	ProductDto dto = new ProductDto();
-    	dto.name="Tree";
-    	dto.upc="1";
-    	dto.supplierId = supplierId;
-    	String createJson = new ObjectMapper().writeValueAsString(dto);
-        
+    	createProduct(new ProductDto("Tree", "1",supplierId, ProductCategory.ME));
+    	List<ProductSearchResult> results = searchProduct(criteria);
+    	assertThat(results).hasSize(1);
+    }
+    @Test
+    public void testSearchByName() throws Exception {
+    	createProduct(new ProductDto("Tree", "1",supplierId, ProductCategory.ME));
+    	criteria.name = "rE";
+    	List<ProductSearchResult> results = searchProduct(criteria);
+		assertThat(results).hasSize(1);
+    }
 
-    	mockMvc.perform(post("/product/create")
-    			.content(createJson)
-    			.contentType(MediaType.APPLICATION_JSON)
-    			)
-	    	.andExpect(status().isOk())
-	    	.andExpect(header().string("Custom-Header", "true"));
-
-
-    	ProductSearchCriteria criteria = new ProductSearchCriteria();
-    	
-    	String searchJson = new ObjectMapper().writeValueAsString(criteria);
+	private List<ProductSearchResult> searchProduct(ProductSearchCriteria criteria)
+			throws JsonProcessingException, UnsupportedEncodingException, Exception, JsonMappingException {
+		String searchJson = new ObjectMapper().writeValueAsString(criteria);
 		String responseJson = mockMvc.perform(post("/product/search")
             .content(searchJson)
             .contentType(MediaType.APPLICATION_JSON)
@@ -83,9 +91,18 @@ public class ProductMvcTest {
 		List<ProductSearchResult> results = new ObjectMapper().readValue(responseJson,
 //				List<ProductSearchResult>.class
 				new TypeReference<List<ProductSearchResult>>() {});
-		
-		
-		assertThat(results).hasSize(1);
-		
-    }
+		return results;
+	}
+
+	private void createProduct(ProductDto dto) throws JsonProcessingException, Exception {
+		String createJson = new ObjectMapper().writeValueAsString(dto);
+        
+
+    	mockMvc.perform(post("/product/create")
+    			.content(createJson)
+    			.contentType(MediaType.APPLICATION_JSON)
+    			)
+	    	.andExpect(status().isOk())
+	    	.andExpect(header().string("Custom-Header", "true"));
+	}
 }
