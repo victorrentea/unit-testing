bb0cee72057f3128a375a4553d7533a52f7c9ff7
Victor Rentea <victorrentea@gmail.com>
2020-10-23 14:07:43 +0300

    Black box test: prin MockMvc

--- src/test/java/victor/testing/spring/web/ProductMvcTest.java
+++ src/test/java/victor/testing/spring/web/ProductMvcTest.java
@@ -1,65 +1,70 @@
 package victor.testing.spring.web;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
 import victor.testing.spring.domain.Product;
+import victor.testing.spring.domain.Supplier;
 import victor.testing.spring.repo.ProductRepo;
+import victor.testing.spring.repo.SupplierRepo;
+import victor.testing.spring.web.dto.ProductDto;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Transactional
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(properties = "safety.service.url.base=http://localhost:8089")
 @AutoConfigureMockMvc
 @ActiveProfiles("db-mem")
+@Transactional
 public class ProductMvcTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private ProductRepo productRepo;
+    private SupplierRepo supplierRepo;
+    
+    @Rule
+    public WireMockRule wireMock = new WireMockRule(8089);
 
     @Test
     public void testSearch() throws Exception {
-        productRepo.save(new Product().setName("Tree"));
+    	Long supplierId = supplierRepo.save(new Supplier()).getId();
+//        productRepo.save(new Product().setName("Tree"));
+        
+    	ProductDto dto = new ProductDto();
+    	dto.name="Tree";
+    	dto.upc="1";
+    	dto.supplierId = supplierId;
+    	String createJson = new ObjectMapper().writeValueAsString(dto);
+        
 
-        mockMvc.perform(post("/product/search")
+    	mockMvc.perform(post("/product/create")
+    			.content(createJson)
+    			.contentType(MediaType.APPLICATION_JSON)
+    			)
+	    	.andExpect(status().isOk())
+	    	.andExpect(header().string("Custom-Header", "true"));
+
+    	mockMvc.perform(post("/product/search")
             .content("{}")
             .contentType(MediaType.APPLICATION_JSON)
         )
             .andExpect(status().isOk())
             .andExpect(header().string("Custom-Header", "true"))
             .andExpect(jsonPath("$", hasSize(1)));
-//            .andExpect(jsonPath("$[0].name").value("Tree"));
     }
-
-    // The MockMvc EMULATES a HTTP call, w/o Tomcat, w/o any HTTP Worker Thread Pool,
-    // I'm running the COntroller in the same thread as the test
-    // and since the Transaction in Spring is bound to the current thread ->
-    // I can make sure the .search repo works in the same Tx as the test one,
-    // in which I INSERTed the Product
-
-    @Test
-    public void testSearc2h() throws Exception {
-        productRepo.save(new Product().setName("Tree"));
-
-        mockMvc.perform(post("/product/search")
-            .content("{}")
-            .contentType(MediaType.APPLICATION_JSON)
-        )
-            .andExpect(status().isOk())
-            .andExpect(header().string("Custom-Header", "true"))
-
-
-            .andExpect(jsonPath("$[0].name").value("Tree"));
-    }
-
-
 }
