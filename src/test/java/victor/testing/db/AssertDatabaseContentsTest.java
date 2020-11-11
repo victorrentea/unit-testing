cd69c369ef84a40bfea2eb00e80a8808e2958ac9
Victor Rentea <victorrentea@gmail.com>
2020-11-11 16:49:15 +0200

    TO try out: Run tests with DB in a docker

--- src/test/java/victor/testing/db/AssertDatabaseContentsTest.java
+++ src/test/java/victor/testing/db/AssertDatabaseContentsTest.java
@@ -8,12 +8,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.github.springtestdbunit.annotation.ExpectedDatabase;
 
 import victor.testing.db.prod.NotificationRepo;
 
+
+//@ClearCache
+//@Dataset(value = "dataset/ConfigOptionsDaoImplTest_dataset.xml", flatXml = true)
+//@ContextConfiguration("classpath:common-dao-context.xml")
+//@Transactional
+//@TransactionConfiguration
+//public class ConfigOptionsDaoImplTest extends AbstractTestsAssertions {
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AssertDatabaseContentsTest {
