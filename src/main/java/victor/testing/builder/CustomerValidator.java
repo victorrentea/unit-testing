ce2cb90707e3b048f9c2c9ca1ed17bff84e190f4
Victor Rentea <victorrentea@gmail.com>
2021-03-04 15:14:09 +0200

    Assserting exceptions

--- src/main/java/victor/testing/builder/CustomerValidator.java
+++ src/main/java/victor/testing/builder/CustomerValidator.java
@@ -1,9 +1,10 @@
 package victor.testing.builder;
 
+import javax.validation.constraints.NotNull;
+
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
 public class CustomerValidator {
-
 	public void validate(Customer customer) {
 		if (isBlank(customer.getName())) {
 			throw new IllegalArgumentException("Missing customer name");
---
+++ src/test/java/victor/testing/builder/CustomerValidatorTest.java
@@ -0,0 +1,42 @@
+package victor.testing.builder;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
+
+public class CustomerValidatorTest {
+
+   private CustomerValidator validator = new CustomerValidator();
+
+//   public void whenCustomerNameIsEmpty_throws() {
+//   public void emptyName() { // cam lene
+   @Test(expected = IllegalArgumentException.class)
+   public void throwsForMissingAddress() {
+      Customer customer = new Customer();
+      customer.setName("John");
+      validator.validate(customer);
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void throwsForEmptyName() {
+      validator.validate(new Customer());
+   }
+
+   @Rule
+   public ExpectedException exception = ExpectedException.none();
+
+   @Test
+   public void throwsForEmptyCity() {
+
+      Customer customer = new Customer();
+      customer.setName("John");
+      Address address = new Address();
+      customer.setAddress(address);
+
+      exception.expectMessage("city");// Draga JUnit, vezi ca testul asta o sa crape cu exceptie. E Ok. relax. Cata vreme ex cotnine "city"
+
+      validator.validate(customer);
+   }
+}
