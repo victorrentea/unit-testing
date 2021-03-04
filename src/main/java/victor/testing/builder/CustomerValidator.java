8bcd2366db8b1dcd3ddf4d9c00406cd0547af040
Victor Rentea <victorrentea@gmail.com>
2021-03-04 16:00:50 +0200

    Mutation Testing e mai draconic decat coverage 100%

--- src/main/java/victor/testing/builder/CustomerValidator.java
+++ src/main/java/victor/testing/builder/CustomerValidator.java
@@ -4,12 +4,20 @@ import javax.validation.constraints.NotNull;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
+class NotificationSender {
+
+	public void sendEmailCaEOK() {
+
+	}
+}
 public class CustomerValidator {
+	NotificationSender sender = new NotificationSender();
 	public void validate(Customer customer) {
 		validateAddress(customer.getAddress());
 		if (isBlank(customer.getName())) {
 			throw new IllegalArgumentException("Missing customer name");
 		}
+		sender.sendEmailCaEOK(); // linia asta nu e verificata acum de teste.
 		//etc
 	}
 	
