cb7617e606e4d20a077329c5934fa35e3d713c87
Victor Rentea <victorrentea@gmail.com>
2021-03-04 16:46:38 +0200

    NPE ca Validator ramane null, ca nu e @Mock sus.

--- src/test/java/victor/testing/mocks/PrimeleMockuri.java
+++ src/test/java/victor/testing/mocks/PrimeleMockuri.java
@@ -2,35 +2,45 @@ package victor.testing.mocks;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import victor.testing.builder.Customer;
+import victor.testing.builder.CustomerValidator;
 
 import static org.junit.Assert.assertEquals;
 
-//@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class PrimeleMockuri {
 
+   @Mock
+   private Incomoda incomoda;// = Mockito.mock(Incomoda.class);
 //   @Mock
-   Incomoda incomoda = Mockito.mock(Incomoda.class);
-
+//   private CustomerValidator validator;// = Mockito.mock(Incomoda.class);
+   @InjectMocks
+   private UserService deTestat;// = new UserService(incomoda);
    @Test
    public void test() {
       Mockito.when(incomoda.cevaExtern()).thenReturn(2);
-      UserService deTestat = new UserService(incomoda);
       assertEquals(3, deTestat.deBiz());;
    }
 }
-
 class UserService {
    private final Incomoda incomoda;
-   UserService(Incomoda incomoda) {
+   private final CustomerValidator validator;
+
+   UserService(Incomoda incomoda, CustomerValidator validator) {
+      System.out.println("val="+validator);
       this.incomoda = incomoda;
+      this.validator = validator;
    }
    public int deBiz() {
+      validator.validate(new Customer());
        return incomoda.cevaExtern() + 1;
    }
 }
+
 class Incomoda {
    public int cevaExtern() {
        throw new IllegalArgumentException(" nu pot fi chemat din teste");
