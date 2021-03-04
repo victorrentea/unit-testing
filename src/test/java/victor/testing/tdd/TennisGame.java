df84fbfcec0a7cfe476147bf09b91248353f50f0
Victor Rentea <victorrentea@gmail.com>
2021-03-04 12:26:15 +0200

    End of TDD

--- src/test/java/victor/testing/tdd/TennisGame.java
+++ src/test/java/victor/testing/tdd/TennisGame.java
@@ -12,8 +12,19 @@ public class TennisGame {
 
 
    public String score() {
-      if (player1Score >= 3 && player2Score == player1Score) {
-         return "Deuce";
+      if (player1Score >= 3 && player2Score >= 3) {
+         if (player1Score - player2Score == 1) {
+            return "Advantage Player 1";
+         }
+         if (player2Score - player1Score == 1) {
+            return "Advantage Player 2";
+         }
+         if (player2Score == player1Score) {
+            return "Deuce";
+         }
+      }
+      if (player1Score >= 4 && player1Score - player2Score >= 2) {
+         return "Game Won Player 1";
       }
       return translateScore(player1Score) + " - " +
              translateScore(player2Score);
--- src/test/java/victor/testing/tdd/TennisGameTest.java
+++ src/test/java/victor/testing/tdd/TennisGameTest.java
@@ -1,50 +1,55 @@
 package victor.testing.tdd;
 
-import org.junit.Before;
 import org.junit.Test;
 import victor.testing.tdd.TennisGame.Player;
 
 import static org.junit.Assert.assertEquals;
+import static victor.testing.tdd.TennisGame.Player.ONE;
+import static victor.testing.tdd.TennisGame.Player.TWO;
 
 public class TennisGameTest {
 
-
    private TennisGame tennisGame = new TennisGame();
 
-   public TennisGameTest() {
-      System.out.println("Cate instante de clasa de test se creeaza");
+   private void setScore(int player1Points, int player2Points) {
+      scorePoints(ONE, player1Points);
+      scorePoints(TWO, player2Points);
+   }
+
+   // mini-test framework
+   private void scorePoints(Player player, int points) {
+      for (int i = 0; i < points; i++) {
+         tennisGame.playerScores(player);
+      }
+   }
+   private String translateScore(int player1Score, int player2Score) {
+      setScore(player1Score, player2Score);
+      return tennisGame.score();
    }
 
    @Test
    public void loveLove() {
-      String score = tennisGame.score();
+      String score = translateScore(0, 0);
       assertEquals("Love - Love",score);
    }
    @Test
    public void loveFifteen() {
-      tennisGame.playerScores(Player.TWO);
-      String score = tennisGame.score();
+      String score = translateScore(0, 1);
       assertEquals("Love - Fifteen",score);
    }
    @Test
    public void loveThirty() {
-      tennisGame.playerScores(Player.TWO);
-      tennisGame.playerScores(Player.TWO);
-      String score = tennisGame.score();
+      String score = translateScore(0, 2);
       assertEquals("Love - Thirty",score);
    }
    @Test
    public void loveForty() {
-      tennisGame.playerScores(Player.TWO);
-      tennisGame.playerScores(Player.TWO);
-      tennisGame.playerScores(Player.TWO);
-      String score = tennisGame.score();
+      String score = translateScore(0, 3);
       assertEquals("Love - Forty",score);
    }
    @Test
    public void fifteenLove() {
-      tennisGame.playerScores(Player.ONE);
-      String score = tennisGame.score();
+      String score = translateScore(1, 0);
       assertEquals("Fifteen - Love",score);
    }
 
@@ -54,41 +59,67 @@ public class TennisGameTest {
 
    @Test
    public void thirtyThirty() {
-      tennisGame.playerScores(Player.TWO);
-      tennisGame.playerScores(Player.TWO);
-      tennisGame.playerScores(Player.ONE);
-      tennisGame.playerScores(Player.ONE);
-      String score = tennisGame.score();
+      String score = translateScore(2, 2);
       assertEquals("Thirty - Thirty",score);
    }
 
    @Test
    public void deuce() {
-      tennisGame.playerScores(Player.ONE);
-      tennisGame.playerScores(Player.ONE);
-      tennisGame.playerScores(Player.ONE);
-      tennisGame.playerScores(Player.TWO);
-      tennisGame.playerScores(Player.TWO);
-      tennisGame.playerScores(Player.TWO);
-      String score = tennisGame.score();
+      String score = translateScore(3, 3);
       assertEquals("Deuce",score);
    }
 
    @Test
    public void deuce4() {
-      playerOneScores4Points(tennisGame);
-      tennisGame.playerScores(Player.TWO);
-      tennisGame.playerScores(Player.TWO);
-      tennisGame.playerScores(Player.TWO);
-      tennisGame.playerScores(Player.TWO);
-      String score = tennisGame.score();
+      String score = translateScore(4, 4);
       assertEquals("Deuce",score);
    }
 
-   private void playerOneScores4Points(TennisGame tennisGame) {
-      for (int i = 0; i < 4; i++) {
-         tennisGame.playerScores(Player.ONE);
-      }
+
+
+   // A game is won by the first player to have scored at
+   // least 4 points in total and at least 2 points
+   // more than the opponent.
+
+   @Test
+   public void gameWonPlayer1() {
+      String score = translateScore(4, 2);
+      assertEquals("Game Won Player 1", score);
+   }
+   @Test
+   public void gameWonPlayer1_over4() {
+      String score = translateScore(5, 3);
+      assertEquals("Game Won Player 1", score);
+   }
+   @Test
+   public void gameWonPlayer1_batut_mar() {
+      String score = translateScore(4, 0);
+      assertEquals("Game Won Player 1", score);
+   }
+   // TODO game won player2
+
+   // If at least three points have been scored by each side
+   // and a player has one more point than his opponent,
+   // the score of the game is "Advantage" for the player in the lead.
+   @Test
+   public void advantagePlayer1() {
+      String score = translateScore(4,3);
+      assertEquals("Advantage Player 1", score);
+   }
+   @Test
+   public void advantagePlayer1_later() {
+      String score = translateScore(7,6);
+      assertEquals("Advantage Player 1", score);
+   }
+   @Test
+   public void advantagePlayer2() {
+      String score = translateScore(3,4);
+      assertEquals("Advantage Player 2", score);
+   }
+   @Test
+   public void advantagePlayer2_later() {
+      String score = translateScore(6,7);
+      assertEquals("Advantage Player 2", score);
    }
 
 
