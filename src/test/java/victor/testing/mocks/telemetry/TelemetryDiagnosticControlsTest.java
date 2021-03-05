fbd3d6825c9a467016a479dbc458fd69b741e823
Victor Rentea <victorrentea@gmail.com>
2021-03-05 11:22:26 +0200

    Am terminat de testat cu ArgumentCaptor

--- src/test/java/victor/testing/mocks/telemetry/TelemetryDiagnosticControlsTest.java
+++ src/test/java/victor/testing/mocks/telemetry/TelemetryDiagnosticControlsTest.java
@@ -1,13 +1,27 @@
 package victor.testing.mocks.telemetry;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import victor.testing.mocks.telemetry.TelemetryClient.ClientConfiguration;
+import victor.testing.mocks.telemetry.TelemetryClient.ClientConfiguration.AckMode;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+
+import static java.time.LocalDateTime.now;
+import static java.time.temporal.ChronoUnit.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -20,63 +34,23 @@ public class TelemetryDiagnosticControlsTest {
    private TelemetryDiagnosticControls controls; // setter, ctor or private field injection
 
    @Test
-   public void disconnects() {
-      when(client.getOnlineStatus()).thenReturn(true);
-      controls.checkTransmission(true);
-      verify(client).disconnect(true);
-   }
-
-   @Test
-   public void sendsDiagnosticMessage() {
-      when(client.getOnlineStatus()).thenReturn(true);
-      controls.checkTransmission(true);
-
-//      verify(client).send(anyString()); // nu-mi pasa ce valoare imi da - riscanta
-
-      verify(client).send(TelemetryClient.DIAGNOSTIC_MESSAGE); // refolosesc orbeste constanta
-      // foloseste asta oricand constanta ta NU IESE din sistemul tau.
-
-      verify(client).send("AT#UD"); // validez ca literalul este cel care trebuie
-      // varianta asta o folosim doar daca : E UN PROTOCOL extern cu un sistem tertz (Seriala)
-   }
-
-   @Test
-   public void receivesDiagnosticInfo() {
+   public void happyFlow() {
       when(client.getOnlineStatus()).thenReturn(true);
       when(client.receive()).thenReturn("tataie");
 
       controls.checkTransmission(true);
 
-//      verify(client).receive(); // inutila
+      verify(client).disconnect(true);
+      verify(client).send(TelemetryClient.DIAGNOSTIC_MESSAGE); // refolosesc orbeste constanta
       assertEquals("tataie", controls.getDiagnosticInfo());
    }
 
    @Test(expected = IllegalStateException.class)
    public void throwsWhenNotOnline() {
-      when(client.getOnlineStatus()).thenReturn(false);
+      when(client.getOnlineStatus()).thenReturn(false); // reprogrameaza mockul ignorand ce era in @Befor einvatat sa faca
       controls.checkTransmission(true);
    }
 
-//   @Test
-//   public void test() {
-////   LdapUserApiClient client = new LdapUserApiClient();
-//      LdapUserApiClient client = new LdapUserApiClient(){
-//         @Override
-//         public String getFullName(String username) {
-////            super.findUser() NU cheama metoda originala !!
-//            return "ldapCica";
-//         }
-//      };
-//
-//       Mockito.mock(LdapUserApiClient.class);
-//      when(client.getFullName("a")).thenReturn("ldapCica");
-//      UserService service = new UserService(client);
-//
-//      String result = service.biz();
-//      assertEquals("LDAPCICA", result);
-//   }
-   
-   
    @Test
    public void ifOfflineTriesToConnectOnce() {
       when(client.getOnlineStatus()).thenReturn(false, true);
@@ -88,6 +62,37 @@ public class TelemetryDiagnosticControlsTest {
       when(client.getOnlineStatus()).thenReturn(false,false, true);
       controls.checkTransmission(true);
       verify(client, times(2)).connect(TelemetryDiagnosticControls.DIAGNOSTIC_CHANNEL_CONNECTION_STRING);
+   }
+
+
+
+   @Test
+   public void configuresClient() {
+      when(client.getOnlineStatus()).thenReturn(true);
+      when(client.getVersion()).thenReturn("VER#");
+      controls.checkTransmission(true);
+
+      ArgumentCaptor<ClientConfiguration> configCaptor =
+          ArgumentCaptor.forClass(ClientConfiguration.class); // capcana
+
+      verify(client).configure(configCaptor.capture());
+
+      ClientConfiguration configuDatDinProd = configCaptor.getValue();
+
+//      assertEquals(now(), configuDatDinProd.getSessionStart()); // nu merge ca sunt cateva mili intre
+
+
+      assertThat(configuDatDinProd.getAckMode()).isEqualTo(AckMode.NORMAL);
+      assertThat(configuDatDinProd.getSessionStart()).isNotNull(); // cel mai des
+      assertThat(configuDatDinProd.getSessionStart()).isCloseTo(now(), within(1, MINUTES));
+
+//      assertTrue(  configuDatDinProd.getSessionId().startsWith("xVER#-")   );
+
+      assertThat(configuDatDinProd.getSessionId())
+          .startsWith("VER#-") // un mesaj de failure FRUMOS
+//          .hasSize() // optional
+          ;
+
    }
 
 
