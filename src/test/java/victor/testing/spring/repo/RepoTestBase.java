90e1b9938b769f7a9967b2e11ef996f779ffd042
Victor Rentea <victorrentea@gmail.com>
2021-03-05 15:33:07 +0200

    Oracle WIP

--- src/test/java/victor/testing/spring/repo/RepoTestBase.java
+++ src/test/java/victor/testing/spring/repo/RepoTestBase.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.TestPropertySourceUtils;
 import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.OracleContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import victor.testing.spring.domain.Supplier;
 import victor.testing.spring.repo.RepoTestBase.DockerPostgreDataSourceInitializer;
@@ -31,11 +31,12 @@ public abstract class RepoTestBase {
 
 
 
-
-   public static PostgreSQLContainer<?> postgreDBContainer = new PostgreSQLContainer<>("postgres:9.4");
+   public static OracleContainer oracle = new OracleContainer("oracle/database");
+//   public static PostgreSQLContainer<?> postgreDBContainer = new PostgreSQLContainer<>("postgres:9.4");
 
    static {
-      postgreDBContainer.start();
+//      postgreDBContainer.start();
+      oracle.start();
    }
 
    public static class DockerPostgreDataSourceInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
@@ -45,11 +46,11 @@ public abstract class RepoTestBase {
 
          TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
              applicationContext,
-             "spring.datasource.url=" + insertP6Spy(postgreDBContainer.getJdbcUrl()),
-             "spring.datasource.username=" + postgreDBContainer.getUsername(),
+             "spring.datasource.url=" + insertP6Spy(oracle.getJdbcUrl()),
+             "spring.datasource.username=" + oracle.getUsername(),
 //                "spring.datasource.driver-class-name=org.postgresql.Driver",
              "spring.datasource.driver-class-name=com.p6spy.engine.spy.P6SpyDriver",
-             "spring.datasource.password=" + postgreDBContainer.getPassword()
+             "spring.datasource.password=" + oracle.getPassword()
          );
       }
    }
