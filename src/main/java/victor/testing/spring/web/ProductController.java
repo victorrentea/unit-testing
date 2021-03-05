f769c5bb21dfe41ac6393ed2a5b3c840b704dae1
Victor Rentea <victorrentea@gmail.com>
2021-03-05 13:03:44 +0200

    Pt @Diana

--- src/main/java/victor/testing/spring/web/ProductController.java
+++ src/main/java/victor/testing/spring/web/ProductController.java
@@ -2,6 +2,10 @@ package victor.testing.spring.web;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +14,7 @@ import victor.testing.spring.web.dto.ProductSearchCriteria;
 import victor.testing.spring.web.dto.ProductSearchResult;
 import victor.testing.spring.web.dto.ProductDto;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -24,8 +29,12 @@ public class ProductController {
       return facade.createProduct(productDto);
    }
 
+//   @PreAuthorize("hasRole('ADMIN')")
    @PostMapping("product/search")
-   public List<ProductSearchResult> search(@RequestBody ProductSearchCriteria criteria) {
+   public List<ProductSearchResult> search(@RequestBody ProductSearchCriteria criteria/*,
+                                           @AuthenticationPrincipal Authentication principal*/) {
+      System.out.println("Current user:" + 		SecurityContextHolder.getContext().getAuthentication().getName());
+//      System.out.println("Current user : " + principal.getName());
       return facade.searchProduct(criteria);
    }
 
--- src/test/java/victor/testing/spring/web/ProductMvcTest.java
+++ src/test/java/victor/testing/spring/web/ProductMvcTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,6 +32,7 @@ public class ProductMvcTest {
     @Autowired
     private ProductRepo productRepo;
 
+    @WithMockUser(username = "gigi"/*,roles = "ADMIN"*/)
     @Test
     public void testSearch() throws Exception {
         productRepo.save(new Product().setName("Tree"));
