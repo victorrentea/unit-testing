e86f6c3984b56905647b3697c602bbee4e156d4a
Victor Rentea <victorrentea@gmail.com>
2021-04-15 17:55:42 +0300

    back on java 8

--- src/main/java/victor/testing/tdd/TennisScore.java
+++ src/main/java/victor/testing/tdd/TennisScore.java
@@ -7,31 +7,23 @@ public class TennisScore {
 
    private int homePlayerScore;
    private int awayPlayerScore;
-   private static final Map<Integer, String > scoreMap = new HashMap<>();
-   static {
-      scoreMap.put(1, "Fifteen");
-   }
 
    public String getScore() {
       return translate(homePlayerScore) + " - " + translate(awayPlayerScore);
    }
    public String translate(int score) {
-
-      Player someEnum = Player.HOME;
-
-      int v = switch (someEnum) {
-         case AWAY -> 1;
-         case HOME -> 2;
-      };
-
-//      return scoreMap.getOrget(score);
-      return switch (score) {
-         case 0 -> "Love";
-         case 1 -> "Fifteen";
-         case 2 -> "Thirty";
-         default -> throw new IllegalStateException("Unexpected value: " + score);
-      };
+      switch (score) {
+         case 0:
+            return "Love";
+         case 1:
+            return "Fifteen";
+         case 2:
+            return "Thirty";
+         default:
+            throw new IllegalStateException("Unexpected value: " + score);
+      }
    }
+
 
    public void addPoint(Player player) {
       if (player == Player.AWAY) {
