bc57a102a77c2f2aa29b0013261e2337ca8e724b
Victor Rentea <victorrentea@gmail.com>
2021-06-03 10:39:06 +0300

    Validator Test + fluent setters

--- src/main/java/victor/testing/builder/CustomerValidator.java
+++ src/main/java/victor/testing/builder/CustomerValidator.java
@@ -1,8 +1,14 @@
 package victor.testing.builder;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
+@Service
 public class CustomerValidator {
+
 
 	public void validate(Customer customer) {
 		if (isBlank(customer.getName())) {
---
+++ src/test/java/victor/testing/builder/CustomerValidatorTest.java
@@ -0,0 +1,17 @@
+package victor.testing.builder;
+
+import org.junit.Test;
+
+public class CustomerValidatorTest {
+
+   private CustomerValidator validator = new CustomerValidator();
+
+   @Test
+   public void yesSir() {
+      validator.validate(new Customer()
+          .setName("John")
+          .setAddress(new Address()
+				 .setCity("Bucuresti. Orasul smogului.")));
+   }
+
+}
