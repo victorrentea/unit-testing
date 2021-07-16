ac5308b600102cd4b88cb7ef8cd7b2646ff3e3f8
Victor Rentea <victorrentea@gmail.com>
2021-07-16 13:21:01 +0300

    Mock Bean + @Transactional @Test

--- src/test/java/victor/testing/spring/repo/ProductRepoTest.java
+++ src/test/java/victor/testing/spring/repo/ProductRepoTest.java
@@ -2,33 +2,50 @@ package victor.testing.spring.repo;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.annotation.DirtiesContext.MethodMode;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.web.dto.ProductSearchCriteria;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+//@RunWith(SpringRunner.class)  JUNIT 4
+
 @SpringBootTest
 @ActiveProfiles("db-mem")
+@Transactional
+//@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD) // GRESEALA pt ca adauga ~ + 20~ sec / test
+// criminal act de comis pe GIT > incetineste Jenkins. >
 public class ProductRepoTest {
     @Autowired
     private ProductRepo repo;
 
     private ProductSearchCriteria criteria = new ProductSearchCriteria();
 
+//    @BeforeEach
+//    public final void before() {
+//        repo.deleteAll();
+//    }
     @Test
+//    @DirtiesContext(methodMode = MethodMode.BEFORE_METHOD) // GRESEALA pt ca adauga ~ + 20~ sec / test
     public void noCriteria() {
         repo.save(new Product("A"));
-        Assertions.assertThat(repo.search(criteria)).hasSize(1); // org.assertj:assertj-core:3.16.1
+        assertThat(repo.search(criteria)).hasSize(1); // org.assertj:assertj-core:3.16.1
     }
     @Test
     public void noCriteriaBis() {
         repo.save(new Product("B"));
-        Assertions.assertThat(repo.search(criteria)).hasSize(1); // org.assertj:assertj-core:3.16.1
+        assertThat(repo.search(criteria)).hasSize(1); // org.assertj:assertj-core:3.16.1
     }
 
 }
--- src/test/java/victor/testing/spring/service/ProductServiceClientMockTest.java
+++ src/test/java/victor/testing/spring/service/ProductServiceClientMockTest.java
@@ -6,7 +6,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.domain.ProductCategory;
 import victor.testing.spring.domain.Supplier;
@@ -25,16 +30,18 @@ import static org.assertj.core.api.Assertions.byLessThan;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+//@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@ActiveProfiles("db-mem")
 public class ProductServiceClientMockTest {
-   @Mock
+   @MockBean
    public SafetyClient mockSafetyClient;
-   @Mock
-   private ProductRepo productRepo;
-   @Mock
+   @Autowired
+   private ProductRepo productRepo; //Mockito.mock(ProductRepo.class)
+   @Autowired
    private SupplierRepo supplierRepo;
-   @InjectMocks
-   private ProductService productService;
+   @Autowired
+   private ProductService productService; // = new ProductService(mock1, mock  mock ...)
 
    @Test
    public void createThrowsForUnsafeProduct() {
@@ -46,20 +53,18 @@ public class ProductServiceClientMockTest {
 
    @Test
    public void createOk() {
-      Supplier supplier = new Supplier().setId(13L);
-      when(supplierRepo.getOne(supplier.getId())).thenReturn(supplier);
+//      Supplier supplier = new Supplier().setId(13L);
+//      when(supplierRepo.getOne(supplier.getId())).thenReturn(supplier);
+      Long supplierId = supplierRepo.save(new Supplier("oarecare")).getId();
       when(mockSafetyClient.isSafe("safebar")).thenReturn(true);
 
-      productService.createProduct(new ProductDto("name", "safebar", supplier.getId(), ProductCategory.HOME));
+      productService.createProduct(new ProductDto("name", "safebar", supplierId, ProductCategory.HOME));
 
-      // Yuck!
-      ArgumentCaptor<Product> productCaptor = ArgumentCaptor.forClass(Product.class);
-      verify(productRepo).save(productCaptor.capture());
-      Product product = productCaptor.getValue();
+      Product product = productRepo.findAll().get(0);
 
       assertThat(product.getName()).isEqualTo("name");
       assertThat(product.getBarcode()).isEqualTo("safebar");
-      assertThat(product.getSupplier().getId()).isEqualTo(supplier.getId());
+      assertThat(product.getSupplier().getId()).isEqualTo(supplierId);
       assertThat(product.getCategory()).isEqualTo(ProductCategory.HOME);
       assertThat(product.getCreateDate()).isNotNull();
       assertThat(product.getCreateDate()).isCloseTo(now(), byLessThan(1, SECONDS));
