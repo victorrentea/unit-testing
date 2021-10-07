d855715594cbad09adb0a0986506f4a2631c2904
Victor Rentea <victorrentea@gmail.com>
2021-10-07 10:58:24 +0300

    start

---
+++ src/test/java/victor/testing/tdd/TennisGameTest.java
@@ -0,0 +1,9 @@
+package victor.testing.tdd;
+
+public class TennisGameTest {
+
+
+   // The running score of each game is described in a manner peculiar to tennis:
+   // scores from zero to three points are described as
+   // “Love”, “Fifteen”, “Thirty”, and “Forty” respectively.
+}
