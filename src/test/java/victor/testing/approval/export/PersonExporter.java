4cda20fc0212a3b88b360766ff7b1ec380aaf11b
Victor Rentea <victorrentea@gmail.com>
2021-10-20 12:35:53 +0300

    undo approval

--- src/test/java/victor/testing/approval/export/PersonExporter.java
+++ src/test/java/victor/testing/approval/export/PersonExporter.java
@@ -2,12 +2,8 @@ package victor.testing.approval.export;
 
 import java.io.IOException;
 import java.io.Writer;
-import java.time.format.DateTimeFormatter;
-
-import static java.time.format.DateTimeFormatter.ofPattern;
 
 public class PersonExporter {
-   private static final DateTimeFormatter BIRTH_DATE_FORMAT = ofPattern("dd MMM yyyy");
    private final PersonRepo personRepo;
 
    public PersonExporter(PersonRepo personRepo) {
@@ -19,9 +15,9 @@ public class PersonExporter {
       for (Person person : personRepo.findAll()) {
          writer.write(person.getFirstName() + " " + person.getLastName().toUpperCase());
          writer.write(";");
-         writer.write(String.join(",", person.getPhoneList()));
+         writer.write(String.join(",", person.getPhoneList())); // TODO change delimiter
          writer.write(";");
-         writer.write(person.getBirthDate().format(BIRTH_DATE_FORMAT));
+         writer.write(person.getBirthDate().toString()); // TODO change format to 12 Nov 2021
          writer.write("\n");
       }
    }
