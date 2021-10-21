d6051121de396fcaf20cca243f49f5876123dae6
Victor Rentea <victorrentea@gmail.com>
2021-10-21 14:25:48 +0300

    cam alambicat testu asta ... hai cu fake

--- src/test/java/victor/testing/mocks/fake/FeedProcessorTest.java
+++ src/test/java/victor/testing/mocks/fake/FeedProcessorTest.java
@@ -6,7 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,21 +22,25 @@ public class FeedProcessorTest {
 
    @Test
    public void oneFileWithOneLine() {
-      when(fileRepoMock.getFileNames()).thenReturn(Arrays.asList("one.txt"));
+      when(fileRepoMock.getFileNames()).thenReturn(List.of("one.txt"));
       when(fileRepoMock.openFile("one.txt")).thenReturn(Stream.of("one"));
       assertThat(feedProcessor.countPendingLines()).isEqualTo(1);
    }
 
    @Test
    public void oneFileWith2Lines() {
-      when(fileRepoMock.getFileNames()).thenReturn(Arrays.asList("two.txt"));
+      when(fileRepoMock.getFileNames()).thenReturn(List.of("two.txt"));
       when(fileRepoMock.openFile("two.txt")).thenReturn(Stream.of("one","two"));
       assertThat(feedProcessor.countPendingLines()).isEqualTo(2);
    }
 
    @Test
    public void twoFilesWith3Lines() {
-      // TODO
+      when(fileRepoMock.getFileNames()).thenReturn(List.of("one.txt", "two.txt"));
+      when(fileRepoMock.openFile("one.txt")).thenReturn(Stream.of("line1"));
+      when(fileRepoMock.openFile("two.txt")).thenReturn(Stream.of("line2", "line3"));
+
+      assertThat(feedProcessor.countPendingLines()).isEqualTo(3);
 
       // TODO How to DRY the tests?
    }
