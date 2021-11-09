fed2707df020eb4062733ded0c4fbd7c412b2af3
Victor Rentea <victorrentea@gmail.com>
2021-11-09 14:03:22 +0200

    Regandit responsabilitatile cand pui mockuri foarte multe in testul tau.

--- src/test/java/victor/testing/mocks/overspecifying/ParcelFacadeTest.java
+++ src/test/java/victor/testing/mocks/overspecifying/ParcelFacadeTest.java
@@ -5,19 +5,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import victor.testing.mocks.overspecifying.repo.ParcelRepo;
-import victor.testing.mocks.overspecifying.repo.TrackingProviderRepo;
-import victor.testing.mocks.overspecifying.service.DisplayService;
 import victor.testing.mocks.overspecifying.model.Parcel;
+import victor.testing.mocks.overspecifying.repo.ParcelRepo;
+import victor.testing.mocks.overspecifying.service.DisplayService;
 import victor.testing.mocks.overspecifying.service.ParcelFacade;
 import victor.testing.mocks.overspecifying.service.PlatformService;
-import victor.testing.mocks.overspecifying.model.TrackingProvider;
 import victor.testing.mocks.overspecifying.service.TrackingService;
 
-import java.util.List;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ParcelFacadeTest {
@@ -29,8 +24,6 @@ class ParcelFacadeTest {
    PlatformService platformService;
    @Mock
    TrackingService trackingService;
-   @Mock
-   TrackingProviderRepo trackingProviderRepo;
    @InjectMocks
    ParcelFacade target;
 
@@ -41,14 +34,11 @@ class ParcelFacadeTest {
           .setAwb("AWB")
           .setPartOfCompositeShipment(true);
       when(parcelRepo.findByBarcode("BAR")).thenReturn(parcel);
-      List<TrackingProvider> trackingProviders = List.of(new TrackingProvider());
-      when(trackingProviderRepo.findByAwb("AWB")).thenReturn(trackingProviders);
 
       target.processBarcode("BAR", 99);
 
-      verify(displayService).displayAWB("AWB");
-      verify(displayService).displayMultiParcelWarning();
+      verify(displayService).displayAWB(parcel);
       verify(platformService).addParcel(parcel);
-      verify(trackingService).markDepartingWarehouse("AWB", 99, trackingProviders);
+      verify(trackingService).markDepartingWarehouse("AWB", 99);
    }
 }
