546bcdfca6f42db3e174854df4c55704f2f330fc
Victor Rentea <victorrentea@gmail.com>
2021-11-09 15:25:23 +0200

    compile

--- src/test/java/victor/testing/spring/repo/ProductRepoTest.java
+++ src/test/java/victor/testing/spring/repo/ProductRepoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 import victor.testing.spring.web.dto.ProductSearchCriteria;
 
 @Slf4j
