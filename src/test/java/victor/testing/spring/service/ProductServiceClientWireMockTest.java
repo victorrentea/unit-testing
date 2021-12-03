6613b0a1a3af81b65a2f0b6983def3a42e480454
Victor Rentea <victorrentea@gmail.com>
2021-12-03 17:00:28 +0200

    Wiremock

--- src/test/java/victor/testing/spring/service/ProductServiceClientWireMockTest.java
+++ src/test/java/victor/testing/spring/service/ProductServiceClientWireMockTest.java
@@ -1,104 +1,64 @@
 package victor.testing.spring.service;
 
+import org.assertj.core.api.AutoCloseableSoftAssertions;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.domain.ProductCategory;
 import victor.testing.spring.domain.Supplier;
-import victor.testing.spring.infra.SafetyClient;
 import victor.testing.spring.repo.ProductRepo;
 import victor.testing.spring.repo.SupplierRepo;
-import victor.testing.tools.WireMockExtension;
 import victor.testing.spring.web.dto.ProductDto;
+import victor.testing.tools.WireMockExtension;
 
-import java.time.Clock;
-import java.time.Instant;
-import java.time.ZoneId;
+import static java.time.LocalDateTime.now;
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static org.assertj.core.api.Assertions.byLessThan;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest(properties = "safety.service.url.base=http://localhost:8089")
-@ActiveProfiles("db-mem")
 @Transactional
+@SpringBootTest(properties = "safety.service.url.base=http://localhost:9999")
+@ActiveProfiles("db-mem") // CONNECT BY  ;  /*+ hints in some SQL */  DBMS.STD_FCT_FROM_ORACLE
 public class ProductServiceClientWireMockTest {
-   @Autowired
-   public SafetyClient mockSafetyClient;
    @Autowired
    private ProductRepo productRepo;
    @Autowired
    private SupplierRepo supplierRepo;
    @Autowired
    private ProductService productService;
-
    @RegisterExtension
-   public WireMockExtension wireMock = new WireMockExtension(8089);
-   @Autowired
-   private CacheManager cacheManager;
-
-   @BeforeEach
-   public void initialize() {
-      // Clear manually all caches
-      cacheManager.getCacheNames().stream().map(cacheManager::getCache).forEach(Cache::clear);
-   }
+   public  WireMockExtension wireMock = new WireMockExtension(9999);
 
    @Test
-   public void throwsForUnsafeProduct() {
+   public void createThrowsForUnsafeProduct() {
       Assertions.assertThrows(IllegalStateException.class, () -> {
-         productService.createProduct(new ProductDto("name", "bar", -1L, ProductCategory.HOME));
+//         when(mockSafetyClient.isSafe("bar")).thenReturn(false);
+         productService.createProduct(new ProductDto("name", "bar",-1L, ProductCategory.HOME));
       });
    }
 
    @Test
-   public void throwsForUnsafeProductProgrammaticWireMock() {
-//      Assertions.assertThrows(IllegalStateException.class, () -> {
-//         String template;
-//         try (FileReader reader = new FileReader("C:\\workspace\\integration-testing-spring\\src\\test\\java\\victor\\testing\\spring\\facade\\inTemplate.json")) {
-//            template = IOUtils.toString(reader);
-//         }
-//         WireMock.stubFor(get(urlEqualTo("/product/customXX/safety"))
-//             .willReturn(aResponse()
-//                 .withStatus(200)
-//                 .withHeader("Content-Type", "application/json")
-//                 .withBody("{\"entries\": [{\"category\": \"DETERMINED\",\"detailsUrl\": \"http://wikipedia.com\"}]}"))); // override
-//
-//         productService.createProduct(new ProductDto("name", "customXX", -1L, ProductCategory.HOME));
-//      });
-   }
+   public void createOk() {
+      Supplier supplier = supplierRepo.save(new Supplier());
 
-   @Test
-   public void fullOk() {
-      long supplierId = supplierRepo.save(new Supplier()).getId();
+//      when(mockSafetyClient.isSafe("safebar")).thenReturn(true);
 
-      ProductDto dto = new ProductDto("name", "safebar", supplierId, ProductCategory.HOME);
-      productService.createProduct(dto);
+      productService.createProduct(new ProductDto("name", "safebar", supplier.getId(), ProductCategory.HOME));
 
+      org.assertj.core.api.Assertions.assertThat(productRepo.count()).isEqualTo(1);
       Product product = productRepo.findAll().get(0);
 
-      assertThat(product.getName()).isEqualTo("name");
-      assertThat(product.getBarcode()).isEqualTo("safebar");
-      assertThat(product.getSupplier().getId()).isEqualTo(supplierId);
-      assertThat(product.getCategory()).isEqualTo(ProductCategory.HOME);
-
-      assertThat(product.getCreateDate()); // TODO test time
-   }
-
-   @TestConfiguration
-   public static class TestConfig {
-      @Bean
-      @Primary
-      public Clock clockFixed() {
-         return Clock.fixed(Instant.parse("2014-12-22T10:15:30.00Z"), ZoneId.systemDefault());
+      try (AutoCloseableSoftAssertions softly = new AutoCloseableSoftAssertions()) {
+         softly.assertThat(product.getName()).isEqualTo("name");
+         softly.assertThat(product.getBarcode()).isEqualTo("safebar");
+         softly.assertThat(product.getSupplier().getId()).isEqualTo(supplier.getId());
+         softly.assertThat(product.getCategory()).isEqualTo(ProductCategory.HOME);
+         softly.assertThat(product.getCreateDate()).isNotNull();
+         softly.assertThat(product.getCreateDate()).isCloseTo(now(), byLessThan(5, SECONDS));
       }
    }
 
