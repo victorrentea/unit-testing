8392efea2ab6ff5b4e8fff004d482385c057604c
Victor Rentea <victorrentea@gmail.com>
2021-12-17 12:07:09 +0200

    CEL MAI BUN mod de a testa cod ce vb cu baza relationala : @Transactional

--- src/test/java/victor/testing/spring/repo/ProductRepoTest.java
+++ src/test/java/victor/testing/spring/repo/ProductRepoTest.java
@@ -1,11 +1,10 @@
 package victor.testing.spring.repo;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.web.dto.ProductSearchCriteria;
 import victor.testing.spring.web.dto.ProductSearchResult;
@@ -15,7 +14,10 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@ActiveProfiles("db-mem")
+//@ActiveProfiles("db-mem")
+//@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD) // - NU are voie sa ajunga comisa pe GIT. O folosesti doar pe local sa faci de bugging: adica: cu @DC mai crapa testul asta rulat cu fratii ? daca NU -> leaking state a fost legata de spring: 95% - ceva ramas in DB din mem 5% state prin singleton sau cacheuri
+
+@Transactional // >>>> RUPE ! 99% din testele relationale trebuie scrise asa
 public class ProductRepoTest {
 
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(ProductRepoTest.class);
@@ -25,12 +27,14 @@ public class ProductRepoTest {
 
     private ProductSearchCriteria criteria = new ProductSearchCriteria();
 
-    @BeforeEach
-    final void before() {
-        repo.deleteAll(); // solutie foarte ok pt a curata orice resurse ramane murdare
-        // (mai putin baze relationale , pt care avem o solutie mult mai buna)
-    }
+//    @BeforeEach
+//    final void before() {
+//        repo.deleteAll(); // solutie foarte ok pt a curata orice resurse ramane murdare
+//        // (mai putin baze relationale , pt care avem o solutie mult mai buna)
+//    }
     @Test
+    // @Commit -- il pui doar ca sa vezi ceva date comituite intr-o
+    // baza fizica cand faci debug
     public void noCriteria() throws InterruptedException {
         repo.save(new Product("A"));
         List<ProductSearchResult> results = repo.search(criteria);
