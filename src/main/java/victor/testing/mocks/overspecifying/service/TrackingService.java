effde63abb561dfd11b22ac21b24d9149d9df538
Victor Rentea <victorrentea@gmail.com>
2022-02-08 11:20:03 +0200

    comiplatiion :P

--- src/main/java/victor/testing/mocks/overspecifying/service/TrackingService.java
+++ src/main/java/victor/testing/mocks/overspecifying/service/TrackingService.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class TrackingService {
    private final TrackingProviderRepo trackingProviderRepo;
    public void markDepartingWarehouse(String awb, int warehouseId) {
-      List<TrackingProvider> trackingProviders = trackingProviderRepo.findByAwb(parcel.getAwb());
+      List<TrackingProvider> trackingProviders = trackingProviderRepo.findByAwb(awb);
       for (TrackingProvider trackingProvider : trackingProviders) {
          System.out.println("Report "+awb+" departing warehouse " + warehouseId
                             + " to " + trackingProvider.getId());
