adc94070d63585deff74b3ec7ece1041998a10d7
Victor Rentea <victorrentea@gmail.com>
2022-03-03 12:56:28 +0200

    Prep Talk

---
+++ src/main/java/victor/testing/designhints/cqs/Target.java
@@ -0,0 +1,28 @@
+package victor.testing.designhints.cqs;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Target {
+   private final Dependency dependency;
+
+   public void testedMethod(Obj obj) {
+      // logic to test
+      int x = dependency.stuff(obj, 5);
+      System.out.println("Logic with " + x);
+   }
+}
+
+class Dependency {
+   public int stuff(Obj obj, int x) {
+      // imagine complexity => tested separately
+      obj.setTotal(obj.getTotal() + x); // side effect ==> command
+      return x * 2; // returns ==> query
+   }
+}
+
+@Data
+class Obj {
+   private int total;
+}
