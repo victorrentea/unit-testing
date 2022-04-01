2f6784894c838f33da046ffc268c9a6e8f7ce2d4
Victor Rentea <victorrentea@gmail.com>
2022-04-01 12:25:38 +0300

    tests that only look at the data THEY inserted (could be run even in parallel)

---
+++ src/test/java/victor/testing/spring/repo/sub/ProductRepo2Test.java
@@ -0,0 +1,31 @@
+package victor.testing.spring.repo.sub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import victor.testing.spring.domain.Product;
+import victor.testing.spring.repo.ProductRepo;
+import victor.testing.spring.web.dto.ProductSearchCriteria;
+import victor.testing.spring.web.dto.ProductSearchResult;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles({"db-mem","in-mem-kafka"})
+public class ProductRepo2Test {
+   @Autowired
+   ProductRepo repo;
+
+   ProductSearchCriteria criteria = new ProductSearchCriteria();
+
+   @Test
+   public void noCriteria() {
+      repo.save(new Product("A"));
+      List<ProductSearchResult> results = repo.search(criteria);
+      assertThat(results).hasSize(1);
+   }
+}
+
--- src/test/java/victor/testing/spring/repo/sub/ProductRepoTest.java
+++ src/test/java/victor/testing/spring/repo/sub/ProductRepoTest.java
@@ -1,11 +1,11 @@
-package victor.testing.spring.repo;
+package victor.testing.spring.repo.sub;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import victor.testing.spring.domain.Product;
+import victor.testing.spring.repo.ProductRepo;
 import victor.testing.spring.web.dto.ProductSearchCriteria;
 import victor.testing.spring.web.dto.ProductSearchResult;
 
@@ -15,29 +15,46 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles({"db-mem","in-mem-kafka"})
+// TODO
+//@Sql(value = "classpath:/sql/common-reference-data.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 public class ProductRepoTest {
    @Autowired
    ProductRepo repo;
 
    ProductSearchCriteria criteria = new ProductSearchCriteria();
 
-   @AfterEach
-   final void cleanDB() {
-      repo.deleteAll();
-   }
+//   @BeforeAll TODO
+//   public void beforeTheWholeClass() {
+////      insert heavy
+//   }
+//   @AfterAll
+//   public void afterTheWholeClass() {
+////      DELETE heavy
+//   }
+//   @BeforeEach // before not after because we do not trust that others cleaned after them : we clean what we need to clean
+//   final void cleanDB() {
+//      repo.deleteAll();
+//   }
 
    @Test
    public void noCriteria() {
-      repo.save(new Product("A"));
+      Product product = new Product("A");
+      repo.save(product);
       List<ProductSearchResult> results = repo.search(criteria);
-      assertThat(results).hasSize(1);
+//      assertThat(results).hasSize(1);
+      assertThat(results.stream())
+          .anyMatch(p -> product.getId().equals(p.getId()))
+//          .hasSize(1)
+      ;
    }
 
    @Test
    public void noCriteriaBis() { // not isolated tests
       repo.save(new Product("B"));
       List<ProductSearchResult> results = repo.search(criteria);
-      assertThat(results).hasSize(1);
+      assertThat(results)
+          .anyMatch(p -> "B".equals(p.getName()))
+      ;
    }
 }
 
