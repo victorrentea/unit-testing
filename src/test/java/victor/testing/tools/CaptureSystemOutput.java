2bf48fb5eeeeae5f39d12d5a703ea1d165e7b377
Victor Rentea <victorrentea@gmail.com>
2022-05-03 21:25:08 +0300

    compacting

--- src/test/java/victor/testing/tools/CaptureSystemOutput.java
+++ src/test/java/victor/testing/tools/CaptureSystemOutput.java
@@ -17,7 +17,10 @@
 package victor.testing.tools;
 
 import org.hamcrest.Matcher;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.*;
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+import org.junit.jupiter.api.extension.ExtensionContext.Store;
+import victor.testing.tools.CaptureSystemOutput.Extension;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -32,6 +35,8 @@ import java.util.List;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
 
 /**
  * {@code @CaptureSystemOutput} is a JUnit JUpiter extension for capturing
@@ -69,8 +74,63 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Target({ TYPE, METHOD })
 @Retention(RUNTIME)
 @Documented
-@ExtendWith(CaptureSystemOutputExtension.class)
+@ExtendWith(Extension.class)
 public @interface   CaptureSystemOutput {
+
+
+    /**
+     * JUnit Jupiter extension for capturing output to {@code System.out} and
+     * {@code System.err} with expectations supported via Hamcrest matchers.
+     *
+     * <p>Based on code from Spring Boot's
+     * <a href="https://github.com/spring-projects/spring-boot/blob/d3c34ee3d1bfd3db4a98678c524e145ef9bca51c/spring-boot-project/spring-boot-tools/spring-boot-test-support/src/main/java/org/springframework/boot/testsupport/rule/OutputCapture.java">OutputCapture</a>
+     * rule for JUnit 4 by Phillip Webb and Andy Wilkinson.
+     *
+     * @author Sam Brannen
+     * @author Phillip Webb
+     * @author Andy Wilkinson
+     */
+    class Extension implements BeforeEachCallback, AfterEachCallback, ParameterResolver {
+
+        @Override
+        public void beforeEach(ExtensionContext context) throws Exception {
+            getOutputCapture(context).captureOutput();
+        }
+
+        @Override
+        public void afterEach(ExtensionContext context) throws Exception {
+            OutputCapture outputCapture = getOutputCapture(context);
+            try {
+                if (!outputCapture.matchers.isEmpty()) {
+                    String output = outputCapture.toString();
+                    assertThat(output, allOf(outputCapture.matchers));
+                }
+            }
+            finally {
+                outputCapture.releaseOutput();
+            }
+        }
+
+        @Override
+        public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+            boolean isTestMethodLevel = extensionContext.getTestMethod().isPresent();
+            boolean isOutputCapture = parameterContext.getParameter().getType() == OutputCapture.class;
+            return isTestMethodLevel && isOutputCapture;
+        }
+
+        @Override
+        public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+            return getOutputCapture(extensionContext);
+        }
+
+        private OutputCapture getOutputCapture(ExtensionContext context) {
+            return getStore(context).getOrComputeIfAbsent(OutputCapture.class);
+        }
+
+        private Store getStore(ExtensionContext context) {
+            return context.getStore(Namespace.create(getClass(), context.getRequiredTestMethod()));
+        }
+    }
 
     /**
      * {@code OutputCapture} captures output to {@code System.out} and {@code System.err}.
