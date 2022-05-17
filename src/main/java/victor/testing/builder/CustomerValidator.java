00dbfaa1d78211f1bebd4c4fc8910ce70f757152
victorrentea <victorrentea@gmail.com>
2022-05-17 15:00:22 +0300

    fix unit tests

--- src/main/java/victor/testing/builder/CustomerValidator.java
+++ src/main/java/victor/testing/builder/CustomerValidator.java
@@ -19,7 +19,7 @@ public class CustomerValidator {
 			throw new IllegalArgumentException("Missing address city");
 		}
 		address.setCity(address.getCity().trim()); // mutate this
-		if (address.getCity().length() < 3) {
+		if (address.getCity().length() < 3) { //.. or swap the ifs
 			throw new IllegalArgumentException("Address city too short");
 		}
 	}
--- src/test/java/victor/testing/designhints/signatures/PreciseTest.java
+++ src/test/java/victor/testing/designhints/signatures/PreciseTest.java
@@ -15,6 +15,6 @@ class PreciseTest {
 
       Precise.sendSprintFinishedEmail(project);
 
-      assertThat(outputCapture.toString()).isEqualTo("Sending email to boss@my.corp with subject 'Sprint Finished' and some body");
+      assertThat(outputCapture.toString()).isEqualToIgnoringNewLines("Sending email to boss@my.corp with subject 'Sprint Finished' and some body");
    }
 }
