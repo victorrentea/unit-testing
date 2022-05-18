3279faa43e914660d85411ef7357f63f1d1eceaf
victorrentea <victorrentea@gmail.com>
2022-05-18 11:34:01 +0300

    Spares implemented (rule #2)

--- src/test/java/victor/testing/tdd/bowling/Game.java
+++ src/test/java/victor/testing/tdd/bowling/Game.java
@@ -1,16 +1,42 @@
 package victor.testing.tdd.bowling;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class Game {
 
-    // TODO array.
-    private int total;
+    private final List<Integer> rolls = new ArrayList<>();
 
     public void roll(int pins) {
-        total += pins;
+        rolls.add(pins);
     }
 
     public int score() {
-        return total;
+        int bonusForSpares = 0;
+        for (int i = 0; i < rolls.size(); i+=2) {
+            int pinsInFrame = rolls.get(i)  + rolls.get(i+1);
+            if (pinsInFrame == 10) {
+                bonusForSpares += rolls.get(i + 2);
+            }
+        }
+        return bonusForSpares + rolls.stream().mapToInt(i->i).sum();
     }
-
 }
+
+// if Frame were part of the API, then make it immutable.
+//record Frame(int roll1, Optional<Integer> roll2, Optional<Integer> roll3){}
+
+//class Frame{
+//    private final int roll1;
+//    private Integer roll2;
+//
+//    Frame(int roll1) {
+//        this.roll1 = roll1;
+//    }
+//
+//    public void setRoll2(Integer roll2) {
+//        this.roll2 = roll2;
+//    }
+//
+//}
--- src/test/java/victor/testing/tdd/bowling/GameTest.java
+++ src/test/java/victor/testing/tdd/bowling/GameTest.java
@@ -1,5 +1,6 @@
 package victor.testing.tdd.bowling;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,7 +18,8 @@ import static org.assertj.core.api.Assertions.assertThat;
  * A strike is when the player knocks down all 10 pins on his first roll.
  * The frame is then completed with a single roll. The bonus for that frame is the value of the next two rolls.
  *
- * In the tenth frame a player who rolls a spare or strike is allowed to roll the extra balls to complete the frame. However no more than three balls can be rolled in tenth frame.
+ * In the tenth frame a player who rolls a spare or strike is allowed to roll the extra balls to complete the frame.
+ * However no more than three balls can be rolled in tenth frame.
  *
  */
 
@@ -25,41 +27,56 @@ public class GameTest {
     Game game = new Game();
     @Test
     void beginnersGame() {
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        rollZeros(20);
 
         assertThat(game.score()).isEqualTo(0);
     }
     @Test
-    void returns20_whenRolls1EachTime() {
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
+    void returnsSumOfRolls() {
+        game.roll(1);
+        game.roll(1);
+        rollZeros(18);
+
+        assertThat(game.score()).isEqualTo(2);
+    }
+    @Test
+    void spare() {
+        final int ROLL_AFTER_SPARE = 1;
+        game.roll(5);
+        game.roll(5);
+        game.roll(ROLL_AFTER_SPARE);
+        rollZeros(17);
+
+        assertThat(game.score()).isEqualTo(10 + ROLL_AFTER_SPARE + ROLL_AFTER_SPARE);
+    }
+    @Test
+    void notASpare() {
+        final int ROLL_AFTER_SPARE = 1;
+        game.roll(0);
+        game.roll(5);
+        game.roll(5);
+        game.roll(1);
+        rollZeros(16);
+
+        assertThat(game.score()).isEqualTo(11);
+    }
+
+    private void rollZeros(int x) {
+        for (int i = 0; i < x; i++) {
+            game.roll(0);
         }
-
-        assertThat(game.score()).isEqualTo(20);
     }
-    @Test
-    void spare() { // 5 5 5 1 1 ...
-        final int SPARE_BONUS = 1;
-        game.roll(5);
-        game.roll(5);
-        for (int i = 0; i < 18; i++) {
-            game.roll(1);
-        } // we can focus the test better ("test less")
-
-        assertThat(game.score()).isEqualTo(10 + 18 + SPARE_BONUS);
-    }
-    @Test
-    void justOneSpare() { // 5 5 5 1 1 1 1 ...
-        final int SPARE_BONUS = 5;
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        for (int i = 0; i < 17; i++) {
-            game.roll(1);
-        } // we can focus the test better.
-
-        assertThat(game.score()).isEqualTo(15 + 17 + SPARE_BONUS);
-    }
+//    @Test
+//    @Disabled("not there yet")
+//    void justOneSpare() { // 5 5 5 1 1 1 1 ...
+//        final int SPARE_BONUS = 5;
+//        game.roll(5);
+//        game.roll(5);
+//        game.roll(5);
+//        for (int i = 0; i < 17; i++) {
+//            game.roll(1);
+//        } // we can focus the test better.
+//
+//        assertThat(game.score()).isEqualTo(15 + 17 + SPARE_BONUS);
+//    }
 }
