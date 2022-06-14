12e1bde4ce862f99f774b3c40d091764ffee9cd2
victorrentea <victorrentea@gmail.com>
2022-06-14 12:09:54 +0300

    PICA pentru ca testul FifteenLove corupe datele din campul STATIC message. si testul urmator gasesete acolo aiurea datele

--- src/test/java/victor/testing/tdd/classic/TennisScoreTest.java
+++ src/test/java/victor/testing/tdd/classic/TennisScoreTest.java
@@ -5,21 +5,18 @@ import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TennisScoreTest {
     @BeforeEach
     final void before() {
 
     }
     @Test
-    @Order(0)
     void newGame() {
         String actual = TennisScore.getScore();
 
         String expected = "Love - Love";
         assertThat(actual).isEqualTo(expected);
     }
-    @Order(1)
     @Test
     void fifteenLove() {
         TennisScore.winPoint(0); // cum altfel pot spune "jucatorul 1"?
