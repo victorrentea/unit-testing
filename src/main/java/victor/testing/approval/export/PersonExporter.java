9ea33ac09ed71349f7856feea0af441d1d6163c3
victorrentea <victorrentea@gmail.com>
2022-06-14 15:59:02 +0300

    Characterization test + primele mockuri.

--- src/main/java/victor/testing/approval/export/PersonExporter.java
+++ src/main/java/victor/testing/approval/export/PersonExporter.java
@@ -1,7 +1,16 @@
 package victor.testing.approval.export;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.io.Writer;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PersonExporter {
    private final PersonRepo personRepo;
@@ -11,7 +20,7 @@ public class PersonExporter {
    }
 
    public void export(Writer writer) throws IOException {
-      writer.write("full_name;phones;birth_date\n");
+      writer.write("full_name;phones;birth_date\n"); // header
       for (Person person : personRepo.findAll()) {
          writer.write(person.getFirstName() + " " + person.getLastName().toUpperCase());
          writer.write(";");
@@ -21,5 +30,39 @@ public class PersonExporter {
          writer.write(person.getBirthDate().toString()); // TODO CR: change format to "12 Nov 2021"
          writer.write("\n");
       }
+   }
+}
+
+
+class SaNeJucam  {
+   // practic pe sub cand mockuiesti o metoda, Mockito subclaseaza clasa data si apoi te lasa sa ii inveti metodele ce sa faca, aprox ca mai jos:
+//      PersonRepo personRepoMock = new PersonRepo() {
+//         @Override
+//         public List<Person> findAll() {
+//            return List.of(person);
+//         }
+//      }
+
+   @Test
+   void explore() throws IOException {
+      // given
+      // Plan a (Ionut): inseram in DB date, rulam codul, apoi stergem datele. > test de integration : mai mult de munca
+      // Plan b (Victor): sa stabuim metoda #findAll sa intoarca ceea ce noi vrem > Mockuri
+      Person person = new Person(1L, "John", "Doe", LocalDate.now(), List.of("0720019564"));
+      PersonRepo personRepoMock = Mockito.mock(PersonRepo.class);
+      Mockito.when(personRepoMock.findAll()).thenReturn(List.of(person));
+      PersonExporter personExporter = new PersonExporter(personRepoMock);
+      StringWriter writer = new StringWriter();
+
+      // when (prod call)
+      personExporter.export(writer);
+
+      // then (asserts)
+      String output = writer.toString();
+      assertThat(output).isEqualTo("full_name;phones;birth_date\n" +
+                                   "John DOE;0720019564;2022-06-14\n"); // habar n-am ce produce,
+      // dar sigur e corect, ca e in prod de 4ani si nimeni nu s-a plans
+      // atunci vrei sa "CAPTUREZI" behavior curent al codului ca-n prod, ca sa te asiguri ca TU nu-l strici.
+      // >> Characterization Testing :
    }
 }
