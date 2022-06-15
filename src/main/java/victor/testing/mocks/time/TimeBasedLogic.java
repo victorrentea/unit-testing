ae1728f54b2e90a93997c73330de2b3acc6dd0e7
victorrentea <victorrentea@gmail.com>
2022-06-15 10:36:44 +0300

    Morala: ce-i static mai greu de mockuit.

--- src/main/java/victor/testing/mocks/time/TimeBasedLogic.java
+++ src/main/java/victor/testing/mocks/time/TimeBasedLogic.java
@@ -1,6 +1,7 @@
 package victor.testing.mocks.time;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -11,8 +12,26 @@ import java.util.List;
 public class TimeBasedLogic {
    private final OrderRepo orderRepo;
 
-   public boolean isFrequentBuyer(int customerId) {
-      LocalDate now = LocalDate.now();
+   // Optiunea 4: introduci o noua clasa numita TimeProvider a ta pe care apoi o @Mock @InjectMocks ca de obicei
+//   @Autowired
+//   private TimeProvider timeProvider;
+
+   public boolean  isFrequentBuyer(int customerId) {
+//      LocalDate now = timeProvider.getNow(); //apel al unei metode DE INSTANTA dintr-o clasa de-a mea pe care sunt liber sa o mockuiesc cum imi tuna
+      LocalDate now = LocalDate.now(); // cuplare cu timpul curent
+      LocalDate sevenDaysAgo = now.minusDays(7);
+
+      System.out.println("Run with now=" + now);
+      List<Order> recentOrders = orderRepo.findByCustomerIdAndCreatedOnBetween(customerId, sevenDaysAgo, now);
+
+      double totalAmount = recentOrders.stream().mapToDouble(Order::getTotalAmount).sum();
+      boolean anyGenius = recentOrders.stream().anyMatch(Order::isGenius);
+
+      return totalAmount > 100 || anyGenius;
+   }
+
+   public boolean  isFrequentBuyer_candTestuPute_designulProduluiPoateFiImbunatatit(int customerId, LocalDate now) {
+//      LocalDate now = LocalDate.now(); // cuplare cu timpul curent
       LocalDate sevenDaysAgo = now.minusDays(7);
 
       System.out.println("Run with now=" + now);
--- src/test/java/victor/testing/mocks/time/TimeBasedLogicTest.java
+++ src/test/java/victor/testing/mocks/time/TimeBasedLogicTest.java
@@ -5,12 +5,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static java.time.LocalDate.parse;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,15 +24,42 @@ class TimeBasedLogicTest {
    OrderRepo orderRepo;
    @InjectMocks
    TimeBasedLogic target;
+   private List<Order> orders = List.of(new Order().setTotalAmount(130d));
+
+   @Test // 95 din cazuri esti inginer
+   void isFrequentBuyer_inSiktir_imprecis_darVerdeSiMaine() {
+      when(orderRepo.findByCustomerIdAndCreatedOnBetween(eq(13), any(), any()))
+              .thenReturn(orders);
+
+      assertThat(target.isFrequentBuyer(13)).isTrue();
+   }
 
    @Test
 //   @Disabled("flaky, time-based")
-   void isFrequentBuyer() {
+   void isFrequentBuyer_mockuriStaticeCuMockitoInline_ultimaVer() {
       when(orderRepo.findByCustomerIdAndCreatedOnBetween(
-          13, parse("2022-06-08"), parse("2022-06-15")))
-              .thenReturn(List.of(new Order().setTotalAmount(130d)));
+          13, parse("2021-12-18"), parse("2021-12-25")))
+              .thenReturn(orders);
 
-      assertThat(target.isFrequentBuyer(13)).isTrue();
+      LocalDate fixedTime = parse("2021-12-25");
+
+      try (MockedStatic<LocalDate> staticMock = mockStatic(LocalDate.class)) { // dep de pom: org.mockito:mockito-inline
+         staticMock.when(() -> LocalDate.now()).thenReturn(fixedTime);
+         boolean result = target.isFrequentBuyer(13);
+
+         assertThat(result).isTrue();
+      } // de aici incolo orice met statica din LocalDate ruleaza ca inainte curata
+
+
+      // avertisment: 20 de ani mockurile statice au fost proscrise de comunitate. arse pe rug.
+      // oameni pe SO urlau vai ce PROST esti daca mockuiesti met statice.
+      // ca nu faci OOP mama ta de bizon.
+      // de fapt batranii erau oripilati de mizeria produsa de librariile care ofereau mockuri statice (PowerMock)
+
+
+
+
+
 
       // 1: inject a Clock; Hint: you'll need ZoneId.systemDefault()
       // 2: interface for Clock retrival [general solution] -> **indirection without abstraction**
@@ -35,5 +67,18 @@ class TimeBasedLogicTest {
       // 4: pass time as method arg
       // 5: package-protected variant for testing
       // 6: mock now() - mocks all methods (try to add another LD.parse in the prod code) >> mockStatic(, CALLS_REAL_METHODS)
+   }
+
+   //   @Disabled("flaky, time-based")
+   @Test
+   void isFrequentBuyer() {
+      LocalDate now = parse("2022-06-15");
+      when(orderRepo.findByCustomerIdAndCreatedOnBetween(
+              13, parse("2022-06-08"), now))
+              .thenReturn(orders);
+
+      boolean result = target.isFrequentBuyer_candTestuPute_designulProduluiPoateFiImbunatatit(13, now);
+
+      assertThat(result).isTrue();
    }
 }
