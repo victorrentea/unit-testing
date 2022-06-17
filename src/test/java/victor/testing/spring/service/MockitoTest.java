761b61871f928ae2a8fc05f3ebc92bf24e0a1f29
victorrentea <victorrentea@gmail.com>
2022-06-17 17:09:38 +0300

    tata lor: TEST containers e default astazi

--- src/test/java/victor/testing/spring/service/MockitoTest.java
+++ src/test/java/victor/testing/spring/service/MockitoTest.java
@@ -1,5 +1,6 @@
 package victor.testing.spring.service;
 
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -12,9 +13,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.domain.ProductCategory;
 import victor.testing.spring.domain.Supplier;
@@ -22,6 +28,9 @@ import victor.testing.spring.infra.SafetyClient;
 import victor.testing.spring.repo.ProductRepo;
 import victor.testing.spring.repo.SupplierRepo;
 import victor.testing.spring.web.dto.ProductDto;
+import victor.testing.tools.TestcontainersUtils;
+
+import java.io.IOException;
 
 import static java.time.LocalDateTime.now;
 import static java.time.temporal.ChronoUnit.SECONDS;
@@ -29,7 +38,8 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 //@ConditionaOn
 @SpringBootTest//(properties = "spring.datasource.url=jdbc:h2:mem:")
-@ActiveProfiles("db-mem")
+//@ActiveProfiles("db-mem")
+@Testcontainers
 @Transactional // LOVE❤️
 //@Preven
 //@Tag("")
@@ -44,6 +54,27 @@ public class MockitoTest extends AbstractRepoTestBase {
    private SupplierRepo supplierRepo;
    @Autowired
    private ProductService productService;
+
+   @Container
+   static public PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:11")
+           .withDatabaseName("prop")
+           .withUsername("postgres")
+           .withPassword("password");
+
+//   @SneakyThrows
+   @DynamicPropertySource
+   public static void registerPgProperties(DynamicPropertyRegistry registry) {
+      // A: if you want to spy the JDBC calls
+      registry.add("spring.datasource.url", () -> TestcontainersUtils.injectP6SPYInJdbcUrl(postgres.getJdbcUrl()));
+      registry.add("spring.datasource.driver-class-name", () -> "com.p6spy.engine.spy.P6SpyDriver");
+      // B: clean (no spying)
+      // registry.add("spring.datasource.url", () -> postgres.getJdbcUrl());
+      // registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
+
+      registry.add("spring.datasource.username", postgres::getUsername);
+      registry.add("spring.datasource.password", postgres::getPassword);
+//throw new IOException();
+   }
 
 //@BeforeEach
 //final void before() {
