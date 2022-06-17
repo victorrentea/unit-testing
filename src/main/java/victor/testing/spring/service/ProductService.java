07ec2b8278c82c6b8573d5037714224600cc1a3d
victorrentea <victorrentea@gmail.com>
2022-06-17 16:32:07 +0300

    suntme bine

--- src/main/java/victor/testing/spring/service/ProductService.java
+++ src/main/java/victor/testing/spring/service/ProductService.java
@@ -1,6 +1,7 @@
 package victor.testing.spring.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.infra.SafetyClient;
