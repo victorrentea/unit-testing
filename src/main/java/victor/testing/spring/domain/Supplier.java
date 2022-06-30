71879d56d8e8b9056d9501af8256de3c6e7608ab
victorrentea <victorrentea@gmail.com>
2022-06-30 16:38:05 +0300

    Am trecut la DB in memorie (H2)

--- src/main/java/victor/testing/spring/domain/Supplier.java
+++ src/main/java/victor/testing/spring/domain/Supplier.java
@@ -5,13 +5,14 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Supplier {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue// (strategy = GenerationType.IDENTITY)
     @Getter @Setter
     private Long id;
     @Getter @Setter
