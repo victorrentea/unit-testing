919c997ce9f69741f26fb4765191a829318c56d5
victorrentea <victorrentea@gmail.com>
2022-06-30 22:26:04 +0300

    add flyway

---
+++ src/main/resources/db.migration.postgresql/V1__initial.sql
@@ -0,0 +1,7 @@
+create sequence hibernate_sequence start 1 increment 1;
+
+create table product (id int8 not null, barcode varchar(255), category int4, create_date timestamp, name varchar(255), supplier_id int8, primary key (id));
+
+create table supplier (id int8 not null, active boolean not null, name varchar(255), primary key (id));
+
+alter table if exists product add constraint FK2kxvbr72tmtscjvyp9yqb12by foreign key (supplier_id) references supplier;
--- src/test/java/victor/testing/tools/TestcontainersUtils.java
+++ src/test/java/victor/testing/tools/TestcontainersUtils.java
@@ -1,10 +1,13 @@
 package victor.testing.tools;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class TestcontainersUtils {
     public static String injectP6SPYInJdbcUrl(String originalJdbcUrl) {
        String remainingUrl = originalJdbcUrl.substring("jdbc:".length());
        String p6spyUrl = "jdbc:p6spy:" + remainingUrl;
-       System.out.println("Injected p6spy into jdbc url:" + p6spyUrl + "; ORIGINAL=" + originalJdbcUrl);
+       log.debug("Injected p6spy into jdbc url= " + p6spyUrl + "  (orignal= " + originalJdbcUrl+")");
        return p6spyUrl;
     }
 }
