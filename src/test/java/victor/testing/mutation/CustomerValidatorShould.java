dafc799d35b088194787227fe72e7df9158473db
victorrentea <victorrentea@gmail.com>
2022-07-13 11:08:00 +0300

    Object mother.

--- src/test/java/victor/testing/mutation/CustomerValidatorShould.java
+++ src/test/java/victor/testing/mutation/CustomerValidatorShould.java
@@ -1,23 +1,31 @@
 package victor.testing.mutation;
 
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import victor.testing.tools.CaptureSystemOutput;
 import victor.testing.tools.CaptureSystemOutput.OutputCapture;
 
 import static org.assertj.core.api.Assertions.assertThat;
+class TestData {
+    // PRO =
+    // RISK = changes here may break dozens of tests >>> RULE: never change, but add a new method
+
+   public static Customer johnCustomer() {
+      return new Customer()
+              .setName("::name::")
+              .setEmail("::email::")
+              .setAddress(new Address()
+                      .setCity("::city::"));
+   }
+}//Object Mother F...er https://martinfowler.com/bliki/ObjectMother.html
 
 
 public class CustomerValidatorShould {
     CustomerValidator validator = new CustomerValidator();
-    private Customer customer = new Customer()
-            .setName("::name::")
-            .setEmail("::email::")
-            .setAddress(new Address()
-                    .setCity("::city::"));
+    private Customer customer = TestData.johnCustomer();
 
-    public CustomerValidatorShould() {
+   public CustomerValidatorShould() {
         System.out.println("each test in JUnit gets a new instance of the test class > the field valiues never 'leak' between tests");
     }
 
