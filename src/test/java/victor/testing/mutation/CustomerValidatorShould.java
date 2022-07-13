7f903aee67db1d784376e927eba197a76bdfef10
victorrentea <victorrentea@gmail.com>
2022-07-13 11:03:10 +0300

    Initialization of canonical data in fields + change it in each test

--- src/test/java/victor/testing/mutation/CustomerValidatorShould.java
+++ src/test/java/victor/testing/mutation/CustomerValidatorShould.java
@@ -4,63 +4,71 @@ package victor.testing.mutation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import victor.testing.tools.CaptureSystemOutput;
+import victor.testing.tools.CaptureSystemOutput.OutputCapture;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class CustomerValidatorShould {
-   CustomerValidator validator = new CustomerValidator();
-   private Customer customer ;
+    CustomerValidator validator = new CustomerValidator();
+    private Customer customer = new Customer()
+            .setName("::name::")
+            .setEmail("::email::")
+            .setAddress(new Address()
+                    .setCity("::city::"));
 
-   @BeforeEach
-   final void before() {
-      // you do NOT need builders for mutable data objects. Chainable setters instead.
-      customer = new Customer()
-              .setName("::name::")
-              .setEmail("::email::")
-              .setAddress(new Address()
-                      .setCity("::city::"));
-   }
-   @Test
-   @CaptureSystemOutput
-   void valid(CaptureSystemOutput.OutputCapture capture) {
+    public CustomerValidatorShould() {
+        System.out.println("each test in JUnit gets a new instance of the test class > the field valiues never 'leak' between tests");
+    }
 
-      validator.validate(customer);
+    //   @BeforeEach
+//   final void before() {
+//      // you do NOT need builders for mutable data objects. Chainable setters instead.
+//      customer = ;
+//   }
+    @Test
+    @CaptureSystemOutput
+    void valid(OutputCapture capture) {
 
-      assertThat(capture.toString()).contains("This");
+        validator.validate(customer);
 
-   }
-   @Test
-   @CaptureSystemOutput
-   void valida(CaptureSystemOutput.OutputCapture capture) {
-      customer.setName("::nme::");
+        assertThat(capture.toString()).contains("This");
 
-      validator.validate(customer);
+    }
 
-      assertThat(capture.toString()).contains("This");
+    @Test
+    @CaptureSystemOutput
+    void valida(OutputCapture capture) {
+        customer.setName("::nme::");
 
-   }
-   @Test
-   @CaptureSystemOutput
-   void four(CaptureSystemOutput.OutputCapture capture) {
-      // Arrange = Given = context of the test
-      customer.setEmail("::eil::");
+        validator.validate(customer);
 
-      // Act = When = the prod call
-      validator.validate(customer);
+        assertThat(capture.toString()).contains("This");
 
-      // Assert = Then = assertions and verifications
-      assertThat(capture.toString()).contains("This");
-   }
-   @Test
-   @CaptureSystemOutput
-   void three(CaptureSystemOutput.OutputCapture capture) {
-      customer.setName("::nme::");
-      customer.setEmail("::eail::");
+    }
 
-      validator.validate(customer);
+    @Test
+    @CaptureSystemOutput
+    void four(OutputCapture capture) {
+        // Arrange = Given = context of the test
+        customer.setEmail("::eil::");
 
-      assertThat(capture.toString()).doesNotContain("This");
-   }
+        // Act = When = the prod call
+        validator.validate(customer);
+
+        // Assert = Then = assertions and verifications
+        assertThat(capture.toString()).contains("This");
+    }
+
+    @Test
+    @CaptureSystemOutput
+    void three(OutputCapture capture) {
+        customer.setName("::nme::")
+                .setEmail("::eail::");
+
+        validator.validate(customer);
+
+        assertThat(capture.toString()).doesNotContain("This");
+    }
 
 }
