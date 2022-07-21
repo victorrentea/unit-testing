d55f075ff4335d6213d6a6ba2d7c97309209aa66
victorrentea <victorrentea@gmail.com>
2022-07-21 10:50:04 +0300

    AM curatat testu - builderi pt imutabile sau setteri fluent - Test Data (Object Mother) - deriva date de la un 'standard'

--- src/test/java/victor/testing/mutation/CustomerValidatorTest.java
+++ src/test/java/victor/testing/mutation/CustomerValidatorTest.java
@@ -1,50 +1,42 @@
 package victor.testing.mutation;
 
 
-import org.junit.Rule;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.fail;
 
+class TestData {
+   // e prea tehnica.
+   public static Customer john() {
+      return new Customer()
+              .setName("::name::")
+              .setEmail("::email::")
+              .setAddress(new Address()
+                      .setCity("::city::"));
+   } // Object Mother
+
+}
 
 public class CustomerValidatorTest {
    CustomerValidator validator = new CustomerValidator();
 
    @Test
    void happy() {
-      Customer customer = new Customer();
-      customer.setName("::name::");
-      customer.setEmail("::email::");
-      customer.setAddress(new Address());
-      customer.getAddress().setCity("::city::");
+      Customer customer = TestData.john();
       validator.validate(customer);
    }
-//   @Rule  ExpectedException expectedException = ExpectedException.none(); / Junit 4
-//   @org.junit.Test(expected = IllegalArgumentException.class) // Junit4
-   @Test
-//   void whenCustomerNameNull_thenExceptionIsThrown() {
-   void throws_forMissingCustomerName() {
-      Customer customer = new Customer();
-      customer.setEmail("::email::");
-      customer.setAddress(new Address());
-      customer.getAddress().setCity("::city::");
 
-      // JUnit5 < E NASPA
-      Assertions.assertThrows(IllegalArgumentException.class,
-              () -> validator.validate(customer)
-      );
-      // AssertJ << ASTA E BUN
-      assertThatThrownBy(() -> validator.validate(customer)).isInstanceOf(IllegalArgumentException.class);
+   @Test
+   void throws_forMissingCustomerName() {
+      Customer customer = TestData.john().setName(null);
+
+      assertThatThrownBy(() -> validator.validate(customer))
+              .isInstanceOf(IllegalArgumentException.class);
    }
    @Test
    void throws_forMissingCustomerEmail() {
-      Customer customer = new Customer();
-      customer.setName("::name::");
-      customer.setAddress(new Address());
-      customer.getAddress().setCity("::city::");
+      Customer customer = TestData.john().setEmail(null);
 
       assertThatThrownBy(() -> validator.validate(customer))
               .isInstanceOf(IllegalArgumentException.class);
