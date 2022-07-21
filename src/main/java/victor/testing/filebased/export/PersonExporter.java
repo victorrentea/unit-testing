1806d615995423a64369b5c7ba23764347c9a9bf
victorrentea <victorrentea@gmail.com>
2022-07-21 14:53:33 +0300

    approval testing : fac o schimbare in prod si "validez" ca noua valouare produsa de prod e CORECTA. daca e> modific testele orbest (copy-paste expected)

--- src/main/java/victor/testing/filebased/export/PersonExporter.java
+++ src/main/java/victor/testing/filebased/export/PersonExporter.java
@@ -2,6 +2,7 @@ package victor.testing.filebased.export;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.time.format.DateTimeFormatter;
 
 public class PersonExporter {
    private final PersonRepo personRepo;
@@ -29,7 +30,7 @@ public class PersonExporter {
          writer.write( person.getPhoneList().get(0)); // TODO fix bug: what if no phone?
 //         writer.write(String.join(",", person.getPhoneList())); // TODO CR: output all phones comma-separated
          writer.write(";");
-         writer.write(person.getBirthDate().toString()); // TODO CR: change format to "12 Nov 2021"
+         writer.write(person.getBirthDate().format(DateTimeFormatter.ofPattern("dd MMM yyyy"))); // TODO CR: change format to "12 Nov 2021"
          writer.write("\n");
       }
    }
