a842547a5a0c4bd63d13dd09997d303aa0f26d07
victorrentea <victorrentea@gmail.com>
2022-07-21 10:32:13 +0300

    cleanup

--- src/main/java/victor/testing/mutation/CustomerValidator.java
+++ src/main/java/victor/testing/mutation/CustomerValidator.java
@@ -26,21 +26,3 @@ public class CustomerValidator {
 }
 
 
-class MyException extends RuntimeException {
-	MyException(ErrorCode errorCode) {
-		this.errorCode = errorCode;
-	}
-	// + other 12 overloads, taking a message string, a cause exception, and combinations
-
-	public enum ErrorCode {
-		CUSTOMER_MISSING_CITY,
-		CUSTOMER_TOO_SHORT_CITY,
-		GENERAL
-	}
-	private final ErrorCode errorCode;
-
-	// to use in tests:
-	public static <T extends Throwable> Predicate<T> hasCode(ErrorCode errorCode) {
-		return e -> (e instanceof MyException && ((MyException) e).errorCode == errorCode);
-	}
-}
