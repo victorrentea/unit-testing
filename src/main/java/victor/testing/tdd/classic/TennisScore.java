eb7e6848eccaf3dd86c040265b942a6b84e52501
victorrentea <victorrentea@gmail.com>
2022-07-21 13:06:39 +0300

    pranzul

--- src/main/java/victor/testing/tdd/classic/TennisScore.java
+++ src/main/java/victor/testing/tdd/classic/TennisScore.java
@@ -14,16 +14,6 @@ public class TennisScore {
 
     private String translateScore(int points) {
         return LABELS[points];
-//        switch (points) {
-//            case 0:
-//                return "Love";
-//            case 1:
-//                return "Fifteen";
-//            case 2:
-//                return "Thirty";
-//            default:
-//                throw new IllegalStateException("Unexpected value: " + points);
-//        }
     }
 
     public void addPoint(Player player) {
--- src/test/java/victor/testing/tdd/classic/TennisScoreTest.java
+++ src/test/java/victor/testing/tdd/classic/TennisScoreTest.java
@@ -1,7 +1,9 @@
 package victor.testing.tdd.classic;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
