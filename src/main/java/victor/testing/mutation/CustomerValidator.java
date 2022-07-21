be397fcbe2a8a252a4ab6cd1add8b70aafe0724e
victorrentea <victorrentea@gmail.com>
2022-07-21 16:27:57 +0300

    cleanup

--- src/main/java/victor/testing/mutation/CustomerValidator.java
+++ src/main/java/victor/testing/mutation/CustomerValidator.java
@@ -19,17 +19,10 @@ public class CustomerValidator {
 			throw new IllegalArgumentException("Missing address city");
 		}
 
-if (address.getCity() != null) {
-	sendKafkaMessage("a");
-}
-
 		address.setCity(address.getCity().trim()); // mutate this
 		if (address.getCity().length() < 3) { //.. or swap the ifs
 			throw new IllegalArgumentException("Address city too short");
 		}
-	}
-
-	private void sendKafkaMessage(String a) {
 	}
 
 }
