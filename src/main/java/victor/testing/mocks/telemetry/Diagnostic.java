bb4bb85f85b7b14ac986cac22ea71771bc17501e
victorrentea <victorrentea@gmail.com>
2022-07-21 17:29:50 +0300

    Verify + when pe aceeasi functie = dubious

---
+++ src/main/java/victor/testing/mocks/telemetry/Diagnostic.java
@@ -0,0 +1,52 @@
+package victor.testing.mocks.telemetry;
+
+import victor.testing.mocks.telemetry.Client.ClientConfiguration;
+import victor.testing.mocks.telemetry.Client.ClientConfiguration.AckMode;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+// am gasit acest cod prin proiect.
+// si am ajuns la concl ca trebuie sa-l schimb
+
+public class Diagnostic {
+	public static final String DIAGNOSTIC_CHANNEL_CONNECTION_STRING = "*111#";
+	private final Client client;
+
+	public Diagnostic(Client client) {
+		this.client = client;
+	}
+
+	private String diagnosticInfo = "";
+
+	public String getDiagnosticInfo() {
+		return diagnosticInfo;
+	}
+	public void setDiagnosticInfo(String diagnosticInfo) {
+		this.diagnosticInfo = diagnosticInfo;
+	}
+
+	public void checkTransmission(boolean force) {
+		client.disconnect(force);
+
+		int currentRetry = 1;
+		while (! client.getOnlineStatus() && currentRetry <= 3) {
+			client.connect(DIAGNOSTIC_CHANNEL_CONNECTION_STRING);
+			currentRetry ++;
+		}
+
+		if (! client.getOnlineStatus()) {
+			throw new IllegalStateException("Unable to connect.");
+		}
+
+		ClientConfiguration config = new ClientConfiguration();
+		config.setSessionId(client.getVersion()/*.toUpperCase()*/ + "-" + UUID.randomUUID().toString());
+		config.setSessionStart(LocalDateTime.now());
+		config.setAckMode(AckMode.NORMAL);
+		client.configure(config);
+
+		client.send(Client.DIAGNOSTIC_MESSAGE);
+		diagnosticInfo = client.receive();
+	}
+
+}
--- src/test/java/victor/testing/mocks/telemetry/TelemetryDiagnosticTest.java
+++ src/test/java/victor/testing/mocks/telemetry/TelemetryDiagnosticTest.java
@@ -1,12 +1,9 @@
 package victor.testing.mocks.telemetry;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,11 +15,9 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class TelemetryDiagnosticTest {
     @Mock
-    TelemetryClient clientMock; // = mock(TelemetryClient.class);
+    Client clientMock; // = mock(TelemetryClient.class);
     @InjectMocks
-    TelemetryDiagnostic diagnostic;// = new TelemetryDiagnostic(mock);
-
-
+    Diagnostic diagnostic;// = new TelemetryDiagnostic(mock);
 
     @Test
     void disconnects() {
@@ -37,8 +32,50 @@ public class TelemetryDiagnosticTest {
     void throwsWhenNotOnline() {
         when(clientMock.getOnlineStatus()).thenReturn(false);
 
-        assertThatThrownBy(() ->diagnostic.checkTransmission(true))
+        assertThatThrownBy(() -> diagnostic.checkTransmission(true))
                 .isInstanceOf(IllegalStateException.class);
 
     }
+    @Test
+    void sends() {
+        when(clientMock.getOnlineStatus()).thenReturn(true);
+
+        // prod call
+        diagnostic.checkTransmission(true);
+
+        verify(clientMock).send(notNull());// null este any() dar nu este anyString()
+//        verify(clientMock).send(any());// null este any() dar nu este anyString()
+//        verify(clientMock).send(anyString()); // ai consumat 6 car degeaba
+
+
+        // mai strict
+        // refolosesti constanta din prod: PRO: are semantica (nume), si testul NU pica daca val ct se modifica
+        verify(clientMock).send(Client.DIAGNOSTIC_MESSAGE); // in general asta
+
+        // pui literal: daca VREI sa pice testu cand val se schimba
+        verify(clientMock).send("AT#UD"); // daca literalul este trimis/primit de la alte sistem (parte din API)
+    }
+
+    @Test
+    void receivesDiagnosticInfo() {
+        when(clientMock.getOnlineStatus()).thenReturn(true);
+        when(clientMock.receive()).thenReturn("acelceva");
+
+        diagnostic.checkTransmission(true);
+
+        verify(clientMock).receive(); // deageaba (in afara cazului cand vreau sa vf ca 1 singura data a fost chemata).
+        // cand ma doare de cate ori chem o functie:
+        // 1) cand poate intoarce altceva a doua oara (eg SELECT, GET)
+        // 2) cand functia chemata are side effects (eg INSERT, POST)
+        // 3) daca functia dureaza timp.
+
+        // cum se cheama o fucntie care intoarce aleasi rezultat si nu face side effects : FUNCTIE  PURE
+        // o functie PURA nu ar trebui sa-ti pese de cate ori o chemi
+
+        // daca AI NEVOIE Sa faci VERIFY pe aceeasi  fucntie pe care ai facut when..then >> fucntia fie face retea fie nu e PURA
+
+        assertThat(diagnostic.getDiagnosticInfo()).isEqualTo("acelceva");
+    }
+
+
 }
