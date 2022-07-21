91ec0be76da7e9f97873e6f135847290b9d69835
victorrentea <victorrentea@gmail.com>
2022-07-21 10:34:54 +0300

    play

--- src/main/java/victor/testing/mutation/CustomerValidator.java
+++ src/main/java/victor/testing/mutation/CustomerValidator.java
@@ -18,9 +18,9 @@ public class CustomerValidator {
 		if (address.getCity() == null) {
 			throw new IllegalArgumentException("Missing address city");
 		}
-		if (address.getCity() != null) {
-			sendKafkaMessage("a");
-		}
+if (address.getCity() != null) {
+	sendKafkaMessage("a");
+}
 		address.setCity(address.getCity().trim()); // mutate this
 		if (address.getCity().length() < 3) { //.. or swap the ifs
 			throw new IllegalArgumentException("Address city too short");
