5adc32e78c7b9a5eb1eea3dc716dcd907b3e382f
victorrentea <victorrentea@gmail.com>
2022-08-01 14:32:49 +0300

    PRANZ

--- src/main/java/victor/testing/tdd/classic/TennisScore.java
+++ src/main/java/victor/testing/tdd/classic/TennisScore.java
@@ -10,12 +10,21 @@ public class TennisScore {
     private static final Map<Integer, String> POINTS_TO_SCORE = Map.of(
             0, "Love",
             1, "Fifteen",
-            2, "Thirty"
+            2, "Thirty",
+            3, "Forty"
     );
 
     public String getScore() {
+        if (player1Points >= 3 && player2Points >= 3) {
+            if (player1Points == player2Points + 1) {
+                return "Advantage Player1";
+            }
+            if (player1Points == player2Points) {
+                return "Deuce";
+            }
+        }
         return translate(player1Points) + " - " + translate(player2Points);
-    }
+        }
 
     private String translate(int player1Points) {
         return POINTS_TO_SCORE.get(player1Points);
--- src/test/java/victor/testing/tdd/classic/TennisScoreTest.java
+++ src/test/java/victor/testing/tdd/classic/TennisScoreTest.java
@@ -37,6 +37,48 @@ public class TennisScoreTest {
         String actual = tennis.getScore();
         assertThat(actual).isEqualTo("Love - Fifteen");
     }
+    @Test
+    void loveForty() {
+        tennis.wonPoint(Player.TWO);
+        tennis.wonPoint(Player.TWO);
+        tennis.wonPoint(Player.TWO);
+        String actual = tennis.getScore();
+        assertThat(actual).isEqualTo("Love - Forty");
+    }
 
+    //If at least three points have been scored
+    // by each player, and the scores are equal,
+    // the score is “Deuce”.
 
+    @Test
+    void deuce() {
+        wonPoints(Player.ONE, 3);
+        wonPoints(Player.TWO, 3);
+        String actual = tennis.getScore();
+
+        assertThat(actual).isEqualTo("Deuce");
+    }
+    @Test
+    void deuce4() {
+        wonPoints(Player.ONE, 4);
+        wonPoints(Player.TWO, 4);
+        String actual = tennis.getScore();
+
+        assertThat(actual).isEqualTo("Deuce");
+    }
+    @Test
+    void advantagePlayer1() {
+        wonPoints(Player.ONE, 14);
+        wonPoints(Player.TWO, 13);
+        String actual = tennis.getScore();
+
+        assertThat(actual).isEqualTo("Advantage Player1");
+    }
+
+    // bebe mic testing framework....
+    private void wonPoints(Player player, int points) {
+        for (int i = 0; i < points; i++) {
+            tennis.wonPoint(player);
+        }
+    }
 }
