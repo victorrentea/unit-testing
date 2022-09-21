08003f0d4e37d300f821c6f8940b62c1e54c6612
victorrentea <victorrentea@gmail.com>
2022-09-21 10:41:35 +0300

    TDD warmup

---
+++ src/main/java/victor/testing/tdd/classic/TennisScore.java
@@ -0,0 +1,20 @@
+package victor.testing.tdd.classic;
+
+public class TennisScore {
+
+    // comment in prod code outdated
+    private String score = "Love - Love";
+
+    public String getScore() {
+        return score;
+    }
+
+    public void winsPoint(Player player) {
+        if (player == Player.ONE)
+        score = "Fifteen - Love";
+        else score = "Love - Fifteen";
+    }
+}
+enum Player {
+    TWO, ONE
+}
---
+++ src/test/java/victor/testing/tdd/classic/TennisScoreShould.java
@@ -0,0 +1,35 @@
+package victor.testing.tdd.classic;
+
+import org.junit.jupiter.api.*;
+import victor.testing.tools.ReplaceCamelCase;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayNameGeneration(ReplaceCamelCase.class)
+//@TestMethodOrder(MethodOrderer.class) // don't!
+public class TennisScoreShould {
+    private final TennisScore tennisScore = new TennisScore();
+
+    @Test
+    void returnsLoveLoveForNewGame() {
+        String score = tennisScore.getScore();
+        assertThat(score).isEqualTo("Love - Love");
+    }
+
+    //The running score of each game is described in a manner
+    // peculiar to tennis: scores from zero to three points
+    // are described as “Love”, “Fifteen”, “Thirty”, and “Forty” respectively.
+
+    @Test
+    void returnsFifteenLoveWhenPlayer1Scores1Point() {
+        tennisScore.winsPoint(Player.ONE);
+        String score = tennisScore.getScore();
+        assertThat(score).isEqualTo("Fifteen - Love");
+    }
+    @Test
+    void returnsLoveFifteenWhenPlayer2Scores1Point() {
+        tennisScore.winsPoint(Player.TWO);
+        String score = tennisScore.getScore();
+        assertThat(score).isEqualTo("Love - Fifteen");
+    }
+}
