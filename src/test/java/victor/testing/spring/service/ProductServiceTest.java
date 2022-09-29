dbf79122a9284a71e075c1ee1acd063506ad45f4
Victor Rentea <victorrentea@gmail.com>
2022-09-29 12:28:22 +0300

    Cleanup folowing FK direction

--- src/test/java/victor/testing/spring/service/ProductServiceTest.java
+++ src/test/java/victor/testing/spring/service/ProductServiceTest.java
@@ -1,5 +1,6 @@
 package victor.testing.spring.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -50,6 +51,11 @@ public class ProductServiceTest {
    @Autowired
    private ProductService productService;
 
+   @BeforeEach
+   final void before() {
+       productRepo.deleteAll();
+       supplierRepo.deleteAll();
+   }
    @Test
    public void createThrowsForUnsafeProduct() {
       when(mockSafetyClient.isSafe("bar")).thenReturn(false);
@@ -61,24 +67,31 @@ public class ProductServiceTest {
 
    @Test
    public void createOk() {
-      // GIVEN
-
       Long supplierId = supplierRepo.save(new Supplier()).getId();
-
       when(mockSafetyClient.isSafe("safebar")).thenReturn(true);
       ProductDto dto = new ProductDto("name", "safebar", supplierId, ProductCategory.HOME);
 
       // WHEN
-      Long productId = productService.createProduct(dto);
+      productService.createProduct(dto);
 
-      // THEN
-      // 1) assuming an empty DB (simper)
       Product product = productRepo.findAll().get(0);
+      assertThat(product.getName()).isEqualTo("name");
+      assertThat(product.getBarcode()).isEqualTo("safebar");
+      assertThat(product.getSupplier().getId()).isEqualTo(supplierId);
+      assertThat(product.getCategory()).isEqualTo(ProductCategory.HOME);
+      assertThat(product.getCreateDate()).isCloseTo(now(), byLessThan(1, SECONDS));
+   }
 
-      // 2) Data slice tests
-//      Product product = productRepo.findById(productId).orElseThrow();
+   @Test
+   public void createOk2() {
+      Long supplierId = supplierRepo.save(new Supplier()).getId();
+      when(mockSafetyClient.isSafe("safebar")).thenReturn(true);
+      ProductDto dto = new ProductDto("name", "safebar", supplierId, ProductCategory.HOME);
 
+      // WHEN
+      productService.createProduct(dto);
 
+      Product product = productRepo.findAll().get(0);
       assertThat(product.getName()).isEqualTo("name");
       assertThat(product.getBarcode()).isEqualTo("safebar");
       assertThat(product.getSupplier().getId()).isEqualTo(supplierId);
