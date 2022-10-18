43ad64d6c672e15ef591947e808bbee36d8236b0
Victor Rentea <victorrentea@gmail.com>
2022-10-18 16:25:56 +0300

    h2 care intelege sintaxa de DDL (create table) specifica PG

--- src/main/resources/db/migration/V1__initial.sql
+++ src/main/resources/db/migration/V1__initial.sql
@@ -1,6 +1,6 @@
-create sequence hibernate_sequence start 1 increment 1;
+create sequence hibernate_sequence start with 1 increment 1;
 
-create table products (id int8 not null, barcode varchar(255), category int4, create_date timestamp, name varchar(255), supplier_id int8, primary key (id));
+create table product (id int8 not null, barcode varchar(255), category int4, create_date timestamp, name varchar(255), supplier_id int8, primary key (id));
 
 create table supplier (id int8 not null, active boolean not null, name varchar(255), primary key (id));
 
