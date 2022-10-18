af3ede424d3a2245630ed3aa72869fd70db727b4
Victor Rentea <victorrentea@gmail.com>
2022-10-18 15:02:32 +0300

    MOTIVE (bis) pentru care Spring nu poate reutiliza contextul de spring si isi re-creaza alt context = +20...40sec de test run time

--- src/test/java/victor/testing/spring/parallel/TestIncet.java
+++ src/test/java/victor/testing/spring/parallel/TestIncet.java
@@ -2,6 +2,7 @@ package victor.testing.spring.parallel;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Configuration;
@@ -13,13 +14,17 @@ import victor.testing.spring.repo.ProductRepo;
 //@Configuration
 //class SecurityConfig {}
 @Slf4j
+
+//@DataJpaTest // test slices : porneste din Spring Boot doar parti mici : merge mai repede testu tau in clasa asta (pe local),
+//  #4 dar pe CI ar putea cauza sa se creeze inca un context dedicat pt clasa asta
+
 @SpringBootTest
             //(properties = "oProp=altaValoare") // #2 pentru ca prop nu se pot REINJECTA in beanuri, daca o prop e diferita => alt context de test
 //@ActiveProfiles("prrofil") // #1 conditioneaza @Bean @Component... @Config , application-prrofil.properties
 public class TestIncet {
 
-    @MockBean
-    private ProductRepo productRepo; // #3 @MockBean inlocuieste un bean. orice alta compo spring isi injecteaza acest repo,
+//    @MockBean
+//    private ProductRepo productRepo; // #3 @MockBean inlocuieste un bean. orice alta compo spring isi injecteaza acest repo,
     // va injecta lucruri diferite intre TestIncet si TestIncet2...
     @Test
     void unu() throws InterruptedException {
--- src/test/java/victor/testing/spring/parallel/TestIncet2AltaClasa.java
+++ src/test/java/victor/testing/spring/parallel/TestIncet2AltaClasa.java
@@ -2,10 +2,12 @@ package victor.testing.spring.parallel;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @Slf4j
-@SpringBootTest
+@DataJpaTest
+//@SpringBootTest
 public class TestIncet2AltaClasa {
     @Test
     void trei() throws InterruptedException {
@@ -13,5 +15,7 @@ public class TestIncet2AltaClasa {
         Thread.sleep(1000);
         log.info("End3");
     }
+    // 31.3 ... 37.5 -- 6.2 sec -- 3.2s: @SpringBootTest
+    // 19.9 .. 24.9 -- 5 -- 2s  sec @DataJPa
 
 }
