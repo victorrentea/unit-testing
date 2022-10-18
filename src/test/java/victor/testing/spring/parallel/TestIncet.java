df2a18d5c5ffe36484839359c6e1daba2e905963
Victor Rentea <victorrentea@gmail.com>
2022-10-18 14:49:43 +0300

    MOTIVE pentru care Spring nu poate reutiliza contextul de spring si isi re-creaza alt context = +20...40sec de test run time

--- src/test/java/victor/testing/spring/parallel/TestIncet.java
+++ src/test/java/victor/testing/spring/parallel/TestIncet.java
@@ -3,10 +3,24 @@ package victor.testing.spring.parallel;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import victor.testing.spring.repo.ProductRepo;
 
+//@Profile("!local")
+//@Configuration
+//class SecurityConfig {}
 @Slf4j
 @SpringBootTest
+            //(properties = "oProp=altaValoare") // #2 pentru ca prop nu se pot REINJECTA in beanuri, daca o prop e diferita => alt context de test
+//@ActiveProfiles("prrofil") // #1 conditioneaza @Bean @Component... @Config , application-prrofil.properties
 public class TestIncet {
+
+    @MockBean
+    private ProductRepo productRepo; // #3 @MockBean inlocuieste un bean. orice alta compo spring isi injecteaza acest repo,
+    // va injecta lucruri diferite intre TestIncet si TestIncet2...
     @Test
     void unu() throws InterruptedException {
         log.info("Start1");
