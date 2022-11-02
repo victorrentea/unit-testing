399e1bdb5c89b9cd8ddf000bd931dc6f607a0c10
Victor Rentea <victorrentea@gmail.com>
2022-11-02 12:58:06 +0200

    A BUG IN a test !!!

--- src/test/java/victor/testing/tdd/classic/TennisScore.java
+++ src/test/java/victor/testing/tdd/classic/TennisScore.java
@@ -5,39 +5,34 @@ import java.util.Objects;
 public class TennisScore {
     private int player1Points = 0;
     private int player2Points = 0;
-//    private String firstPlayerScore = "Love";
-//    private String secondPlayerScore = "Love";
 
     public String getScore() {
-        // what here ????
-//        return firstPlayerScore + ":" + secondPlayerScore;
         return translate(player1Points) + ":" + translate(player2Points);
     }
 
     public void addPoint(Player player) {
         if (player==Player.ONE) {
-//            firstPlayerScore = "Fifteen";
             player1Points++;
         } else {
             player2Points++;
-//            if (Objects.equals(secondPlayerScore, "Fifteen"))
-//                secondPlayerScore = "Thirty";
-//            else secondPlayerScore = "Fifteen";
         }
     }
 
-    // Simon says... anticipating this implem funct > DON'T TEST IMPLEM FUNCTIONS.
-    // TEST AT THE PUBLIC API ONLY
     private String translate(int points) {
         if (points == 0) {
             return "Love";
         } else if (points == 1) {
             return "Fifteen";
-        } else if (points == 2) {
+        } else {
             return "Thirty";
+//        } else if (points == 3) {
+//            return "Forty";
         }
-        throw new IllegalArgumentException("impossible");
+//        throw new IllegalArgumentException("impossible");
     }
+
+    // Simon says... anticipating this implem funct > DON'T TEST IMPLEM FUNCTIONS.
+    // TEST AT THE PUBLIC API ONLY
 
     // the class "Player". do we need it NOW?
     // 💋 KISS principle : don't do it now
--- src/test/java/victor/testing/tdd/classic/TennisScoreTest.java
+++ src/test/java/victor/testing/tdd/classic/TennisScoreTest.java
@@ -11,9 +11,6 @@ public class TennisScoreTest {
     // down-clocking your brain: when developing with TDD you might BLOCK: your brain goes flat / req fuzzy
     // TDD tells you to feature-slice the requ to the littlest amount of behavior to test
 
-    public TennisScoreTest() {
-        System.out.println("new test class instance x");
-    }
     TennisScore tennisScore = new TennisScore();
     @Test
     void loveLove() {
@@ -41,5 +38,25 @@ public class TennisScoreTest {
         tennisScore.addPoint(Player.TWO);
         String actual = tennisScore.getScore();
         assertThat(actual).isEqualTo("Love:Thirty");
+    }
+    @Test
+    void loveForty() {
+        tennisScore.addPoint(Player.TWO);
+        tennisScore.addPoint(Player.TWO);
+        tennisScore.addPoint(Player.TWO);
+        String actual = tennisScore.getScore();
+        assertThat(actual).isEqualTo("Love:Thirty");
+    }
+//    @Test// redundant/overlapping test
+    // in TDD when a new test is already green
+    // - stupid your test is INCORRECT
+    // - stupid
+    // - smart
+    void fortyLove() {
+        tennisScore.addPoint(Player.ONE);
+        tennisScore.addPoint(Player.ONE);
+        tennisScore.addPoint(Player.ONE);
+        String actual = tennisScore.getScore();
+        assertThat(actual).isEqualTo("Forty:Love");
     }
 }
