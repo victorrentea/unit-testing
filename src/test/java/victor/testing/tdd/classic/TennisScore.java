c0361f18f6566f072574c03fbfa2fa32d8a67a88
Victor Rentea <victorrentea@gmail.com>
2022-11-02 12:52:20 +0200

    yes

--- src/test/java/victor/testing/tdd/classic/TennisScore.java
+++ src/test/java/victor/testing/tdd/classic/TennisScore.java
@@ -3,26 +3,41 @@ package victor.testing.tdd.classic;
 import java.util.Objects;
 
 public class TennisScore {
-    private String firstPlayerScore = "Love";
-    private String secondPlayerScore = "Love";
+    private int player1Points = 0;
+    private int player2Points = 0;
+//    private String firstPlayerScore = "Love";
+//    private String secondPlayerScore = "Love";
 
     public String getScore() {
-        return firstPlayerScore + ":" + secondPlayerScore;
+        // what here ????
+//        return firstPlayerScore + ":" + secondPlayerScore;
+        return translate(player1Points) + ":" + translate(player2Points);
     }
 
     public void addPoint(Player player) {
         if (player==Player.ONE) {
-            firstPlayerScore = "Fifteen";
+//            firstPlayerScore = "Fifteen";
+            player1Points++;
         } else {
-            if (Objects.equals(secondPlayerScore, "Fifteen"))
-                secondPlayerScore = "Thirty";
-            else secondPlayerScore = "Fifteen";
+            player2Points++;
+//            if (Objects.equals(secondPlayerScore, "Fifteen"))
+//                secondPlayerScore = "Thirty";
+//            else secondPlayerScore = "Fifteen";
         }
     }
 
     // Simon says... anticipating this implem funct > DON'T TEST IMPLEM FUNCTIONS.
     // TEST AT THE PUBLIC API ONLY
-//     String translate(int points) {}
+    private String translate(int points) {
+        if (points == 0) {
+            return "Love";
+        } else if (points == 1) {
+            return "Fifteen";
+        } else if (points == 2) {
+            return "Thirty";
+        }
+        throw new IllegalArgumentException("impossible");
+    }
 
     // the class "Player". do we need it NOW?
     // 💋 KISS principle : don't do it now
