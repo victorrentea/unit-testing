0070959932d03a75cbbf5318060a41e8714a34cd
Victor Rentea <victorrentea@gmail.com>
2022-11-03 13:37:12 +0200

    static mocks

--- src/test/java/victor/testing/design/time/TimeBasedLogicTest.java
+++ src/test/java/victor/testing/design/time/TimeBasedLogicTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 import victor.testing.spring.domain.Supplier;
 
@@ -18,6 +19,7 @@ import java.util.List;
 import static java.time.LocalDate.now;
 import static java.time.LocalDate.parse;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,15 +33,28 @@ class TimeBasedLogicTest {
    //   @Mock Clock clock; // exposing some awkward library Intant ZoneId
    @Test
 //   @Disabled("flaky, time-based")
+   // mockito-inline deprecated PowerMock
    void isFrequentBuyer() {
+      LocalDate christmas = parse("2022-12-25");
       when(orderRepo.findByCustomerIdAndCreatedOnBetween(
           13,
               LocalDate.parse("2022-12-18"),
-              LocalDate.parse("2022-12-25")))
+              christmas))
         .thenReturn(List.of(new Order().setTotalAmount(130d)));
 
-      // call to tested code
-      assertThat(target.isFrequentBuyer(13, LocalDate.parse("2022-12-25"))).isTrue();
+
+      try (MockedStatic<LocalDate> staticMockThatINeedToClose = mockStatic(LocalDate.class)) {
+         staticMockThatINeedToClose.when(LocalDate::now).thenReturn(christmas);
+         // here in the try block, on THIS thread any call to now() gets hacked
+         // call to tested code
+         boolean result = target.isFrequentBuyer(13);
+         assertThat(result).isTrue();
+
+         System.out.println(LocalDate.now());
+      }
+      // after this line, now() returns normally.
+
+
 
       //NO too heavy 3.5y ago 1: inject a Clock; Hint: you'll need ZoneId.systemDefault()
       //NO 3: inject a Supplier<LocalDate> - FP maniac version
