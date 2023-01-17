83b0cdd09532226f2be806e2bdeb18e97dbf9e74
Victor Rentea <victorrentea@gmail.com>
2023-01-17 15:08:26 +0200

    cleanup

--- src/main/java/victor/testing/design/roles/repo/TrackingProviderRepo.java
+++
@@ -1,9 +0,0 @@
-package victor.testing.design.roles.repo;
-
-import victor.testing.design.roles.model.TrackingProvider;
-
-import java.util.List;
-
-public interface TrackingProviderRepo {
-   List<TrackingProvider> findByAwb(String awb);
-}
--- src/test/java/victor/testing/tdd/unusualspending/Category.java
+++
@@ -1,9 +0,0 @@
-package victor.testing.tdd.unusualspending;
-
-public enum Category {
-    ENTERTAINMENT,
-    GOLF,
-    RESTAURANTS,
-    GROCERIES,
-    TRAVEL
-}
