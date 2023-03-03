ac7ae9373f529c37315cae0a2d28c6b65dd6ec48
Victor Rentea <victorrentea@gmail.com>
2023-03-03 13:19:04 +0200

    spring context CAN be reused between the two test clasess - identical config -> 1 banner in the log

--- src/test/java/victor/testing/spring/service/ProductService2Test.java
+++ src/test/java/victor/testing/spring/service/ProductService2Test.java
@@ -7,8 +7,8 @@ import victor.testing.spring.infra.SafetyClient;
 
 @Transactional
 public class ProductService2Test extends BaseTest {
-//  @MockBean // replaces a spring bean with a Mockito mock reset between each test and injects that here for you
-//  public SafetyClient mockSafetyClient;
+  @MockBean // replaces a spring bean with a Mockito mock reset between each test and injects that here for you
+  public SafetyClient mockSafetyClient;
   @Test
   void explore() {
     System.out.println("Me too");
