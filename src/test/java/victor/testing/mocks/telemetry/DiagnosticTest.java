27a306faf9adfb9fcd5742e9125409c53ec9d72c
Victor Rentea <victorrentea@gmail.com>
2023-03-09 16:34:26 +0200

    annotations @Mock

--- src/test/java/victor/testing/mocks/telemetry/DiagnosticTest.java
+++ src/test/java/victor/testing/mocks/telemetry/DiagnosticTest.java
@@ -2,6 +2,10 @@ package victor.testing.mocks.telemetry;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -9,17 +13,15 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 
+@ExtendWith(MockitoExtension.class) // this extension to Junit reflection on the fields and sees @Mock @InjectMocks
 public class DiagnosticTest {
 
 
-   private Diagnostic diagnostic = new Diagnostic();
-   private Client client = mock(Client.class);
+   @InjectMocks
+   private Diagnostic diagnostic;
+   @Mock
+   private Client client;
 
-   @BeforeEach
-   final void before() {
-      diagnostic.setTelemetryClient(client);
-
-   }
    @Test
    void throwsWhenNotOnline() {
       // an unstubbed mock method returns the 'null' value for the type,
