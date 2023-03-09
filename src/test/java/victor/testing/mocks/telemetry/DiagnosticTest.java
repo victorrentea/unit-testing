2ab8dd87d844767ec3bf6cc46b2b251f28aae71f
Victor Rentea <victorrentea@gmail.com>
2023-03-09 16:32:00 +0200

    Programmatic Mockito usage [avoid]

--- src/test/java/victor/testing/mocks/telemetry/DiagnosticTest.java
+++ src/test/java/victor/testing/mocks/telemetry/DiagnosticTest.java
@@ -1,5 +1,6 @@
 package victor.testing.mocks.telemetry;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,11 +12,18 @@ import static org.mockito.Mockito.*;
 public class DiagnosticTest {
 
 
+   private Diagnostic diagnostic;
+   private Client client;
+
+   @BeforeEach
+   final void before() {
+      diagnostic = new Diagnostic();
+      client = mock(Client.class);
+      diagnostic.setTelemetryClient(client);
+
+   }
    @Test
    void throwsWhenNotOnline() {
-      Diagnostic diagnostic = new Diagnostic();
-      Client client = mock(Client.class);
-      diagnostic.setTelemetryClient(client);
       // an unstubbed mock method returns the 'null' value for the type,
       //  boolean=false, Boolean=null, List=emptyList
        assertThatThrownBy(() -> diagnostic.checkTransmission(true))
@@ -25,9 +33,6 @@ public class DiagnosticTest {
 
    @Test
    void disconnects() {
-      Diagnostic diagnostic = new Diagnostic();
-      Client client = mock(Client.class);
-      diagnostic.setTelemetryClient(client);
       when(client.getOnlineStatus()).thenReturn(true); // "mock a method" = "stubbing": i am teaching a method what return
 
        diagnostic.checkTransmission(true);
@@ -38,9 +43,6 @@ public class DiagnosticTest {
 
    @Test
    void sendsDiagnosticMessage() {
-      Diagnostic diagnostic = new Diagnostic();
-      Client client = mock(Client.class);
-      diagnostic.setTelemetryClient(client);
       when(client.getOnlineStatus()).thenReturn(true); // "mock a method" = "stubbing": i am teaching a method what return
 
       diagnostic.checkTransmission(true);
