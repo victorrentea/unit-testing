4bb5d501430e625be05ff3735bd6d0faa0e66cf1
Victor Rentea <victorrentea@gmail.com>
2023-03-27 13:33:46 +0300

    refactored to use int for points

--- src/test/java/victor/testing/tdd/classic/TennisGame.java
+++ src/test/java/victor/testing/tdd/classic/TennisGame.java
@@ -1,36 +1,28 @@
 package victor.testing.tdd.classic;
 
 public class TennisGame {
-
-  // static ?!?
-  private String score = "Love - Love";
-  int player1Points = 0;
-  int player2Points = 0;
+  private int player1Points;
+  private int player2Points;
 
   public String score() {
-    String s = translatePoints(player1Points) + " - " + translatePoints(player2Points);
-
-    return score;
+    return translatePoints(player1Points) +
+           " - " + translatePoints(player2Points);
   }
 
   private String translatePoints(int points) {
-    if (points == 0) {
-      return "Love";
-    } else {
-      return "Fifteen";
-    }
+    return switch (points) {
+      case 0 -> "Love";
+      case 1 -> "Fifteen";
+      case 2 -> "Thirty";
+      default -> throw new IllegalArgumentException("No translation for " + points);
+    };
   }
 
   public void earnsPoint(TennisSide side) {
     if (side == TennisSide.ONE) {
-      player1Points ++ ;
-      score = "Fifteen - Love";
+      player1Points++;
     } else {
-      player2Points ++;
-//      if (score.equals("Love - Fifteen")) {
-//        score = "Love - Thirty";
-//      } else
-//        score = "Love - Fifteen";
+      player2Points++;
     }
   }
 }
