dfb69e34cdef5f98fd91e8f80eda999c3877630c
Victor Rentea <victorrentea@gmail.com>
2023-03-27 22:03:38 +0300

    characterization testing

--- src/main/java/victor/testing/filebased/export/PersonExporter.java
+++ src/main/java/victor/testing/filebased/export/PersonExporter.java
@@ -4,37 +4,39 @@ import java.io.IOException;
 import java.io.Writer;
 
 public class PersonExporter {
-   private final PersonRepo personRepo;
+  private final PersonRepo personRepo;
 
-   public PersonExporter(PersonRepo personRepo) {
-      this.personRepo = personRepo;
-   }
+  public PersonExporter(PersonRepo personRepo) {
+    this.personRepo = personRepo;
+  }
 
-   // This legacy code is in production for 7 years.
-   // So, it has no bugs, 🤞
-   //    but your task is to change it... 😱
-   // Ofc, you want to make sure you don't introduce bugs.
+  // This legacy code is in production for 7 years.
+  // Therefore, it has no bugs. 🤞
+  // Your plan to refactor it... 😱
+  // How can you make sure you don't break the current behavior?
 
-   // So: (** CHARACTERIZATION TESTS **)
-   // - You capture its current output
-   // - Find inputs to go "everywhere" (Line coverage helps here)
-   // - Save actual current output as tests
+  // You write CHARACTERIZATION TESTS:
+  // 1. You capture the relevant output of the code
+  // 2. Find proper inputs to go "everywhere" (use line coverage)
+  // 3. Save current output as tests along with the inputs
+  // These tests should stay green when refactoring the code.
 
-   // Then, when refactoring/evolving it, the tests should stay green.
-   public void export(Writer writer) throws IOException {
-      writer.write("full_name;phones;birth_date\n");
-      for (Person person : personRepo.findAll()) {
-         writer.write(person.getFirstName() + " " + person.getLastName().toUpperCase());
-         writer.write(";");
-         writer.write( person.getPhoneList().get(0)); // TODO fix bug when no phones
-            // TODO CR: output all phones comma-separated
-         writer.write(";");
-         if (person.getBirthDate() != null) {
-            writer.write(person.getBirthDate().toString()); // TODO CR: change format to "12 Nov 2021"
-         } else {
-            writer.write("N/A");
-         }
-         writer.write("\n");
+  public void export(Writer writer) throws IOException {
+    writer.write("full_name;phones;birth_date\n");
+    for (Person person : personRepo.findAll()) {
+      writer.write(person.getFirstName() + " " + person.getLastName().toUpperCase());
+      writer.write(";");
+      writer.write(person.getPhoneList().get(0));
+      // TODO BUGFIX: exception when no phones
+      // TODO CR: output all phones comma-separated
+
+      writer.write(";");
+      if (person.getBirthDate() != null) {
+        writer.write(person.getBirthDate().toString()); // TODO CR: change format to "12 Nov 2021"
+      } else {
+        writer.write("N/A");
       }
-   }
+      writer.write("\n");
+    }
+  }
 }
