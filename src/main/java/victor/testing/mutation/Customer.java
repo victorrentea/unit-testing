f8815883288ee8294a3b89122e440a50d935f002
Victor Rentea <victorrentea@gmail.com>
2023-03-27 12:24:45 +0300

    take1

--- src/main/java/victor/testing/mutation/Customer.java
+++ src/main/java/victor/testing/mutation/Customer.java
@@ -1,11 +1,14 @@
 package victor.testing.mutation;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.Value;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+//@Value @Builder // lombok's immutable class
 @Data
 public class Customer {
 	private Long id;
@@ -15,4 +18,9 @@ public class Customer {
 	private Address address = new Address();
 	private Date createDate;
 	private List<Coupon> coupons = new ArrayList<>();
+
+	public Customer setName(String name) {
+		this.name = name;
+		return this;
+	}
 }
--- src/test/java/victor/testing/mutation/CustomerValidatorTest.java
+++ src/test/java/victor/testing/mutation/CustomerValidatorTest.java
@@ -4,30 +4,65 @@ package victor.testing.mutation;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 
 public class CustomerValidatorTest {
-   CustomerValidator validator = new CustomerValidator();
+  CustomerValidator validator = new CustomerValidator();
+  private Customer validCustomer = new Customer()
+          .setName("::name::")
+          .setEmail("::email::")
+          .setAddress(new Address().setCity("::city::"));
 
-   @Test
-   void valid() {
-      Customer customer = new Customer();
-      customer.setName("::name::");
-      customer.setEmail("::email::");
-      customer.setAddress(new Address());
-      customer.getAddress().setCity("::city::");
-      validator.validate(customer);
-   }
+  @Test
+  void valid() {
+    validator.validate(validCustomer);
+  }
 
-   @Test
-   void throwForMissingName() {
-      Customer customer = new Customer();
-      customer.setEmail("::email::");
-      customer.setAddress(new Address());
-      customer.getAddress().setCity("::city::");
-      Assertions.assertThatThrownBy(() -> validator.validate(customer))
-              .isInstanceOf(IllegalArgumentException.class)
-              .hasMessage("Missing customer name")
-      ;
-   }
+  @Test
+  void throwForMissingName() {
+    Customer customer = validCustomer.setName(null);
+    assertThatThrownBy(() -> validator.validate(customer))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("Missing customer name")
+    ;
+  }
+
+
+  @Test
+  void throwForMissingEmail() {
+    //      Customer customer =validCustomer.builder().withEmail(null).build();
+    Customer customer = validCustomer.setEmail(null);
+    assertThatThrownBy(() -> validator.validate(customer))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("Missing customer email")
+    ;
+  }
+
+  @Test
+  void throwForMissingCity() {
+    validCustomer.getAddress().setCity(null);
+    assertThatThrownBy(() -> validator.validate(validCustomer))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("Missing address city")
+    ;
+  }
+
+  @Test
+  void throwForTooShortCity() {
+    validCustomer.getAddress().setCity("12");
+    assertThatThrownBy(() -> validator.validate(validCustomer))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("Address city too short")
+    ;
+  }
+
+  @Test
+  void trimAddressCity() {
+    validCustomer.getAddress().setCity(" 123 ");
+    validator.validate(validCustomer);
+    assertThat(validCustomer.getAddress().getCity())
+            .isEqualTo("123");
+  }
 
 }
