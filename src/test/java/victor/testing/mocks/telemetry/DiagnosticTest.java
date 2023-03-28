b2aa26d9bdb7ccf7363b843ab1a1055fb496a520
Victor Rentea <victorrentea@gmail.com>
2023-03-28 12:43:17 +0300

    first 3 tests, you just write them, anyway you can. then refactor

--- src/test/java/victor/testing/mocks/telemetry/DiagnosticTest.java
+++ src/test/java/victor/testing/mocks/telemetry/DiagnosticTest.java
@@ -1,60 +1,42 @@
 package victor.testing.mocks.telemetry;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static victor.testing.mocks.telemetry.Diagnostic.DIAGNOSTIC_CHANNEL_CONNECTION_STRING;
 
-
-@ExtendWith(MockitoExtension.class)
 public class DiagnosticTest {
-   @Mock
-   private Client client;
-   @InjectMocks
-   private Diagnostic target;
 
-   @Test
-   public void disconnects() {
-      when(client.getOnlineStatus()).thenReturn(true);
-      target.checkTransmission(true);
-      verify(client).disconnect(true);
-   }
+  @Test
+  void disconnects() {
+    Client mockClient = mock(Client.class);
+    when(mockClient.getOnlineStatus()).thenReturn(true);
+    Diagnostic sut = new Diagnostic(mockClient);
 
-   @Test
-   public void throwsWhenNotOnline() {
-      when(client.getOnlineStatus()).thenReturn(false);
-      assertThatThrownBy(() ->
-              target.checkTransmission(true)).isInstanceOf(IllegalStateException.class);
-   }
+    sut.checkTransmission(true);
 
-   @Test
-   public void sendsDiagnosticInfo() {
-      when(client.getOnlineStatus()).thenReturn(true);
-      target.checkTransmission(true);
-      verify(client).send(Client.DIAGNOSTIC_MESSAGE);
-   }
+    verify(mockClient).disconnect(true);
+  }
+  @Test
+  void connects() {
+    Client mockClient = mock(Client.class);
+    when(mockClient.getOnlineStatus()).thenReturn(false, true);
+    Diagnostic sut = new Diagnostic(mockClient);
 
-   @Test
-   public void receivesDiagnosticInfo() {
-      when(client.getOnlineStatus()).thenReturn(true);
-      when(client.receive()).thenReturn("tataie");
-      target.checkTransmission(true);
-      verify(client).receive();
-      assertThat(target.getDiagnosticInfo()).isEqualTo("tataie");
-   }
+    sut.checkTransmission(true);
 
-   @Test
-   public void configuresClient() throws Exception {
-      when(client.getOnlineStatus()).thenReturn(true);
-      target.checkTransmission(true);
-      verify(client).configure(any());
-      // TODO check config.getAckMode is NORMAL
-   }
+    verify(mockClient).connect(DIAGNOSTIC_CHANNEL_CONNECTION_STRING);
+  }
+
+  @Test
+  void throwsWhenCannotConnect() {
+    Client mockClient = mock(Client.class);
+    when(mockClient.getOnlineStatus()).thenReturn(false);
+    Diagnostic sut = new Diagnostic(mockClient);
+
+    Assertions.assertThatThrownBy(()-> sut.checkTransmission(true))
+      .isInstanceOf(IllegalStateException.class)
+      .hasMessage("Unable to connect.");
+  }
 }
