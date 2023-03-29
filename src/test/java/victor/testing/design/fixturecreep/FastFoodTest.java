38250eae97db291bc86d0f43d3150ad7726ce800
Victor Rentea <victorrentea@gmail.com>
2023-03-29 11:57:28 +0300

    nested test classes

--- src/test/java/victor/testing/design/fixturecreep/FastFoodTest.java
+++ src/test/java/victor/testing/design/fixturecreep/FastFoodTest.java
@@ -1,37 +1,54 @@
 package victor.testing.design.fixturecreep;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class FastFoodTest {
-   @Mock
-   Dependency dependency;
-   @InjectMocks
-   FastFood fastFood;
-
+  @Mock
+  Dependency dependency;
+  @InjectMocks
+  FastFood fastFood;
    @BeforeEach
    final void before() {
-
+      System.out.println("run for both");
    }
 
-   @Test
-   void shawarmaTest() { // + 7 more tests
+  @Nested
+  class Shawarma {
+    @BeforeEach
+    final void before() {
       when(dependency.isOnionAllowed()).thenReturn(true);
+    }
+
+    @Test
+    void shawarmaTest() { // + 7 more tests
       // ... complex
       fastFood.makeShawarma();
-   }
+    }
 
-   @Test
-   void tzatzikiTest() { // + 5 more tests
+  }
+
+  @Nested
+  class Tzatziki {
+    @BeforeEach
+    final void before() {
       when(dependency.isCucumberAllowed()).thenReturn(true);
+    }
+
+    @Test
+    void tzatzikiTest() { // + 5 more tests
       // ... complex
       fastFood.makeTzatziki();
-   }
+    }
+  }
+
+
 }
