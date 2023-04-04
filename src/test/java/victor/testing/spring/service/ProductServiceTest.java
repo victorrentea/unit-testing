7e3d2cd805cb75a9c421234b0553e2decdbc4e42
Victor Rentea <victorrentea@gmail.com>
2023-04-05 01:09:38 +0300

    end

--- src/test/java/victor/testing/spring/service/ProductServiceTest.java
+++ src/test/java/victor/testing/spring/service/ProductServiceTest.java
@@ -1,17 +1,12 @@
 package victor.testing.spring.service;
 
-import net.bytebuddy.utility.nullability.NeverNull.ByDefault;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -19,7 +14,6 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import victor.testing.spring.domain.Product;
-import victor.testing.spring.domain.ProductCategory;
 import victor.testing.spring.domain.Supplier;
 import victor.testing.spring.infra.SafetyClient;
 import victor.testing.spring.repo.ProductRepo;
@@ -27,69 +21,74 @@ import victor.testing.spring.repo.SupplierRepo;
 import victor.testing.spring.web.dto.ProductDto;
 import victor.testing.tools.TestcontainersUtils;
 
-import java.util.Optional;
-
 import static java.time.LocalDateTime.now;
 import static java.time.temporal.ChronoUnit.SECONDS;
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 import static victor.testing.spring.domain.ProductCategory.HOME;
 
 @Testcontainers
 @SpringBootTest
 @ActiveProfiles("db-migration")
 public class ProductServiceTest {
-   @MockBean
-   public SafetyClient mockSafetyClient;
-   @Autowired
-   private ProductRepo productRepo;
-   @Autowired
-   private SupplierRepo supplierRepo;
-   @Autowired
-   private ProductService productService;
+  @MockBean
+  public SafetyClient mockSafetyClient;
+  @Autowired
+  private ProductRepo productRepo;
+  @Autowired
+  private SupplierRepo supplierRepo;
+  @Autowired
+  private ProductService productService;
 
-   @Container
-   static public PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
-           "postgres:11")
-           .withReuse(true);
+  @Container
+  static public PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+          "postgres:11")
+          .withReuse(true);
 
-   @BeforeAll
-   public static void startTestcontainer() {
-      System.out.println("Starting testcontainer");
-      postgres.start();
-   }
+  @BeforeAll
+  public static void startTestcontainer() {
+    System.out.println("Starting testcontainer");
+    postgres.start();
+  }
 
-   @DynamicPropertySource
-   public static void registerPgProperties(DynamicPropertyRegistry registry) {
-      TestcontainersUtils.addDatasourceDetails(registry, postgres, true);
-   }
+  @AfterEach
+  public void method() {
+    productRepo.deleteAll();
+    supplierRepo.deleteAll();
+  }
 
-   @Test
-   public void createThrowsForUnsafeProduct() {
-      when(mockSafetyClient.isSafe("bar")).thenReturn(false);
 
-      ProductDto dto = new ProductDto("name", "bar", -1L, HOME);
-      assertThatThrownBy(() -> productService.createProduct(dto))
-              .isInstanceOf(IllegalStateException.class);
-   }
+  @DynamicPropertySource
+  public static void registerPgProperties(DynamicPropertyRegistry registry) {
+    TestcontainersUtils.addDatasourceDetails(registry, postgres, true);
+  }
 
-   @Test
-   public void createOk() {
-      // GIVEN
-      Long supplierId = supplierRepo.save(new Supplier()).getId();
-      when(mockSafetyClient.isSafe("safebar")).thenReturn(true);
-      ProductDto dto = new ProductDto("name", "safebar", supplierId, HOME);
+  @Test
+  public void createThrowsForUnsafeProduct() {
+    when(mockSafetyClient.isSafe("bar")).thenReturn(false);
 
-      // WHEN
-      Long id = productService.createProduct(dto);
+    ProductDto dto = new ProductDto("name", "bar", -1L, HOME);
+    assertThatThrownBy(() -> productService.createProduct(dto))
+            .isInstanceOf(IllegalStateException.class);
+  }
 
-      // THEN
-      Product product = productRepo.findById(id).orElseThrow();
-      assertThat(product.getName()).isEqualTo("name");
-      assertThat(product.getBarcode()).isEqualTo("safebar");
-      assertThat(product.getSupplier().getId()).isEqualTo(supplierId);
-      assertThat(product.getCategory()).isEqualTo(HOME);
-      assertThat(product.getCreateDate()).isCloseTo(now(), byLessThan(1, SECONDS));
-   }
+  @Test
+  public void createOk() {
+    // GIVEN
+    Long supplierId = supplierRepo.save(new Supplier()).getId();
+    when(mockSafetyClient.isSafe("safebar")).thenReturn(true);
+    ProductDto dto = new ProductDto("name", "safebar", supplierId, HOME);
+
+    // WHEN
+    Long id = productService.createProduct(dto);
+
+    // THEN
+    Product product = productRepo.findById(id).orElseThrow();
+    assertThat(product.getName()).isEqualTo("name");
+    assertThat(product.getBarcode()).isEqualTo("safebar");
+    assertThat(product.getSupplier().getId()).isEqualTo(supplierId);
+    assertThat(product.getCategory()).isEqualTo(HOME);
+    assertThat(product.getCreateDate()).isCloseTo(now(), byLessThan(1, SECONDS));
+  }
 
 }
