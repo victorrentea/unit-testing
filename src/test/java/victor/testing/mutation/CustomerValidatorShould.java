a1cde6bb52e234f76194da18b218f285aeb19a58
Victor Rentea <victorrentea@gmail.com>
2023-04-26 10:42:43 +0300

    100% branch coverage si totusi REJECT PR: "testele-s varza" - cazuri de prod ne testate - codul din test praf

--- src/test/java/victor/testing/mutation/CustomerValidatorShould.java
+++ src/test/java/victor/testing/mutation/CustomerValidatorShould.java
@@ -4,13 +4,17 @@ package victor.testing.mutation;
 // junit 4
 import org.junit.Assert;
 import org.junit.Rule;
-import org.junit.Test;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.rules.ExpectedException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CustomerValidatorShould {
    CustomerValidator validator = new CustomerValidator();
 
-   // Junit 5 exceptiile se fac cu assertThrows
+   // Junit 5 exceptiile se fac cu assertThrows -> BEST: assertThatThrownBy( -> ).isInstanceOf...
    // JUnit 4 metodele @Test publice
    // JUnit 5 nu mai are @Rule ci @RegisterExtension
    @Test
@@ -29,21 +33,47 @@ public class CustomerValidatorShould {
       customer.setEmail("::email::");
       customer.setAddress(new Address());
       customer.getAddress().setCity("::city::");
-      IllegalArgumentException e = Assert.assertThrows(IllegalArgumentException.class,
-              () -> validator.validate(customer));
-      Assert.assertEquals("Missing customer name", e.getMessage());
+      assertThatThrownBy(() -> validator.validate(customer))
+              .hasMessage("Missing customer name")
+              .isInstanceOf(IllegalArgumentException.class);
+//      IllegalArgumentException e = Assert.assertThrows(IllegalArgumentException.class,
+//              () -> validator.validate(customer));
+//      Assert.assertEquals("Missing customer name", e.getMessage());
    }
    @Test//(expected = IllegalArgumentException.class)
    public void throwsForMissingCustomerEmail() { //B❤️
       Customer customer = new Customer();
       customer.setAddress(new Address());
-//      customer.setName("nu nume");
+      customer.setName("nu nume");
       customer.getAddress().setCity("::city::");
-      IllegalArgumentException e = Assert.assertThrows(IllegalArgumentException.class,
-              () -> validator.validate(customer));
-      Assert.assertEquals("Missing customer email", e.getMessage());
+//      IllegalArgumentException e = Assert.assertThrows(IllegalArgumentException.class,
+//              () -> validator.validate(customer));
+//      Assert.assertEquals("Missing customer email", e.getMessage());
+      assertThatThrownBy(() -> validator.validate(customer))
+              .hasMessage("Missing customer email")
+              .isInstanceOf(IllegalArgumentException.class);
    }
 
-//   @Rule
-//   ExpectedException
+   @Test
+   public void throwsForNullCity() {
+      Customer customer = new Customer();
+      customer.setName("::name::");
+      customer.setEmail("::email::");
+      customer.setAddress(new Address());
+      assertThatThrownBy(() -> validator.validate(customer))
+              .hasMessage("Missing address city")
+              .isInstanceOf(IllegalArgumentException.class);
+   }
+   @Test
+   public void throwsForCityTooShort() {
+      Customer customer = new Customer();
+      customer.setName("::name::");
+      customer.setEmail("::email::");
+      customer.setAddress(new Address());
+      customer.getAddress().setCity("Go");
+      assertThatThrownBy(() -> validator.validate(customer))
+              .hasMessage("Address city too short")
+              .isInstanceOf(IllegalArgumentException.class);
+   }
+
 }
