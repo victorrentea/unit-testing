e3959d796c699b3767894435fa4d795fa62e8f93
Victor Rentea <victorrentea@gmail.com>
2023-04-26 11:57:40 +0300

    cleanup

--- src/test/java/victor/testing/tdd/TennisScore.java
+++ src/test/java/victor/testing/tdd/TennisScore.java
@@ -3,25 +3,15 @@ package victor.testing.tdd;
 import java.util.Map;
 
 public class TennisScore {
-//Map<player, String >
-//  LinkedList
   private static final String[] scoreLabels = {"Love", "Fifteen", "Thirty"};
   private int player1Points;
   private int player2Points;
-  private String score = "Love-Love";
 
   public String getScore() {
     return scoreLabels[player1Points] + "-Love";
-//    return score;
   }
 
   public void winsPoint(Player player) {
-    if (score.equals("Fifteen-Love")) {
-      score = "Thirty-Love";
-    } else {
-      score = "Fifteen-Love";
-    }
-    // if todo
     player1Points++;
   }
 }
