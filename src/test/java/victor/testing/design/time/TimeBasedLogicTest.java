e40e205e5a24cfde8aeb5259fd84c379836dcb53
Victor Rentea <victorrentea@gmail.com>
2023-04-26 16:14:42 +0300

    #1 mockuri statice! nu mai sunt asa urate ca acum 2-5-10-15 ani. + mockitoInline

--- src/test/java/victor/testing/design/time/TimeBasedLogicTest.java
+++ src/test/java/victor/testing/design/time/TimeBasedLogicTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
@@ -39,4 +41,20 @@ class TimeBasedLogicTest {
       // 4: pass time as method arg
       // 5: package-protected variant for testing
       // 6: mock now() - mocks all methods (try to add another LD.parse in the prod code) >> mockStatic(, CALLS_REAL_METHODS)
+
+
+   @Test
+   void isFrequentBuyer_cuMockuriStatice() {
+      when(orderRepo.findByCustomerIdAndCreatedOnBetween(
+              13, parse("2023-12-18"), parse("2023-12-25"))).thenReturn(List.of(new Order().setTotalAmount(130d)));
+      LocalDate craciun = parse("2023-12-25");
+      // cu ajutorul mockito-inline, poti scrie:
+
+      try(MockedStatic<LocalDate> staticMock = Mockito.mockStatic(LocalDate.class)) {
+         staticMock.when(() -> LocalDate.now()).thenReturn(craciun);
+
+         boolean result = target.isFrequentBuyer(13);
+         assertThat(result).isTrue();
+      }
+   }
 }
