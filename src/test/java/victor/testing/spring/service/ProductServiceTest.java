4e2681910159589ceba1f33ac86797976081aab2
Victor Rentea <victorrentea@gmail.com>
2023-04-27 16:04:40 +0300

    cleanup

--- src/test/java/victor/testing/spring/service/ProductServiceTest.java
+++ src/test/java/victor/testing/spring/service/ProductServiceTest.java
@@ -1,30 +1,21 @@
 package victor.testing.spring.service;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import victor.testing.spring.domain.Product;
-import victor.testing.spring.domain.ProductCategory;
 import victor.testing.spring.domain.Supplier;
 import victor.testing.spring.infra.SafetyClient;
 import victor.testing.spring.repo.ProductRepo;
 import victor.testing.spring.repo.SupplierRepo;
 import victor.testing.spring.web.dto.ProductDto;
 
-import java.util.Optional;
-
 import static java.time.LocalDateTime.now;
 import static java.time.temporal.ChronoUnit.SECONDS;
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 import static victor.testing.spring.domain.ProductCategory.HOME;
 
 //@ExtendWith(MockitoExtension.class)
@@ -33,41 +24,38 @@ import static victor.testing.spring.domain.ProductCategory.HOME;
 @SpringBootTest
 @ActiveProfiles("db-mem-migration")
 public class ProductServiceTest {
-   @MockBean // inlocuieste un bean de spring cu un mockito mock pe care-l si pune aici sa-l programezi
-   public SafetyClient mockSafetyClient;
-   @Autowired
-   private ProductRepo productRepo;
-   @Autowired
-   private SupplierRepo supplierRepo;
-   @Autowired
-   private ProductService productService;
+  @MockBean // inlocuieste un bean de spring cu un mockito mock pe care-l si pune aici sa-l programezi
+  public SafetyClient mockSafetyClient;
+  @Autowired
+  private ProductRepo productRepo;
+  @Autowired
+  private SupplierRepo supplierRepo;
+  @Autowired
+  private ProductService productService;
 
-   @Test
-   public void createThrowsForUnsafeProduct() {
-      when(mockSafetyClient.isSafe("bar")).thenReturn(false);
+  @Test
+  public void createThrowsForUnsafeProduct() {
+    when(mockSafetyClient.isSafe("bar")).thenReturn(false);
 
-      ProductDto dto = new ProductDto("name", "bar", -1L, HOME);
-      assertThatThrownBy(() -> productService.createProduct(dto))
-              .isInstanceOf(IllegalStateException.class);
-   }
+    ProductDto dto = new ProductDto("name", "bar", -1L, HOME);
+    assertThatThrownBy(() -> productService.createProduct(dto))
+            .isInstanceOf(IllegalStateException.class);
+  }
 
-   @Test
-   public void createOk() {
-      // GIVEN
-      Long supplierId = supplierRepo.save(new Supplier()).getId();
-      when(mockSafetyClient.isSafe("safebar")).thenReturn(true);
-      ProductDto dto = new ProductDto("name", "safebar", supplierId, HOME);
+  @Test
+  public void createOk() {
+    Long supplierId = supplierRepo.save(new Supplier()).getId();
+    when(mockSafetyClient.isSafe("safebar")).thenReturn(true);
+    ProductDto dto = new ProductDto("name", "safebar", supplierId, HOME);
 
-      // WHEN
-      Long productId = productService.createProduct(dto);
+    Long productId = productService.createProduct(dto);
 
-      // THEN
-      Product product = productRepo.findById(productId).orElseThrow();
-      assertThat(product.getName()).isEqualTo("name");
-      assertThat(product.getBarcode()).isEqualTo("safebar");
-      assertThat(product.getSupplier().getId()).isEqualTo(supplierId);
-      assertThat(product.getCategory()).isEqualTo(HOME);
-       assertThat(product.getCreateDate()).isCloseTo(now(), byLessThan(1, SECONDS)); // uses Spring Magic
-   }
+    Product product = productRepo.findById(productId).orElseThrow();
+    assertThat(product.getName()).isEqualTo("name");
+    assertThat(product.getBarcode()).isEqualTo("safebar");
+    assertThat(product.getSupplier().getId()).isEqualTo(supplierId);
+    assertThat(product.getCategory()).isEqualTo(HOME);
+    assertThat(product.getCreateDate()).isCloseTo(now(), byLessThan(1, SECONDS)); // uses Spring Magic
+  }
 
 }
