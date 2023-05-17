7019ddb504190b10b899c716a4d6c42ea0acf97b
Victor Rentea <victorrentea@gmail.com>
2023-05-17 16:26:33 +0300

    cleaner

--- src/test/java/victor/testing/tdd/classic/TennisGameParameterizedJava17Test.java
+++ src/test/java/victor/testing/tdd/classic/TennisGameParameterizedJava17Test.java
@@ -11,14 +11,14 @@ import static org.assertj.core.api.Assertions.in;
 public class TennisGameParameterizedJava17Test {
   TennisGame game = new TennisGame();
 
-  public static Stream<Input> date() {
+  static Stream<Input> date() {
     return Stream.of(
         new Input(7, 7, "Deuce"),
         new Input(3, 2, "Forty:Thirty")
         // min 4-5 combinatii
     );
   }
-  private record Input(int points1, int points2, String expectedScoreString){}
+  record Input(int points1, int points2, String expectedScoreString){}
 
   @ParameterizedTest
   @MethodSource("date")
@@ -27,11 +27,11 @@ public class TennisGameParameterizedJava17Test {
     String actual = game.getScore();
     assertThat(actual).isEqualTo(input.expectedScoreString);
   }
-  public void addPoints(int player1Points, int player2Points) {
+  void addPoints(int player1Points, int player2Points) {
     addPointsToPlayer(Player.ONE, player1Points);
     addPointsToPlayer(Player.TWO, player2Points);
   }
-  public void addPointsToPlayer(Player player, int points) {
+  void addPointsToPlayer(Player player, int points) {
     for (int i = 0; i < points; i++) {
       game.addPoint(player);
     }
