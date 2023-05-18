a30f840ed98f651100426ae73aefd7a695ec2aaf
Victor Rentea <victorrentea@gmail.com>
2023-05-18 12:37:11 +0300

    am spart testul pentru a putea face before-ul sa fie UTIL tuturor testelor.

---
+++ src/test/java/victor/testing/design/fixturecreep/ShawarmaTest.java
@@ -0,0 +1,31 @@
+package victor.testing.design.fixturecreep;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.when;
+
+//@MockitoSettings(strictness = Strictness.LENIENT)
+@ExtendWith(MockitoExtension.class)
+class ShawarmaTest {
+  @Mock // strict mock; see other ways to get strict mocks in the 'strictstubs' package
+  Dependency dependency;
+  @InjectMocks
+  FastFood fastFood;
+
+  @BeforeEach
+  final void before() {
+    when(dependency.isOnionAllowed()).thenReturn(true); // eu stabuiesc o metoda
+  }
+
+  @Test
+  void shawarmaTest() { // + 7 more tests
+    // ... complex
+    fastFood.makeShawarma();
+  }
+
+}
