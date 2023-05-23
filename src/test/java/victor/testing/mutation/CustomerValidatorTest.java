2d8e08d3f25bf41e5448e2c7e15f7952fbd687b0
Victor Rentea <victorrentea@gmail.com>
2023-05-23 12:22:32 +0300

    metoda factory ce produce date 'standard', in cazul meu valide, pe care la hackuiesc prin testele mele

--- src/test/java/victor/testing/mutation/CustomerValidatorTest.java
+++ src/test/java/victor/testing/mutation/CustomerValidatorTest.java
@@ -1,6 +1,7 @@
 package victor.testing.mutation;
 
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
@@ -8,32 +9,53 @@ import org.junit.jupiter.api.Test;
 public class CustomerValidatorTest {
    CustomerValidator validator = new CustomerValidator();
 
-   @Test
-   void valid() {
+   @NotNull
+   private static Customer validCustomer() {
       Customer customer = new Customer();
       customer.setName("::name::");
       customer.setEmail("::email::");
       customer.setAddress(new Address());
       customer.getAddress().setCity("::city::");
+      return customer;
+   }
+
+   @Test
+   void valid() {
+      Customer customer = validCustomer();
       validator.validate(customer);
    }
 
    @Test
+       // givenAAcustomer_whenHerNameIsNull_thenThrowsException
 //   void invalidCustomerName_throwsException() {
 //   void throwsExceptionWhenInvalidCustomerName() {
 //   void throwsWhenInvalidCustomerName() {
 //   void throwsForInvalidCustomerName() {
 //   void throwsForInvalidName() {
    void throwsForNullName() {
-      Customer customer = new Customer();
-      customer.setEmail("::email::");
-      customer.setAddress(new Address());
-      customer.getAddress().setCity("::city::");
+      Customer customer = validCustomer();
+      customer.setName(null);
 
       Assert.assertThrows(IllegalArgumentException.class,
           ()->validator.validate(customer));
    }
 
+   @Test
+   void throwsForNullEmail() {
+      Customer customer = validCustomer();
+      customer.setEmail(null);
 
+      Assert.assertThrows(IllegalArgumentException.class,
+          ()->validator.validate(customer));
+   }
+
+   @Test
+   void throwsForNullAddressCity() {
+      Customer customer = validCustomer();
+      customer.getAddress().setCity(null);
+
+      Assert.assertThrows(IllegalArgumentException.class,
+          ()->validator.validate(customer));
+   }
 
 }
