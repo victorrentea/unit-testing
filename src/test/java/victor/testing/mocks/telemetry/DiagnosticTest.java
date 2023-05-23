c7092b0f029eb29dd46add56c15db10a7490402f
Victor Rentea <victorrentea@gmail.com>
2023-05-23 18:01:39 +0300

    HOMEWORK

--- src/test/java/victor/testing/mocks/telemetry/DiagnosticTest.java
+++ src/test/java/victor/testing/mocks/telemetry/DiagnosticTest.java
@@ -22,59 +22,9 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
-@ExtendWith(MockitoExtension.class)
 public class DiagnosticTest {
-   @Mock
-   private Client client;
-   @InjectMocks
-   private Diagnostic target;
-
-   @BeforeEach
-   final void before() {
-      when(client.getOnlineStatus()).thenReturn(true);
-   }
-
-   @Test
-   public void disconnects() {
-      target.checkTransmission(true);
-      verify(client).disconnect(true);
-   }
-
-   @Test
-   public void throwsWhenNotOnline() {
-      when(client.getOnlineStatus()).thenReturn(false);
-      assertThatThrownBy(() -> target.checkTransmission(true))
-              .isInstanceOf(IllegalStateException.class);
-   }
-
-   @Test
-   public void sendsDiagnosticInfo() {
-      target.checkTransmission(true);
-      verify(client).send(Client.DIAGNOSTIC_MESSAGE);
-   }
-
-   @Test
-   public void receivesDiagnosticInfo() {
-      final String diagnosticMessage = "DIAG";
-      when(client.receive()).thenReturn(diagnosticMessage);
-      target.checkTransmission(true);
-      verify(client).receive();
-      assertThat(target.getDiagnosticInfo()).isEqualTo(diagnosticMessage);
-   }
-
-   @Captor
-   ArgumentCaptor<ClientConfiguration> configCaptor;
-
-   @Test
-   public void configuresClient() throws Exception {
-      when(client.getVersion()).thenReturn("ver");
-
-      target.checkTransmission(true);
-
-      verify(client).configure(configCaptor.capture());
-      ClientConfiguration config = configCaptor.getValue();
-      assertThat(config.getAckMode()).isEqualTo(AckMode.NORMAL);
-      assertThat(config.getSessionStart()).isCloseTo(now(), byLessThan(1, SECONDS));
-      assertThat(config.getSessionId()).startsWith("ver-").hasSize("ver-".length() + 36);
-   }
+  // TODO test the DIagnostic.java class so that
+  //   every line I comment out of it, it fails a test
+  //  don't change the public signatures
+  // send the test class to victorrentea@gmail.com by tomorrow 15:30
 }
