f251ef2ed2aa8cee1bbf590a3d1fe9aaceb039cf
Victor Rentea <victorrentea@gmail.com>
2023-05-24 12:50:28 +0300

    FLAKY tests FLAKY, typical for UI/             async testing that             involves sleep(1000)

--- src/main/java/victor/testing/tdd/tennis/TennisGame.java
+++ src/main/java/victor/testing/tdd/tennis/TennisGame.java
@@ -11,7 +11,7 @@ public class TennisGame {
   public void addPoint(Player player) {
     if (Math.random() < .5) {
 
-    score = "Fifteen-Love";
+      score = "Fifteen-Love";
     } else {
       score = "Thirty-Love";
     }
