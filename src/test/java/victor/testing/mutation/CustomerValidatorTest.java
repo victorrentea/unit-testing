2b5a991357811c13fceceb4842143865cb3b05fb
Victor Rentea <victorrentea@gmail.com>
2023-05-24 11:19:16 +0300

    customer in a field initialized in a shared fixture (@BeforeEach)

--- src/test/java/victor/testing/mutation/CustomerValidatorTest.java
+++ src/test/java/victor/testing/mutation/CustomerValidatorTest.java
@@ -1,6 +1,8 @@
 package victor.testing.mutation;
 
 
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -8,40 +10,54 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CustomerValidatorTest {
    CustomerValidator validator = new CustomerValidator();
+   Customer customer;
 
-   @Test
-   void acceptsValidCustomer() {
+   private static Customer validCustomer() { // factory method for test data
       Customer customer = new Customer();
       customer.setName("::name::");
       customer.setEmail("::email::");
       customer.setAddress(new Address());
       customer.getAddress().setCity("::city::");
+      return customer;
+   }
+   @BeforeEach // run code before every @Test
+   final void fixture() {
+      customer = validCustomer();
+   }
+
+   @Test
+   void acceptsValidCustomer() {
       validator.validate(customer);
    }
 
-//   void invalidName() {
+   //   void invalidName() {
 //   void shouldThrowForMissingName() {
 //   void givenInvalidName_shouldThrow() {
 //   void shouldThrowGivenInvalidName() {
    @Test
    void throwsForMissingName() {
-      Customer customer = new Customer();
-      customer.setEmail("::email::");
-      customer.setAddress(new Address());
-      customer.getAddress().setCity("::city::");
+      customer.setName(null);
 
       assertThatThrownBy(() -> validator.validate(customer))
           .isInstanceOf(IllegalArgumentException.class);
    }
+
    @Test
    void throwsForMissingEmail() {
-      Customer customer = new Customer();
-      customer.setName("::name::");
-      customer.setAddress(new Address());
-      customer.getAddress().setCity("::city::");
+      customer.setEmail(null);
 
       assertThatThrownBy(() -> validator.validate(customer))
           .isInstanceOf(IllegalArgumentException.class)
           .hasMessage("Missing customer email");
    }
+
+   @Test
+   void throwsForMissingAddressCity() {
+      customer.getAddress().setCity(null);
+
+      assertThatThrownBy(() -> validator.validate(customer))
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("Missing address city");
+   }
+
 }
