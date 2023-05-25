577ca0fe46fb6ff28ac70281f1bc2fac5c893be5
Victor Rentea <victorrentea@gmail.com>
2023-05-25 15:22:59 +0300

    // we mock behavior not data.       // we instantiate and fill data

--- src/main/java/victor/testing/design/signatures/Precise.java
+++ src/main/java/victor/testing/design/signatures/Precise.java
@@ -2,16 +2,21 @@ package victor.testing.design.signatures;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
 public class Precise {
    public static void sendSprintFinishedEmail(Project project) {
-      System.out.println("Sending email to " + project.getPoEmail() + " with subject 'Sprint Finished' and some body");
+      System.out.println("Sending email to " + project.getPoEmail() +
+                         " with subject 'Sprint Finished' and some body");
    }
 }
 
 @Data
+@Entity
 class Project {
+   @Id
    private Long id;
    private String code;
    private String name;
@@ -19,4 +24,5 @@ class Project {
    private String poEmail;
    private LocalDate startDate;
    // 10 more fields
+
 }
--- src/test/java/victor/testing/design/signatures/PreciseTest.java
+++ src/test/java/victor/testing/design/signatures/PreciseTest.java
@@ -1,17 +1,26 @@
 package victor.testing.design.signatures;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import victor.testing.tools.CaptureSystemOutput;
 import victor.testing.tools.CaptureSystemOutput.OutputCapture;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class PreciseTest {
+   @Mock Project project;
    @Test
    @CaptureSystemOutput
    void sendSprintFinishedEmail(OutputCapture outputCapture) {
-      Project project = new Project();
-      project.setPoEmail("boss@my.corp");
+//      Project project = new Project();
+//      project.setPoEmail("boss@my.corp");
+      when(project.getPoEmail()).thenReturn("boss@my.corp"); // PR request: NEVER MOCK GETTERS.
+      // we mock behavior not data.
+      // we instantiate and fill data
 
       Precise.sendSprintFinishedEmail(project);
 
