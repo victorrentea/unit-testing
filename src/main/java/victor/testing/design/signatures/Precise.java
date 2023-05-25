5d9bb9384859576f046eb4881a028b3321bd9656
Victor Rentea <victorrentea@gmail.com>
2023-05-25 15:26:40 +0300

    whenever a test is hard to write, you can ALWAYS SHOVEL SOME SH*T INTO A HELPER METHOD.
    But here there is a simpler solution to the problem, that also improves the production design.

--- src/main/java/victor/testing/design/signatures/Precise.java
+++ src/main/java/victor/testing/design/signatures/Precise.java
@@ -7,8 +7,9 @@ import javax.persistence.Id;
 import java.time.LocalDate;
 
 public class Precise {
-   public static void sendSprintFinishedEmail(Project project) {
-      System.out.println("Sending email to " + project.getPoEmail() +
+   // happier prod
+   public static void sendSprintFinishedEmail(String poEmail) {
+      System.out.println("Sending email to " + poEmail +
                          " with subject 'Sprint Finished' and some body");
    }
 }
--- src/test/java/victor/testing/design/signatures/PreciseTest.java
+++ src/test/java/victor/testing/design/signatures/PreciseTest.java
@@ -1,6 +1,5 @@
 package victor.testing.design.signatures;
 
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import victor.testing.tools.CaptureSystemOutput;
 import victor.testing.tools.CaptureSystemOutput.OutputCapture;
@@ -11,19 +10,20 @@ class PreciseTest {
    @Test
    @CaptureSystemOutput
    void sendSprintFinishedEmail(OutputCapture outputCapture) {
-      Project project = projectWithPOEmail("boss@my.corp");
-
-      Precise.sendSprintFinishedEmail(project);
+      // happier test
+      Precise.sendSprintFinishedEmail("boss@my.corp");
 
       assertThat(outputCapture.toString()).isEqualToIgnoringNewLines("Sending email to boss@my.corp with subject 'Sprint Finished' and some body");
    }
 
-   // whenever a test is hard to write, you can ALWAYS SHOVEL SOME SH*T INTO A HELPER METHOD.
-   //
-   //But here there is a simpler solution to the problem, that also improves the production design.
-   private static Project projectWithPOEmail(String mail) {
-      Project project = new Project();
-      project.setPoEmail(mail);
-      return project;
-   }
+
+
+//   // whenever a test is hard to write, you can ALWAYS SHOVEL SOME SH*T INTO A HELPER METHOD.
+//   //
+//   //But here there is a simpler solution to the problem, that also improves the production design.
+//   private static Project projectWithPOEmail(String mail) {
+//      Project project = new Project();
+//      project.setPoEmail(mail);
+//      return project;
+//   }
 }
