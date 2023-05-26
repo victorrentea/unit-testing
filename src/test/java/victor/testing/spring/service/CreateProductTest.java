64a2f9d2f9055793804d9311a0671a9b552dbc4e
Victor Rentea <victorrentea@gmail.com>
2023-05-26 18:19:23 +0300

    format

--- src/test/java/victor/testing/spring/service/CreateProductTest.java
+++ src/test/java/victor/testing/spring/service/CreateProductTest.java
@@ -15,52 +15,53 @@ import victor.testing.spring.web.dto.ProductDto;
 
 import java.util.Optional;
 
-import static java.time.LocalDateTime.now;
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static victor.testing.spring.domain.ProductCategory.HOME;
 
 @ExtendWith(MockitoExtension.class)
 public class CreateProductTest {
-   @Mock
-   public SafetyClient mockSafetyClient;
-   @Mock
-   private ProductRepo productRepo;
-   @Mock
-   private SupplierRepo supplierRepo;
-   @InjectMocks
-   private ProductService productService;
+  @Mock
+  SafetyClient mockSafetyClient;
+  @Mock
+  ProductRepo productRepo;
+  @Mock
+  SupplierRepo supplierRepo;
+  @InjectMocks
+  ProductService productService;
 
-   @Test
-   public void createThrowsForUnsafeProduct() {
-      when(mockSafetyClient.isSafe("bar")).thenReturn(false);
+  @Test
+  void createThrowsForUnsafeProduct() {
+    when(mockSafetyClient.isSafe("bar")).thenReturn(false);
 
-      ProductDto dto = new ProductDto("name", "bar", -1L, HOME);
-      assertThatThrownBy(() -> productService.createProduct(dto))
-              .isInstanceOf(IllegalStateException.class);
-   }
+    ProductDto dto = new ProductDto("name", "bar", -1L, HOME);
+    assertThatThrownBy(() -> productService.createProduct(dto))
+        .isInstanceOf(IllegalStateException.class);
+  }
 
-   @Test
-   public void createOk() {
-      // GIVEN
-      Supplier supplier = new Supplier().setId(13L);
-      when(supplierRepo.findById(supplier.getId())).thenReturn(Optional.of(supplier));
-      when(mockSafetyClient.isSafe("safebar")).thenReturn(true);
-      ProductDto dto = new ProductDto("name", "safebar", supplier.getId(), HOME);
+  @Test
+  void createOk() {
+    // GIVEN
+    Supplier supplier = new Supplier().setId(13L);
+    when(supplierRepo.findById(supplier.getId())).thenReturn(Optional.of(supplier));
+    when(mockSafetyClient.isSafe("safebar")).thenReturn(true);
+    ProductDto dto = new ProductDto("name", "safebar", supplier.getId(), HOME);
 
-      // WHEN
-      productService.createProduct(dto);
+    // WHEN
+    productService.createProduct(dto);
 
-      // THEN
-      ArgumentCaptor<Product> productCaptor = ArgumentCaptor.forClass(Product.class);
-      verify(productRepo).save(productCaptor.capture());
-      Product product = productCaptor.getValue();
+    // THEN
+    ArgumentCaptor<Product> productCaptor = ArgumentCaptor.forClass(Product.class);
+    verify(productRepo).save(productCaptor.capture());
+    Product product = productCaptor.getValue();
 
-      assertThat(product.getName()).isEqualTo("name");
-      assertThat(product.getBarcode()).isEqualTo("safebar");
-      assertThat(product.getSupplier().getId()).isEqualTo(supplier.getId());
-      assertThat(product.getCategory()).isEqualTo(HOME);
-      // assertThat(product.getCreateDate()).isCloseTo(now(), byLessThan(1, SECONDS)); // uses Spring Magic
-   }
+    assertThat(product.getName()).isEqualTo("name");
+    assertThat(product.getBarcode()).isEqualTo("safebar");
+    assertThat(product.getSupplier().getId()).isEqualTo(supplier.getId());
+    assertThat(product.getCategory()).isEqualTo(HOME);
+    // assertThat(product.getCreateDate()).isCloseTo(now(), byLessThan(1, SECONDS)); // uses Spring Magic
+  }
 
 }
