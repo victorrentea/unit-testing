45b2ada5da137a39c3b8ac1cda8e60fe05a04d2d
Victor Rentea <victorrentea@gmail.com>
2023-05-26 11:20:13 +0300

    ENTERING REACTIVE HELL

--- src/test/java/victor/testing/design/purity/PriceServiceTest.java
+++ src/test/java/victor/testing/design/purity/PriceServiceTest.java
@@ -7,6 +7,7 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import victor.testing.design.purity.PriceService.ApplyCouponsResult;
 import victor.testing.mutation.Coupon;
 import victor.testing.mutation.Customer;
 import victor.testing.spring.domain.Product;
@@ -62,18 +63,12 @@ class PriceServiceTest {
       Coupon coupon1 = new Coupon(HOME, 2, Set.of(13L));
       Coupon coupon2 = new Coupon(ELECTRONICS, 4, Set.of(13L));
       Customer customer = new Customer().setCoupons(List.of(coupon1, coupon2));
-//      when(customerRepo.findById(13L)).thenReturn(customer);
       Product p1 = new Product().setId(1L).setCategory(HOME).setSupplier(new Supplier().setId(13L));
       Product p2 = new Product().setId(2L).setCategory(KIDS).setSupplier(new Supplier().setId(13L));
-//      when(productRepo.findAllById(List.of(1L, 2L))).thenReturn(List.of(p1, p2));
-//      when(thirdPartyPrices.retrievePrice(2L)).thenReturn(5d);
       Map<Long, Double> prices = Map.of(1L, 10d, 2L, 5d);
 
-      PriceService.ApplyCouponsResult result = priceService.applyCoupons(customer, List.of(p1,p2),
-          prices);
+      ApplyCouponsResult result = PriceService.applyCoupons(customer, List.of(p1,p2), prices);
 
-//      verify(couponRepo).markUsedCoupons(eq(13L), couponCaptor.capture());
-//      assertThat(couponCaptor.getValue()).containsExactly(coupon1);
       assertThat(result.usedCoupons()).containsExactly(coupon1);
       assertThat(result.finalPrices())
           .containsEntry(1L, 8d)
