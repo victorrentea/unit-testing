85fa23de1a0126908daba881a0bf85f1bc4f5bc0
Victor Rentea <victorrentea@gmail.com>
2023-05-26 10:40:01 +0300

    approval testing = start by changing the prod code => if ok -=> save it into a 'out' file / """

--- src/main/java/victor/testing/filebased/export/PersonExporter.java
+++ src/main/java/victor/testing/filebased/export/PersonExporter.java
@@ -2,6 +2,7 @@ package victor.testing.filebased.export;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.stream.Collectors;
 
 public class PersonExporter {
   private final PersonRepo personRepo;
@@ -26,9 +27,11 @@ public class PersonExporter {
     for (Person person : personRepo.findAll()) {
       writer.write(person.getFirstName() + " " + person.getLastName().toUpperCase());
       writer.write(";");
-      writer.write(person.getPhoneList().get(0));
+//      writer.write(person.getPhoneList().get(0));
+      writer.write(String.join(",", person.getPhoneList()));
       // TODO BUGFIX: exception when no phones
       // TODO CR: output all phones comma-separated
+
 
       writer.write(";");
       if (person.getBirthDate() != null) {
