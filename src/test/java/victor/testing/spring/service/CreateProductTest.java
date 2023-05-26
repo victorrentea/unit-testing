a465bd10537eab8bcbde615d4ac2645f9d7fdb24
Victor Rentea <victorrentea@gmail.com>
2023-05-26 14:31:12 +0300

    started spring with an in mem db + mockBean everything , DI container

--- src/test/java/victor/testing/spring/service/CreateProductTest.java
+++ src/test/java/victor/testing/spring/service/CreateProductTest.java
@@ -1,11 +1,11 @@
 package victor.testing.spring.service;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.domain.Supplier;
 import victor.testing.spring.infra.SafetyClient;
@@ -15,54 +15,57 @@ import victor.testing.spring.web.dto.ProductDto;
 
 import java.util.Optional;
 
-import static java.time.LocalDateTime.now;
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static victor.testing.spring.domain.ProductCategory.HOME;
 
-//@ExtendWith(MockitoExtension.class)
+// the db spring/hibernate needs can be:
+// - in-mem H2
+// - in a Docker just for tests (@Testcontainers ftw)
+@SpringBootTest
+@ActiveProfiles("db-mem")
 public class CreateProductTest {
-//   @Mock
-   public SafetyClient mockSafetyClient = mock(SafetyClient.class);
-//   @Mock
-   private ProductRepo productRepo = mock(ProductRepo.class);
-//   @Mock
-   private SupplierRepo supplierRepo = mock(SupplierRepo.class);
-//   @InjectMocks
-   private ProductService productService =
-    new ProductService(mockSafetyClient, productRepo, supplierRepo,
-      new ProductMapper(supplierRepo));
+  @MockBean // = creates a Mockito.mock for this type and replaces the real class in the context with this mock
+  SafetyClient mockSafetyClient;
+  @MockBean
+  ProductRepo productRepo;
+  @MockBean
+  SupplierRepo supplierRepo;
+  @Autowired
+  ProductService productService;
 
-   @Test
-   public void createThrowsForUnsafeProduct() {
-      when(mockSafetyClient.isSafe("bar")).thenReturn(false);
+  @Test
+  public void createThrowsForUnsafeProduct() {
+    when(mockSafetyClient.isSafe("bar")).thenReturn(false);
 
-      ProductDto dto = new ProductDto("name", "bar", -1L, HOME);
-      assertThatThrownBy(() -> productService.createProduct(dto))
-              .isInstanceOf(IllegalStateException.class);
-   }
+    ProductDto dto = new ProductDto("name", "bar", -1L, HOME);
+    assertThatThrownBy(() -> productService.createProduct(dto))
+        .isInstanceOf(IllegalStateException.class);
+  }
 
-   @Test
-   public void createOk() {
-      // GIVEN
-      Supplier supplier = new Supplier().setId(13L);
-      when(supplierRepo.findById(supplier.getId())).thenReturn(Optional.of(supplier));
-      when(mockSafetyClient.isSafe("safebar")).thenReturn(true);
-      ProductDto dto = new ProductDto("name", "safebar", supplier.getId(), HOME);
+  @Test
+  public void createOk() {
+    // GIVEN
+    Supplier supplier = new Supplier().setId(13L);
+    when(supplierRepo.findById(supplier.getId())).thenReturn(Optional.of(supplier));
+    when(mockSafetyClient.isSafe("safebar")).thenReturn(true);
+    ProductDto dto = new ProductDto("name", "safebar", supplier.getId(), HOME);
 
-      // WHEN
-      productService.createProduct(dto);
+    // WHEN
+    productService.createProduct(dto);
 
-      // THEN
-      ArgumentCaptor<Product> productCaptor = ArgumentCaptor.forClass(Product.class);
-      verify(productRepo).save(productCaptor.capture());
-      Product product = productCaptor.getValue();
+    // THEN
+    ArgumentCaptor<Product> productCaptor = ArgumentCaptor.forClass(Product.class);
+    verify(productRepo).save(productCaptor.capture());
+    Product product = productCaptor.getValue();
 
-      assertThat(product.getName()).isEqualTo("name");
-      assertThat(product.getBarcode()).isEqualTo("safebar");
-      assertThat(product.getSupplier().getId()).isEqualTo(supplier.getId());
-      assertThat(product.getCategory()).isEqualTo(HOME);
-      // assertThat(product.getCreateDate()).isCloseTo(now(), byLessThan(1, SECONDS)); // uses Spring Magic
-   }
+    assertThat(product.getName()).isEqualTo("name");
+    assertThat(product.getBarcode()).isEqualTo("safebar");
+    assertThat(product.getSupplier().getId()).isEqualTo(supplier.getId());
+    assertThat(product.getCategory()).isEqualTo(HOME);
+    // assertThat(product.getCreateDate()).isCloseTo(now(), byLessThan(1, SECONDS)); // uses Spring Magic
+  }
 
 }
