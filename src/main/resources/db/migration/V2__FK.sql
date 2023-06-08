2d0ce477d6b5030046ead1def80588ecd1886cf6
Victor Rentea <victorrentea@gmail.com>
2023-06-08 09:14:02 +0300

    yolo

--- src/main/resources/db/migration/V2__FK.sql
+++ src/main/resources/db/migration/V2__FK.sql
@@ -1,4 +1,2 @@
 
 alter table product add constraint FK_PRODUCT_SUPPLIER foreign key (supplier_id) references supplier;
-
-create table email_to_send (id int8 not null, body varchar(255), recipient_email varchar(255), status varchar(255), subject varchar(255), version int8, primary key (id));
---
+++ src/main/resources/db/migration/V3__email_to_send.sql
@@ -0,0 +1,2 @@
+
+create table email_to_send (id int8 not null, body varchar(255), recipient_email varchar(255), status varchar(255), subject varchar(255), version int8, primary key (id));
