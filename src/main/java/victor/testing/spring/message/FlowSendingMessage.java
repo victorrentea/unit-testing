33f558aa2f5214094211e1398a32c0ac7c4cc31f
Victor Rentea <victorrentea@gmail.com>
2023-06-14 22:53:33 +0300

    yolo

---
+++ src/main/java/victor/testing/spring/message/FlowSendingMessage.java
@@ -0,0 +1,17 @@
+package victor.testing.spring.message;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+import victor.testing.filebased.message.KafkaSender;
+
+//@RequiredArgsConstructor
+//@Service
+//public class FlowSendingMessage {
+//  private final KafkaSender kafkaSender;
+//
+//  public void method() {
+//
+//  }
+//
+//}
