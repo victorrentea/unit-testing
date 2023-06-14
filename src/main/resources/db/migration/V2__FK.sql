084fc6f3d602f3b9331299decaaf1b5357f89ac1
Victor Rentea <victorrentea@gmail.com>
2023-06-14 17:22:31 +0300

    fix incremental

--- src/main/resources/db/migration/V2__FK.sql
+++ src/main/resources/db/migration/V2__FK.sql
@@ -1,2 +1,2 @@
 
-alter table products add constraint FK_PRODUCT_SUPPLIER foreign key (supplier_id) references supplier;
+alter table product add constraint FK_PRODUCT_SUPPLIER foreign key (supplier_id) references supplier;
