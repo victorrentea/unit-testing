1e5959fbd857b614eb90faee90889abed74f11f4
Victor Rentea <victorrentea@gmail.com>
2023-06-16 17:44:11 +0300

    format

--- src/main/java/victor/testing/spring/product/infra/SafetyClient.java
+++ src/main/java/victor/testing/spring/product/infra/SafetyClient.java
@@ -12,20 +12,20 @@ import java.net.URL;
 @Component
 @RequiredArgsConstructor
 public class SafetyClient {
-    private final RestTemplate restTemplate;
-    @Value("${safety.service.url.base}")
-    private URL baseUrl;
+  private final RestTemplate restTemplate;
+  @Value("${safety.service.url.base}")
+  private URL baseUrl;
 
-    public boolean isSafe(String barcode) {
-        SafetyReportDto response = restTemplate.getForEntity(
+  public boolean isSafe(String barcode) {
+    SafetyReportDto response = restTemplate.getForEntity(
             baseUrl + "/product/{barcode}/safety",
             SafetyReportDto.class, barcode)
-                .getBody();
+        .getBody();
 
-        boolean safe = response.getEntries().stream()
-                .anyMatch(report -> "SAFE".equals(report.getCategory()));
-        log.info("Product is safe: " + safe);
-        return safe;
-    }
+    boolean safe = response.getEntries().stream()
+        .anyMatch(report -> "SAFE".equals(report.getCategory()));
+    log.info("Product is safe: " + safe);
+    return safe;
+  }
 
 }
