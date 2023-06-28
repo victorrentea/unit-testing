f93c94170bb205d104e701f4693d2749fd38ace8
Victor Rentea <victorrentea@gmail.com>
2023-06-28 17:35:14 +0300

    Mockito extension

--- src/test/java/victor/testing/mocks/telemetry/DiagnosticCheckTransmissionTest.java
+++ src/test/java/victor/testing/mocks/telemetry/DiagnosticCheckTransmissionTest.java
@@ -1,21 +1,34 @@
 package victor.testing.mocks.telemetry;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+   // Mockito creates a subclass of the Client.class
+// (can't always do it 'final') unless you use
+// mockito-inline dependency in your pom, that deprecated PowerMock
 
 // Purpose : mastering mocks
 
+// extension to the testing framework: it allows Mockito to create and setup the Test class instance (reflection on the fields to find @Mock/@InjectMocks)
+//@RunWith(MockitoJUnitRunner.class) // Junit4 RULES
+@ExtendWith(MockitoExtension.class) // Junit5
 public class DiagnosticCheckTransmissionTest {
+   @Mock
+   Client clientMock;
+   @InjectMocks
+   Diagnostic diagnostic;
+
    @Test
    void disconnects() {
-      // Mockito creates a subclass of the Client.class
-      // (can't always do it 'final') unless you use
-      // mockito-inline dependency in your pom, that deprecated PowerMock
-      Client clientMock = mock(Client.class);
-      Diagnostic diagnostic = new Diagnostic(clientMock);
       when(clientMock.getOnlineStatus()).thenReturn(true);
 
       diagnostic.checkTransmission(true);
@@ -24,8 +37,6 @@ public class DiagnosticCheckTransmissionTest {
    }
    @Test
    void throwsWhenNotOnline() {
-      Client clientMock = mock(Client.class);
-      Diagnostic diagnostic = new Diagnostic(clientMock);
       when(clientMock.getOnlineStatus()).thenReturn(false);
 
       Assertions.assertThatThrownBy(() -> diagnostic.checkTransmission(true))
@@ -33,8 +44,6 @@ public class DiagnosticCheckTransmissionTest {
    }
    @Test
    void sendsDiagnostic() {
-      Client clientMock = mock(Client.class);
-      Diagnostic diagnostic = new Diagnostic(clientMock);
       when(clientMock.getOnlineStatus()).thenReturn(true);
 
       diagnostic.checkTransmission(true);
