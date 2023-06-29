efbd64927d0a1e1ed673f73cdb8fb56b3ef72d38
Victor Rentea <victorrentea@gmail.com>
2023-06-29 13:23:24 +0300

    guideline: isolate complexity in pure functions taking clea inputs and returning data.

--- src/main/java/victor/testing/design/purity/PriceService.java
+++ src/main/java/victor/testing/design/purity/PriceService.java
@@ -1,6 +1,7 @@
 package victor.testing.design.purity;
 
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import victor.testing.mutation.Coupon;
 import victor.testing.mutation.Customer;
 import victor.testing.spring.product.domain.Product;
@@ -21,15 +22,28 @@ public class PriceService {
    public Map<Long, Double> computePrices(long customerId, List<Long> productIds,
                                           Map<Long, Double> internalPrices) {
       Customer customer = customerRepo.findById(customerId);
-      List<Product> products = productRepo.findAllById(productIds);
+      List<Product> products = productRepo.findAllById(productIds); // where id IN (?,?..)
+      Map<Long, Double> resolvedPrices = resolvePrices(internalPrices, products);
+
+      ComputePriceResult result = doComputePrice(products, resolvedPrices, customer.getCoupons());
+
+      couponRepo.markUsedCoupons(customerId, result.usedCoupons());
+      return result.finalPrices();
+   }
+
+   // heavy decisions
+   record ComputePriceResult(List<Coupon> usedCoupons, Map<Long, Double> finalPrices) {
+   }
+
+   // pure functions should be static
+   // if a static function does NOT mutate args => pure (if not time/random contact)
+   // static means no dependecy contact => no network
+   private static ComputePriceResult doComputePrice(List<Product> products, Map<Long, Double> resolvedPrices, List<Coupon> coupons) {
       List<Coupon> usedCoupons = new ArrayList<>();
       Map<Long, Double> finalPrices = new HashMap<>();
       for (Product product : products) {
-         Double price = internalPrices.get(product.getId());
-         if (price == null) {
-            price = thirdPartyPrices.retrievePrice(product.getId());
-         }
-         for (Coupon coupon : customer.getCoupons()) {
+         double price = resolvedPrices.get(product.getId());
+         for (Coupon coupon : coupons) {
             if (coupon.autoApply()
                 && coupon.isApplicableFor(product, price)
                 && !usedCoupons.contains(coupon)) {
@@ -39,8 +53,20 @@ public class PriceService {
          }
          finalPrices.put(product.getId(), price);
       }
-      couponRepo.markUsedCoupons(customerId, usedCoupons);
-      return finalPrices;
+      return new ComputePriceResult(usedCoupons, finalPrices);
+   }
+
+   @NotNull
+   private Map<Long, Double> resolvePrices(Map<Long, Double> internalPrices, List<Product> products) {
+      Map<Long, Double> resolvedPrices = new HashMap<>();
+      for (Product product : products) {
+         Double price = internalPrices.get(product.getId());
+         if (price == null) {
+            price = thirdPartyPrices.retrievePrice(product.getId());
+         }
+         resolvedPrices.put(product.getId(), price);
+      }
+      return resolvedPrices;
    }
 
 }
