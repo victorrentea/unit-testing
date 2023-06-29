56c465973aa1a5a3092bfe5127aa86ab4ac8b4eb
Victor Rentea <victorrentea@gmail.com>
2023-06-29 16:12:54 +0300

    Tests are more resilient vs changes in framework usage leading to the same effects

--- src/main/java/victor/testing/spring/product/service/ProductService.java
+++ src/main/java/victor/testing/spring/product/service/ProductService.java
@@ -45,7 +45,9 @@ public class ProductService {
     product.setName(productDto.getName());
     product.setBarcode(productDto.getBarcode());
     product.setCategory(productDto.getCategory());
-    Supplier supplier = supplierRepo.findById(productDto.getSupplierId()).orElseThrow();
+//    Supplier supplier = supplierRepo.findById(productDto.getSupplierId()).orElseThrow();
+//    Supplier supplier = supplierRepo.getReferenceById(productDto.getSupplierId()); // returns a proxy
+    Supplier supplier = new Supplier().setId(productDto.supplierId);
     product.setSupplier(supplier);
     productRepo.save(product);
     kafkaTemplate.send(PRODUCT_CREATED_TOPIC, "k", product.getName().toUpperCase());
