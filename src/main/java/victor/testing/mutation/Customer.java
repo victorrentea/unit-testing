dc4e6b32e560711f993d88f8c9a8cbdf811844f2
Victor Rentea <victorrentea@gmail.com>
2023-07-04 11:51:22 +0300

    gata cu builderii

--- src/main/java/victor/testing/mutation/Customer.java
+++ src/main/java/victor/testing/mutation/Customer.java
@@ -2,12 +2,14 @@ package victor.testing.mutation;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+// imagine @Entity
 @Data
+//@Builder // GRESIT PR reject.
+//   in schimb daca vrei sa creezi obiectul mai 'compact' poti sa faci setteri sa intoarca 'this'
 public class Customer {
 	private Long id;
 	private String name;
@@ -16,4 +18,13 @@ public class Customer {
 	private Address address = new Address();
 	private Date createDate;
 	private List<Coupon> coupons = new ArrayList<>();
+
+	public String getName() {
+		return name;
+	}
+
+	public Customer setName(String name) {
+		this.name = name;
+		return this;
+	}
 }
--- src/test/java/victor/testing/mutation/CustomerValidatorTest.java
+++ src/test/java/victor/testing/mutation/CustomerValidatorTest.java
@@ -1,7 +1,6 @@
 package victor.testing.mutation;
 
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
@@ -11,50 +10,56 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayNameGeneration(HumanReadableTestNames.class) // extensie custom
 public class CustomerValidatorTest {
-   CustomerValidator validator = new CustomerValidator();
+  CustomerValidator validator = new CustomerValidator();
 
-   Customer customer;
-   @BeforeEach
-   final void before() {
-       customer = new Customer();
-      customer.setName("::name::");
-      customer.setEmail("::email::");
-      customer.setAddress(new Address());
-      customer.getAddress().setCity("::city::");
-   }
-   @Test
-   void valid() {
-      validator.validate(customer);
-   }
-//   @DisplayName("Un customer care vine cu numele null va face validatorul sa arunce o IllegalArgumentException cu mesajul .... bla bla")
-   // human readable.
-   // RISK: desync cu ce face testul/ numele testului
-   // PRO: poti explica cazuri mai complexe e bun pt teste de integrare/mai ample
-   //   unde desi ai incercat nu ai putut face CORPUL sau NUMELE testului self -explanatory
+  Customer customer;
+
+  @BeforeEach
+  final void before() {
+    // scopul unui builder: sa populezi un obiect cu un constructor fff mare si scarbos (max 5)
+    customer = new Customer()
+        .setName("::name::")
+        .setEmail("::email::") // lombok poate genera setteri sa intoarca 'this'. vezi lombok.config din src/main/java
+        .setAddress(new Address()
+            .setCity("::city::"));
+  }
+
+  @Test
+  void valid() {
+    validator.validate(customer);
+  }
+
+  //   @DisplayName("Un customer care vine cu numele null va face validatorul sa arunce o IllegalArgumentException cu mesajul .... bla bla")
+  // human readable.
+  // RISK: desync cu ce face testul/ numele testului
+  // PRO: poti explica cazuri mai complexe e bun pt teste de integrare/mai ample
+  //   unde desi ai incercat nu ai putut face CORPUL sau NUMELE testului self -explanatory
 //   void testCustomerNameNull() {
 //   void shouldThrowException_whenCustomerNameIsNull() {
-   // mai jos: nume la fel de semnificative dar mai scurte
+  // mai jos: nume la fel de semnificative dar mai scurte
 //   void shouldThrow_whenCustomerNameIsNull() {
 //   void throws_whenCustomerNameIsNull() {
-   @Test//(expected = /junit4)
-   void throwsForNullName() { // scurt dar la fel de semnificat
-      customer.setName(null); // Genu proxim si diferenta specifica
+  @Test
+//(expected = /junit4)
+  void throwsForNullName() { // scurt dar la fel de semnificat
+    customer.setName(null); // Genu proxim si diferenta specifica
 
-      assertThatThrownBy(() -> validator.validate(customer)) // AssertJ ❤️ Doar AssertJ. Forever.
-          .isInstanceOf(IllegalArgumentException.class)
-          .hasMessage("Missing customer name"); // fluent assertions
-   }
-   @Test
-   void throwsForNullEmail() {
-      customer.setEmail(null);
-      // mai jos sunt asertii facute cu org.junit (JUNIT4) sau org.jupiter (JUNIT5)
-      // DE EVITAT
+    assertThatThrownBy(() -> validator.validate(customer)) // AssertJ ❤️ Doar AssertJ. Forever.
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Missing customer name"); // fluent assertions
+  }
+
+  @Test
+  void throwsForNullEmail() {
+    customer.setEmail(null);
+    // mai jos sunt asertii facute cu org.junit (JUNIT4) sau org.jupiter (JUNIT5)
+    // DE EVITAT
 //      IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, // NU FOLOSI
 //          () -> validator.validate(customer));
 //      assertEquals("Missing customer email", e.getMessage()); // NU FOLOSI
-      assertThatThrownBy(() -> validator.validate(customer)) // AssertJ ❤️ Doar AssertJ. Forever.
-          .isInstanceOf(IllegalArgumentException.class)
-          .hasMessage("Missing customer email"); // fluent assertions
-   }
+    assertThatThrownBy(() -> validator.validate(customer)) // AssertJ ❤️ Doar AssertJ. Forever.
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Missing customer email"); // fluent assertions
+  }
 
 }
