2d40c542b9c47b5acf39dd75d0c3fe497481d87e
Victor Rentea <victorrentea@gmail.com>
2023-07-04 11:42:55 +0300

    assertThat FOREVER

--- src/test/java/victor/testing/mutation/CustomerValidatorTest.java
+++ src/test/java/victor/testing/mutation/CustomerValidatorTest.java
@@ -1,11 +1,12 @@
 package victor.testing.mutation;
 
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 import victor.testing.tools.HumanReadableTestNames;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayNameGeneration(HumanReadableTestNames.class) // extensie custom
 public class CustomerValidatorTest {
@@ -26,7 +27,6 @@ public class CustomerValidatorTest {
    // PRO: poti explica cazuri mai complexe e bun pt teste de integrare/mai ample
    //   unde desi ai incercat nu ai putut face CORPUL sau NUMELE testului self -explanatory
 //   void testCustomerNameNull() {
-
 //   void shouldThrowException_whenCustomerNameIsNull() {
    // mai jos: nume la fel de semnificative dar mai scurte
 //   void shouldThrow_whenCustomerNameIsNull() {
@@ -37,8 +37,12 @@ public class CustomerValidatorTest {
       customer.setEmail("::email::");
       customer.setAddress(new Address());
       customer.getAddress().setCity("::city::");
-      Assertions.assertThrows(IllegalArgumentException.class,
-          () -> validator.validate(customer));
+//      Assertions.assertThrows(IllegalArgumentException.class,
+//          () -> validator.validate(customer));
+
+      assertThatThrownBy(() -> validator.validate(customer)) // AssertJ ❤️ Doar AssertJ. Forever.
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("Missing customer name"); // fluent assertions
    }
    @Test
    void throwsForNullEmail() {
@@ -46,8 +50,15 @@ public class CustomerValidatorTest {
       customer.setName(":::name:::");
       customer.setAddress(new Address());
       customer.getAddress().setCity("::city::");
-      Assertions.assertThrows(IllegalArgumentException.class,
-          () -> validator.validate(customer));
+      // mai jos sunt asertii facute cu org.junit (JUNIT4) sau org.jupiter (JUNIT5)
+      // DE EVITAT
+//      IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, // NU FOLOSI
+//          () -> validator.validate(customer));
+//      assertEquals("Missing customer email", e.getMessage()); // NU FOLOSI
+
+      assertThatThrownBy(() -> validator.validate(customer)) // AssertJ ❤️ Doar AssertJ. Forever.
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("Missing customer email"); // fluent assertions
    }
 
 }
