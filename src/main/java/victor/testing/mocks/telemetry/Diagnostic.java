af20161cdfb90129a9d320859d811bf3876a4efe
Victor Rentea <victorrentea@gmail.com>
2023-07-04 17:05:55 +0300

    fixed mutant

--- src/main/java/victor/testing/mocks/telemetry/Diagnostic.java
+++ src/main/java/victor/testing/mocks/telemetry/Diagnostic.java
@@ -31,13 +31,6 @@ public class Diagnostic {
 
 		client.send(Client.DIAGNOSTIC_MESSAGE);
 		diagnosticInfo = client.receive();
-		diagnosticInfo = client.receive();
-		diagnosticInfo = client.receive();
-		diagnosticInfo = client.receive();
-		diagnosticInfo = client.receive();
-		diagnosticInfo = client.receive();
-		diagnosticInfo = client.receive();
-		diagnosticInfo = client.receive();
 		// ce dob*toc ar face asta ?!!
 	}
 
