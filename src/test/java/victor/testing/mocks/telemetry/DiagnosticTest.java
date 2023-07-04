0ad76e0b9d59b3eaba8a38a3f97dd02bbe4d9690
Victor Rentea <victorrentea@gmail.com>
2023-07-04 16:20:12 +0300

    1 line testata (disconnect)

--- src/test/java/victor/testing/mocks/telemetry/DiagnosticTest.java
+++ src/test/java/victor/testing/mocks/telemetry/DiagnosticTest.java
@@ -1,17 +1,29 @@
 package victor.testing.mocks.telemetry;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+//@RunWith(MockitoJUnitRunner.class) // JUnit4 equivalent
+@ExtendWith(MockitoExtension.class) // JUnit5 extension care se ocupa cu initializarea instantei ede clasa de test
 class DiagnosticTest {
+  @Mock
+  Client clientMock;
+  @InjectMocks // stie sa injecteze cum stie si Spring:
+  // contructor/field privat
+
+  Diagnostic diagnostic;
 
   @Test
   void disconnects() {
     // given
-    Client clientMock = mock(Client.class);
-    Diagnostic diagnostic = new Diagnostic(clientMock);
     when(clientMock.getOnlineStatus()).thenReturn(true);
 
     // when
