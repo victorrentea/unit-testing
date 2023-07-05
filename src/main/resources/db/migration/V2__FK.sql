090c2aa4ad0949740a850bcca53000b302dd98b0
Victor Rentea <victorrentea@gmail.com>
2023-07-05 16:54:47 +0300

    fixed

--- src/main/resources/db/migration/V2__FK.sql
+++ src/main/resources/db/migration/V2__FK.sql
@@ -1,2 +1,2 @@
 
-alter table product_oups add constraint FK_PRODUCT_SUPPLIER foreign key (supplier_id) references supplier;
+alter table product add constraint FK_PRODUCT_SUPPLIER foreign key (supplier_id) references supplier;
