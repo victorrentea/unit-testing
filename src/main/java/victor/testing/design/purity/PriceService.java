1f898f4cec5b16a9531a4a99c17824f2c216375b
Victor Rentea <victorrentea@gmail.com>
2023-07-05 14:30:00 +0300

    am scos logica grea intr-o functie pura, pe care apoi am testat-o cu un test "subcutanat" @VisibleForTesting ca sa evit dependentele.

--- src/main/java/victor/testing/design/purity/PriceService.java
+++ src/main/java/victor/testing/design/purity/PriceService.java
@@ -1,6 +1,9 @@
 package victor.testing.design.purity;
 
+import com.google.common.annotations.VisibleForTesting;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.jetbrains.annotations.NotNull;
 import victor.testing.mutation.Coupon;
 import victor.testing.mutation.Customer;
 import victor.testing.spring.product.domain.Product;
@@ -18,18 +21,25 @@ public class PriceService {
    private final CouponRepo couponRepo;
    private final ProductRepo productRepo;
 
-   public Map<Long, Double> computePrices(long customerId, List<Long> productIds,
+   public Map<Long, Double> computePrices(long customerId,
+                                          List<Long> productIds,
                                           Map<Long, Double> internalPrices) {
       Customer customer = customerRepo.findById(customerId);
       List<Product> products = productRepo.findAllById(productIds);
+      Map<Long, Double> resolvedPrices = resolvePrices(internalPrices, products);
+      PriceCalculationResult result = doComputePrices(products, resolvedPrices, customer.getCoupons());
+      couponRepo.markUsedCoupons(customerId, result.getUsedCoupons());
+      return result.getFinalPrices();
+   }
+
+   @VisibleForTesting
+    static PriceCalculationResult doComputePrices(
+       List<Product> products, Map<Long, Double> resolvedPrices, List<Coupon> coupons) {
       List<Coupon> usedCoupons = new ArrayList<>();
       Map<Long, Double> finalPrices = new HashMap<>();
       for (Product product : products) {
-         Double price = internalPrices.get(product.getId());
-         if (price == null) {
-            price = thirdPartyPrices.retrievePrice(product.getId());
-         }
-         for (Coupon coupon : customer.getCoupons()) {
+         Double price = resolvedPrices.get(product.getId());
+         for (Coupon coupon : coupons) {
             if (coupon.autoApply()
                 && coupon.isApplicableFor(product, price)
                 && !usedCoupons.contains(coupon)) {
@@ -39,9 +49,29 @@ public class PriceService {
          }
          finalPrices.put(product.getId(), price);
       }
-      couponRepo.markUsedCoupons(customerId, usedCoupons);
-      return finalPrices;
+      return new PriceCalculationResult(usedCoupons, finalPrices);
    }
+
+   @Value
+   static class PriceCalculationResult{ // downside, pret
+      List<Coupon> usedCoupons ;
+      Map<Long, Double> finalPrices;
+   }
+
+   @NotNull
+   private Map<Long, Double> resolvePrices(Map<Long, Double> internalPrices, List<Product> products) {
+      Map<Long, Double> resolvedPrices = new HashMap<>();
+      for (Product product : products) {
+         Double price = internalPrices.get(product.getId());
+         if (price == null) {
+            price = thirdPartyPrices.retrievePrice(product.getId());
+         }
+         resolvedPrices.put(product.getId(), price);
+      }
+      return resolvedPrices;
+   }
+   //3 @Test x 4 @Mock = OK
+   //9 @Test x 4 @Mock =  NU MAI E OK
 
 }
 
--- src/test/java/victor/testing/design/purity/PriceServiceTest.java
+++ src/test/java/victor/testing/design/purity/PriceServiceTest.java
@@ -1,5 +1,6 @@
 package victor.testing.design.purity;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -7,6 +8,7 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import victor.testing.design.purity.PriceService.PriceCalculationResult;
 import victor.testing.mutation.Coupon;
 import victor.testing.mutation.Customer;
 import victor.testing.spring.product.domain.Product;
@@ -23,38 +25,43 @@ import static victor.testing.spring.product.domain.ProductCategory.*;
 
 @ExtendWith(MockitoExtension.class)
 class PriceServiceTest {
-   @Mock
-   CustomerRepo customerRepo;
-   @Mock
-   ThirdPartyPrices thirdPartyPrices;
-   @Mock
-   CouponRepo couponRepo;
-   @Mock
-   ProductRepo productRepo;
-   @InjectMocks
-   PriceService priceService;
-   @Captor
-   ArgumentCaptor<List<Coupon>> couponCaptor;
 
    @Test
    void computePrices() {
       Coupon coupon1 = new Coupon(HOME, 2, Set.of(13L));
       Coupon coupon2 = new Coupon(ELECTRONICS, 4, Set.of(13L));
-      Customer customer = new Customer().setCoupons(List.of(coupon1, coupon2));
-      when(customerRepo.findById(13L)).thenReturn(customer);
       Product p1 = new Product().setId(1L).setCategory(HOME).setSupplier(new Supplier().setId(13L));
       Product p2 = new Product().setId(2L).setCategory(KIDS).setSupplier(new Supplier().setId(13L));
-      when(productRepo.findAllById(List.of(1L, 2L))).thenReturn(List.of(p1, p2));
-      when(thirdPartyPrices.retrievePrice(2L)).thenReturn(5d);
+      Map<Long, Double> prices = Map.of(1L, 10d, 2L, 5d);
 
-      Map<Long, Double> result = priceService.computePrices(13L, List.of(1L, 2L), Map.of(1L, 10d));
+      PriceCalculationResult result = PriceService.doComputePrices(
+          List.of(p1, p2), prices, List.of(coupon1, coupon2));
 
-      verify(couponRepo).markUsedCoupons(eq(13L), couponCaptor.capture());
-      assertThat(couponCaptor.getValue()).containsExactly(coupon1);
-
-      assertThat(result)
+      assertThat(result.getUsedCoupons()).containsExactly(coupon1);
+      assertThat(result.getFinalPrices())
           .containsEntry(1L, 8d)
           .containsEntry(2L, 5d);
    }
+
+//   @Test
+//   void computePrices() {
+//      Coupon coupon1 = new Coupon(HOME, 2, Set.of(13L));
+//      Coupon coupon2 = new Coupon(ELECTRONICS, 4, Set.of(13L));
+//      Customer customer = new Customer().setCoupons(List.of(coupon1, coupon2));
+//      when(customerRepo.findById(13L)).thenReturn(customer);
+//      Product p1 = new Product().setId(1L).setCategory(HOME).setSupplier(new Supplier().setId(13L));
+//      Product p2 = new Product().setId(2L).setCategory(KIDS).setSupplier(new Supplier().setId(13L));
+//      when(productRepo.findAllById(List.of(1L, 2L))).thenReturn(List.of(p1, p2));
+//      when(thirdPartyPrices.retrievePrice(2L)).thenReturn(5d);
+//
+//      Map<Long, Double> result = priceService.computePrices(13L, List.of(1L, 2L), Map.of(1L, 10d));
+//
+//      verify(couponRepo).markUsedCoupons(eq(13L), couponCaptor.capture());
+//      assertThat(couponCaptor.getValue()).containsExactly(coupon1);
+//
+//      assertThat(result)
+//          .containsEntry(1L, 8d)
+//          .containsEntry(2L, 5d);
+//   }
 
 }
