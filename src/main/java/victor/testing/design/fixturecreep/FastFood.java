122773ac13b603a8187b1e63da5cba5de029f3b0
Victor Rentea <victorrentea@gmail.com>
2023-07-05 12:53:04 +0300

    Lenient stubbings

--- src/main/java/victor/testing/design/fixturecreep/FastFood.java
+++ src/main/java/victor/testing/design/fixturecreep/FastFood.java
@@ -6,24 +6,31 @@ import lombok.RequiredArgsConstructor;
 import static victor.testing.design.fixturecreep.FeatureFlags.Feature.PORK_SHAWARMA;
 
 @RequiredArgsConstructor
-public class FastFood {
-   private final Dependency dependency;
-   private final FeatureFlags featureFlags;
+class Shawarma {
+  private final Dependency dependency;
+  private final FeatureFlags featureFlags;
 
-   public void makeShawarma() {
-      if (!dependency.isOnionAllowed()) {
-         throw new IllegalArgumentException();
-      }
-      if (featureFlags.isActive(PORK_SHAWARMA)) {
+  public void makeShawarma(boolean takeaway) {
+    if (!dependency.isOnionAllowed()) {
+      throw new IllegalArgumentException();
+    }
+     if (takeaway) {
+       if (featureFlags.isActive(PORK_SHAWARMA)) {
          // stuff
-      }
-      // complex logic: 7 ifs
-   }
+       }
 
-   public void makeTzatziki() {
-      if (!dependency.isCucumberAllowed()) {
-         throw new IllegalArgumentException();
-      }
-      // complex logic: 5 ifs
-   }
+     }
+    // complex logic: 7 ifs
+  }
+}
+
+@RequiredArgsConstructor
+class Tzatziki {
+  private final Dependency dependency;
+  public void makeTzatziki() {
+    if (!dependency.isCucumberAllowed()) {
+      throw new IllegalArgumentException();
+    }
+    // complex logic: 5 ifs
+  }
 }
--- src/test/java/victor/testing/design/fixturecreep/FastFoodTest.java
+++ src/test/java/victor/testing/design/fixturecreep/FastFoodTest.java
@@ -1,42 +1,47 @@
 package victor.testing.design.fixturecreep;
 
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import static org.mockito.Mockito.*;
 import static victor.testing.design.fixturecreep.FeatureFlags.Feature.PORK_SHAWARMA;
 
 
 @ExtendWith(MockitoExtension.class) // exceptia apare doar daca folosesti @Mock / @InjectMock, nu =mock(
+//@MockitoSettings(strictness = Strictness.LENIENT) // GRESIT: face TOATE stubbingurile LENIENT -> te-ai intors la before de 20+ linii de iad
 class FastFood_ShawarmaTest {
    @Mock // strict mock; see other ways to get strict mocks in the 'strictstubs' package
    Dependency dependency;
    @Mock
    FeatureFlags featureFlags;
    @InjectMocks
-   FastFood fastFood;
+   Shawarma fastFood;
 
    @BeforeEach
    final void before() {
       when(dependency.isOnionAllowed()).thenReturn(true);
-      when(featureFlags.isActive(PORK_SHAWARMA)).thenReturn(true);
+      // CORECT: lenient() selectiv pe stubbingurile ne critice. zice: draga mockito, nu crapa daca n-o chem p'asta
+      lenient().when(featureFlags.isActive(PORK_SHAWARMA)).thenReturn(true);
    }
 
    @Test
    void shawarmaTest() { // + 7 more tests
+      when(featureFlags.isActive(PORK_SHAWARMA)).thenReturn(true);
+
       // ... complex
-      fastFood.makeShawarma();
+      fastFood.makeShawarma(true);
    }
 
    @Test
    void shawarmaTest2() { // + 7 more tests
       // ... complex
-      fastFood.makeShawarma();
+      fastFood.makeShawarma(false);
    }
 
 }
@@ -46,7 +51,7 @@ class FastFood_TzatzikiTest {
    @Mock // strict mock; see other ways to get strict mocks in the 'strictstubs' package
    Dependency dependency;
    @InjectMocks
-   FastFood fastFood;
+   Tzatziki fastFood;
 
 //   @Before // JUnit 4 nu ruleaza daca @Test e din jupiter
    @BeforeEach // JUnit 5
