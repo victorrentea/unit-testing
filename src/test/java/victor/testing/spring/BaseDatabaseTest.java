c2698d81a29e26c363b1dc63f4e7a026d49f37ac
Victor Rentea <victorrentea@gmail.com>
2023-07-05 16:55:24 +0300

    inapoi pe H2

--- src/test/java/victor/testing/spring/BaseDatabaseTest.java
+++ src/test/java/victor/testing/spring/BaseDatabaseTest.java
@@ -10,34 +10,34 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import victor.testing.tools.TestcontainersUtils;
 
 // #1 innocent Testcontainers test (online examples)
-//@SpringBootTest
-//@ActiveProfiles("db-mem")
-//public class BaseDatabaseTest {
-//}
+@SpringBootTest
+@ActiveProfiles("db-mem")
+public class BaseDatabaseTest {
+}
 
 // ==================================================================
 // #2 innocent Testcontainers test (online examples)
-@SpringBootTest
-@Testcontainers
-public class BaseDatabaseTest {
-  // https://stackoverflow.com/questions/62425598/how-to-reuse-testcontainers-between-multiple-springboottests
-  // === The containers is reused across all subclasses ===
-  static public PostgreSQLContainer<?> postgres =
-      new PostgreSQLContainer<>("postgres:11");
-
-  // TODO add in ~/.testcontainers.properties put testcontainers.reuse.enable=true
-
-  @BeforeAll
-  public static void startTestcontainer() {
-    postgres.start();
-  }
-
-  @DynamicPropertySource
-  public static void registerPgProperties(DynamicPropertyRegistry registry) {
-    // ia detaliile de conectare la DB din docker si le da lu Spring
-    TestcontainersUtils.addDatasourceDetails(registry, postgres, true);
-  }
-}
+//@SpringBootTest
+//@Testcontainers
+//public class BaseDatabaseTest {
+//  // https://stackoverflow.com/questions/62425598/how-to-reuse-testcontainers-between-multiple-springboottests
+//  // === The containers is reused across all subclasses ===
+//  static public PostgreSQLContainer<?> postgres =
+//      new PostgreSQLContainer<>("postgres:11");
+//
+//  // TODO add in ~/.testcontainers.properties put testcontainers.reuse.enable=true
+//
+//  @BeforeAll
+//  public static void startTestcontainer() {
+//    postgres.start();
+//  }
+//
+//  @DynamicPropertySource
+//  public static void registerPgProperties(DynamicPropertyRegistry registry) {
+//    // ia detaliile de conectare la DB din docker si le da lu Spring
+//    TestcontainersUtils.addDatasourceDetails(registry, postgres, true);
+//  }
+//}
 
 // ==================================================================
 // #3 tuned Testcontainers test (like a platform team will ofer)
