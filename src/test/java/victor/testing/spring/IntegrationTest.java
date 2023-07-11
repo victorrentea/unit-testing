75b7fbaebc9b865c9e3a92920301dbe88ed82538
Victor Rentea <victorrentea@gmail.com>
2023-07-11 16:16:25 +0300

    inapoi la h2

--- src/test/java/victor/testing/spring/IntegrationTest.java
+++ src/test/java/victor/testing/spring/IntegrationTest.java
@@ -10,33 +10,33 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import victor.testing.tools.TestcontainersUtils;
 
 // #1 innocent Testcontainers test (online examples)
-//@SpringBootTest
-//@ActiveProfiles("db-mem")
-//public abstract class IntegrationTest {
-//}
+@SpringBootTest
+@ActiveProfiles("db-mem") // H2
+public abstract class IntegrationTest {
+}
 
 // ==================================================================
 // #2 innocent Testcontainers test (online examples)
-@SpringBootTest
-@Testcontainers
-public abstract class IntegrationTest {
-  // https://stackoverflow.com/questions/62425598/how-to-reuse-testcontainers-between-multiple-springboottests
-  // === The containers is reused across all subclasses ===
-  static public PostgreSQLContainer<?> postgres =
-      new PostgreSQLContainer<>("postgres:11");
-
-  // TODO add in ~/.testcontainers.properties put testcontainers.reuse.enable=true
-
-  @BeforeAll
-  public static void startTestcontainer() {
-    postgres.start();
-  }
-
-  @DynamicPropertySource
-  public static void registerPgProperties(DynamicPropertyRegistry registry) {
-    TestcontainersUtils.addDatasourceDetails(registry, postgres, true);
-  }
-}
+//@SpringBootTest
+//@Testcontainers
+//public abstract class IntegrationTest {
+//  // https://stackoverflow.com/questions/62425598/how-to-reuse-testcontainers-between-multiple-springboottests
+//  // === The containers is reused across all subclasses ===
+//  static public PostgreSQLContainer<?> postgres =
+//      new PostgreSQLContainer<>("postgres:11");
+//
+//  // TODO add in ~/.testcontainers.properties put testcontainers.reuse.enable=true
+//
+//  @BeforeAll
+//  public static void startTestcontainer() {
+//    postgres.start();
+//  }
+//
+//  @DynamicPropertySource
+//  public static void registerPgProperties(DynamicPropertyRegistry registry) {
+//    TestcontainersUtils.addDatasourceDetails(registry, postgres, true);
+//  }
+//}
 
 // ==================================================================
 // #3 tuned Testcontainers test (like a platform team will ofer)
