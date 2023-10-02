170d009cd16bb1bf549b9ff05727b6c4c5afa44d
Victor Rentea <victorrentea@gmail.com>
2023-10-03 01:04:03 +0200

    undo

--- src/main/java/victor/testing/design/spy/BigService.java
+++ src/main/java/victor/testing/design/spy/BigService.java
@@ -1,28 +1,23 @@
 package victor.testing.design.spy;
 
-import lombok.Data;
-
-import java.time.LocalDate;
-
 import static java.time.LocalDate.now;
 import static victor.testing.design.spy.Order.PaymentMethod.CARD;
 
-public class God {
+public class BigService {
   public String high(Order order) {
     low(order);
-    // complexity requiring 5+ tests
     if (order.getPaymentMethod() == CARD) {
       return "bonus";
     }
+    // complexity requiring 5+ tests
     return "regular";
   }
 
   private void low(Order order) {
-    // complexity requiring 5+ tests
     if (order.getCreationDate().isBefore(now().minusMonths(1))) {
       throw new IllegalArgumentException("Order too old");
     }
-    // more stuff to test
+    // complexity requiring 7+ tests
   }
 }
 
