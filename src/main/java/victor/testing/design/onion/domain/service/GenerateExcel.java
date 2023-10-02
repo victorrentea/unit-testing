c80a675b32caa31f24f6059ed2ac317095fea512
Victor Rentea <victorrentea@gmail.com>
2023-10-03 00:05:43 +0200

    undo

--- src/main/java/victor/testing/design/onion/domain/service/GenerateExcel.java
+++
@@ -1,9 +0,0 @@
-package victor.testing.design.onion.domain.service;
-
-import victor.testing.design.onion.domain.model.ContractForExport;
-
-import java.util.List;
-
-public interface GenerateExcel {
-  void generateXls(List<ContractForExport> contracts);
-}
