7c34ea36f59bf211a9ebdc816400519cc3eba2a2
Victor Rentea <victorrentea@gmail.com>
2023-10-03 11:20:10 +0200

    partial mock can block a local method call. do not use this unless to attack legacy untested code

--- src/test/java/victor/testing/design/spy/BigServiceTest.java
+++ src/test/java/victor/testing/design/spy/BigServiceTest.java
@@ -2,21 +2,31 @@ package victor.testing.design.spy;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 
+import static org.assertj.core.api.Assertions.anyOf;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static victor.testing.design.spy.Order.PaymentMethod.CARD;
 
+@ExtendWith(MockitoExtension.class)
 class BigServiceTest {
+  @Spy
+  BigService bigService;
 
   @Test
   void high() { // + 5 more tests like this
+    doNothing().when(bigService).low(any());
     Order order = new Order()
-        .setPaymentMethod(CARD)
-        .setCreationDate(LocalDate.now());
-    String result = new BigService().high(order);
+        .setPaymentMethod(CARD);
+    String result = bigService.high(order);
     assertThat(result).isEqualTo("bonus");
   }
 
