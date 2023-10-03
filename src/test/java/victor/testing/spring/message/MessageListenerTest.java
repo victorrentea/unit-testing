ca309da441ed00890d60dd94fdebd3852f3de387
Victor Rentea <victorrentea@gmail.com>
2023-10-03 20:35:13 +0200

    yolo

--- src/test/java/victor/testing/spring/message/MessageListenerTest.java
+++ src/test/java/victor/testing/spring/message/MessageListenerTest.java
@@ -1,8 +1,9 @@
 package victor.testing.spring.message;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,34 +12,28 @@ import victor.testing.spring.product.repo.SupplierRepo;
 
 import static java.time.Duration.ofSeconds;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.Mockito.*;
 
 @ActiveProfiles("embedded-kafka")
 @EmbeddedKafka(topics = "supplier-created-event")
-public class MessageListenerSpyTest extends IntegrationTest {
+public class MessageListenerTest extends IntegrationTest {
   private static final String SUPPLIER_NAME = "supplier";
   @Autowired
   KafkaTemplate<String, String> kafkaTemplate;
   @Autowired
   SupplierRepo supplierRepo;
-  @SpyBean
-  MessageListener listener;
 
-  @Test // for infrastructure
-  void listenerIsCalled_whenMessageReceived() {
-    doNothing().when(listener).onMessage(any()); // stop the actual processing
+  @BeforeEach
+  @AfterEach
+  final void cleanDB() { // manual cleanup required as tested code COMMITs an INSERT
+    supplierRepo.deleteAll();
+  }
 
+  @Test
+  void supplierIsCreatedOnMessageReceived() throws InterruptedException {
     kafkaTemplate.send("supplier-created-event", SUPPLIER_NAME);
 
-    verify(listener, timeout(2000)).onMessage(SUPPLIER_NAME);
-  }
-
-  @Test // for message processing logic, imagine 10 tests
-  void listenerLogic() {
-    listener.onMessage(SUPPLIER_NAME);
-
+    Thread.sleep(40); // works on my machine
     assertThat(supplierRepo.findByName(SUPPLIER_NAME)).isNotNull();
   }
+
 }
