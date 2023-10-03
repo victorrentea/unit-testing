44c2c036ab88d5e0f335921f520b8d9c3a316003
Victor Rentea <victorrentea@gmail.com>
2023-10-03 11:33:16 +0200

    LENIENT FTL

--- src/test/java/victor/testing/design/fixturecreep/FastFoodTest.java
+++ src/test/java/victor/testing/design/fixturecreep/FastFoodTest.java
@@ -6,13 +6,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import static org.mockito.Mockito.*;
 import static victor.testing.design.fixturecreep.FeatureFlags.Feature.PORK_SHAWARMA;
 
 
+@MockitoSettings(strictness = Strictness.LENIENT) // dont !!
 @ExtendWith(MockitoExtension.class)
-class FastFoodTest {
+class FastFoodTeRst {
    @Mock // strict mock; see other ways to get strict mocks in the 'strictstubs' package
    Dependency dependency;
    @Mock
@@ -22,19 +25,41 @@ class FastFoodTest {
 
    @BeforeEach
    final void before() {
+      when(dependency.isOnionAllowed()).thenReturn(true);
+      when(featureFlags.isActive(PORK_SHAWARMA)).thenReturn(true);
+      when(dependency.isCucumberAllowed()).thenReturn(true);
    }
 
    @Test
    void shawarmaTest() { // + 7 more tests
-      when(dependency.isOnionAllowed()).thenReturn(true);
-      when(featureFlags.isActive(PORK_SHAWARMA)).thenReturn(true);
+      // ... complex
+      fastFood.makeShawarma();
+   }
+   @Test
+   void shawarmaTest2() { // + 7 more tests
+      // ... complex
+      fastFood.makeShawarma();
+   }
+   @Test
+   void shawarmaTest3() { // + 7 more tests
       // ... complex
       fastFood.makeShawarma();
    }
 
+
+
    @Test
    void tzatzikiTest() { // + 5 more tests
-      when(dependency.isCucumberAllowed()).thenReturn(true);
+      // ... complex
+      fastFood.makeTzatziki();
+   }
+   @Test
+   void tzatzikiTest2() { // + 5 more tests
+      // ... complex
+      fastFood.makeTzatziki();
+   }
+   @Test
+   void tzatzikiTest3() { // + 5 more tests
       // ... complex
       fastFood.makeTzatziki();
    }
