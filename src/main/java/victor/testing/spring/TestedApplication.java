8d8a396f45874c83e6634772176cbeca56129462
Victor Rentea <victorrentea@gmail.com>
2023-10-04 08:19:51 +0200

    even cl;eaner !

--- src/main/java/victor/testing/spring/TestedApplication.java
+++ src/main/java/victor/testing/spring/TestedApplication.java
@@ -6,12 +6,16 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,38 +30,46 @@ import java.util.Optional;
 @EnableCaching
 @EnableAsync
 @SpringBootApplication
+@EnableJpaAuditing
 public class TestedApplication {
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
-        return builder -> {
-            builder.modules(new JavaTimeModule());
-            builder.simpleDateFormat("yyyy-MM-dd");
-            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-        };
-    }
-    public static void main(String[] args) {
-        new SpringApplicationBuilder()
-            .profiles("insertDummyData")
-            .initializers(new WaitForDatabase())
-            .sources(TestedApplication.class).run(args);
-    }
+  @Bean
+  public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+    return builder -> {
+      builder.modules(new JavaTimeModule());
+      builder.simpleDateFormat("yyyy-MM-dd");
+      builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+      builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+    };
+  }
 
-    @Autowired
-    public void printDatabaseUrl(@Value("${spring.datasource.url}") String dbUrl) {
-        log.info("Using database: {} <<<", dbUrl);
-    }
+  public static void main(String[] args) {
+    new SpringApplicationBuilder()
+        .profiles("insertDummyData")
+        .sources(TestedApplication.class).run(args);
+  }
 
-    @Bean
-    public RestTemplate rest() {
-        return new RestTemplate();
-    }
+  @Autowired
+  public void printDatabaseUrl(@Value("${spring.datasource.url}") String dbUrl) {
+    log.info("Using database: {} <<<", dbUrl);
+  }
 
-    @Bean // used by @CreatedBy and @LastModifiedBy
-    public AuditorAware<String> auditorProvider() {
-        return () -> Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
-            .map(Principal::getName);
-    }
+  @Bean
+  public RestTemplate rest() {
+    return new RestTemplate();
+  }
 
+  @Bean // used by @CreatedBy and @LastModifiedBy
+  public AuditorAware<String> auditorProvider() {
+    return () -> Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+        .map(Principal::getName);
+  }
 
+  @Bean
+  @Profile("insertDummyData")
+  public CommandLineRunner insertDummyData(JdbcTemplate jdbcTemplate) {
+    return args -> {
+      jdbcTemplate.update("INSERT INTO supplier(ID, NAME, ACTIVE) VALUES (1, 'Dummy', 1)");
+      log.info("Inserted dummy data");
+    };
+  }
 }
