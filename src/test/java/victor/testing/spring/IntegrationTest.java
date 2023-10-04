aa06efede487e9871900c447d11f548f8ed5aea8
Victor Rentea <victorrentea@gmail.com>
2023-10-04 06:25:20 +0200

    turbo

--- src/test/java/victor/testing/spring/IntegrationTest.java
+++ src/test/java/victor/testing/spring/IntegrationTest.java
@@ -3,7 +3,6 @@ package victor.testing.spring;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-// #1 innocent Testcontainers test (online examples)
 @SpringBootTest
 @ActiveProfiles("db-mem")
 public abstract class IntegrationTest {
@@ -13,7 +12,7 @@ public abstract class IntegrationTest {
 // #2 Testcontainers test (online examples)
 //@SpringBootTest
 //@Testcontainers
-//public abstract class BaseDatabaseTest {
+//public abstract class IntegrationTest {
 //  // https://stackoverflow.com/questions/62425598/how-to-reuse-testcontainers-between-multiple-springboottests
 //  // === The containers is reused across all subclasses ===
 //  static public PostgreSQLContainer<?> postgres =
