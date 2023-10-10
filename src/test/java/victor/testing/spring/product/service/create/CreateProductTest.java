007bd471786f044a1323b1d31c236e759885c732
Victor Rentea <victorrentea@gmail.com>
2023-10-10 16:30:32 +0300

    cleanup manual

--- src/test/java/victor/testing/spring/product/service/create/CreateProductTest.java
+++ src/test/java/victor/testing/spring/product/service/create/CreateProductTest.java
@@ -1,17 +1,19 @@
 package victor.testing.spring.product.service.create;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import victor.testing.spring.product.api.dto.ProductDto;
 import victor.testing.spring.product.domain.Product;
 import victor.testing.spring.product.domain.Supplier;
 import victor.testing.spring.product.infra.SafetyClient;
 import victor.testing.spring.product.repo.ProductRepo;
 import victor.testing.spring.product.repo.SupplierRepo;
-import victor.testing.spring.product.api.dto.ProductDto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -33,6 +35,14 @@ public class CreateProductTest {
   KafkaTemplate<String, String> kafkaTemplate;
   @Autowired
   ProductService productService;
+
+  // #1 manual cleanup de DB: ideal pt db non-sql
+  @BeforeEach
+  @AfterEach
+  final void before() {
+    productRepo.deleteAll();
+    supplierRepo.deleteAll();
+  }
 
   @Test
   void createThrowsForUnsafeProduct() {
@@ -62,6 +72,7 @@ public class CreateProductTest {
     //assertThat(product.getCreatedBy()).isEqualTo("user"); // field set via Spring Magic
     verify(kafkaTemplate).send(ProductService.PRODUCT_CREATED_TOPIC, "k", "NAME");
   }
+
   @Test
   void defaultToUncategorizedWhenMIssingCategory() {
     Long supplierId = supplierRepo.save(new Supplier()).getId();
