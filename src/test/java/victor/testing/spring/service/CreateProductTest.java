ba13a19cb6c4bbabe953ea250302f758dc22d353
Victor Rentea <victorrentea@gmail.com>
2023-10-24 16:29:59 +0300

    cleanup#1

--- src/test/java/victor/testing/spring/service/CreateProductTest.java
+++ src/test/java/victor/testing/spring/service/CreateProductTest.java
@@ -1,29 +1,21 @@
 package victor.testing.spring.service;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cache.CacheManager;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import victor.testing.spring.api.dto.ProductDto;
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.domain.Supplier;
 import victor.testing.spring.infra.SafetyClient;
 import victor.testing.spring.repo.ProductRepo;
 import victor.testing.spring.repo.SupplierRepo;
-import victor.testing.spring.service.ProductService;
-import victor.testing.spring.api.dto.ProductDto;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -45,6 +37,19 @@ public class CreateProductTest {
   KafkaTemplate<String, String> kafkaTemplate;
   @Autowired
   ProductService productService;
+
+  // #1 before/after manual cleanup - good for noSQL
+  @BeforeEach
+  @AfterEach
+  final void before() {
+    productRepo.deleteAll();
+    supplierRepo.deleteAll(); // in the correct FK direction
+    for (String cacheName : cacheManager.getCacheNames()) {
+      cacheManager.getCache(cacheName).clear();
+    }
+  }
+  @Autowired
+  private CacheManager cacheManager;
 
   @Test
   void createThrowsForUnsafeProduct() {
