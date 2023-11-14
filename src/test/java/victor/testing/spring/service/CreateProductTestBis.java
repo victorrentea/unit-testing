e1099851f31d08311342707acad2c3ec7deb504f
Victor Rentea <victorrentea@gmail.com>
2023-11-14 16:43:34 +0200

    2 clase de test identice refolosessc acelasi spring context

--- src/test/java/victor/testing/spring/service/CreateProductTestBis.java
+++ src/test/java/victor/testing/spring/service/CreateProductTestBis.java
@@ -3,31 +3,20 @@ package victor.testing.spring.service;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import victor.testing.spring.api.dto.ProductDto;
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.domain.Supplier;
 import victor.testing.spring.infra.SafetyClient;
 import victor.testing.spring.repo.ProductRepo;
 import victor.testing.spring.repo.SupplierRepo;
-import victor.testing.spring.service.ProductService;
-import victor.testing.spring.api.dto.ProductDto;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static victor.testing.spring.domain.ProductCategory.HOME;
@@ -36,7 +25,7 @@ import static victor.testing.spring.domain.ProductCategory.UNCATEGORIZED;
 @Slf4j
 @SpringBootTest
 @ActiveProfiles("db-mem") // H2 in-memory
-public class CreateProductTest {
+public class CreateProductTestBis {
   @Autowired
   SupplierRepo supplierRepo;
   @Autowired
