6d22b737dc8b5451d9223faa1047396ad039a491
Victor Rentea <victorrentea@gmail.com>
2024-04-08 10:26:10 +0300

    GATAAAAA

--- src/test/java/victor/testing/mutation/CustomerValidatorShould.java
+++ src/test/java/victor/testing/mutation/CustomerValidatorShould.java
@@ -1,20 +1,28 @@
 package victor.testing.mutation;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.Assert.assertEquals;
 
 
 public class CustomerValidatorShould {
    CustomerValidator validator = new CustomerValidator();
 
    @Test
-   void valid() {
+   void validWithCityWithSpaces() {
       Customer customer = new Customer();
       customer.setName("::name::");
       customer.setEmail("::email::");
       customer.setAddress(new Address());
-      customer.getAddress().setCity("::city::");
+      customer.getAddress().setCity("  ::city:: ");
+
       validator.validate(customer);
+
+      assertThat(customer.getAddress().getCity())
+          .isEqualTo("::city::");
    }
 
 }
