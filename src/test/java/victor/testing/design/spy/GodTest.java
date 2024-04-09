0c1ba8f1ffea9497b27859016bfda1fe06a953d5
Victor Rentea <victorrentea@gmail.com>
2024-04-09 12:27:37 +0300

    partial mocks. nu recomand.

--- src/test/java/victor/testing/design/spy/GodTest.java
+++ src/test/java/victor/testing/design/spy/GodTest.java
@@ -12,11 +12,21 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static victor.testing.design.spy.Order.PaymentMethod.CARD;
 
+@ExtendWith(MockitoExtension.class)
 class GodTest {
-
-   God god = new God();
+   @Spy
+   God god;// = new God();
 
    // TODO test high method
+   @Test
+   void experiment() {
+      Order order = new Order();
+//          .setCreationDate(now());// dar apoi + inca 10 inputuri pentru ca low() sanu creape
+      doNothing().when(god).low(any()); // = mock partial: in clasa de testat mockuiesc metode
+      // sau pot bloca apelul de la high->low cu mockuri partiale (aka SPY)
+      String r = god.high(order);
+      assertThat(r).isEqualTo("regular");
+   }
 
    @Test
    void lowThrowsIfOrderTooRecent() { // + 15 more tests like this
