f9111f742c4cfccfb3d48289526b94d1753ff912
Victor Rentea <victorrentea@gmail.com>
2024-06-28 14:58:21 +0300

    tot 2 contexte pornesc

---
+++ src/test/java/victor/testing/spring/service/CreateProduct2bisTest.java
@@ -0,0 +1,72 @@
+package victor.testing.spring.service;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import victor.testing.spring.api.dto.ProductDto;
+import victor.testing.spring.domain.Product;
+import victor.testing.spring.domain.Supplier;
+import victor.testing.spring.infra.SafetyClient;
+import victor.testing.spring.repo.ProductRepo;
+import victor.testing.spring.repo.SupplierRepo;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static victor.testing.spring.domain.ProductCategory.HOME;
+
+//@ExtendWith(MockitoExtension.class)
+@SpringBootTest // porneste un container de spring in cadrul testului
+@ActiveProfiles({"db-mem", "alt"})
+public class CreateProduct2bisTest {
+  @MockBean // inlocuieste bean-ul din context cu un mock
+  SupplierRepo supplierRepo;
+  @MockBean
+  ProductRepo productRepo;
+  @MockBean
+  SafetyClient safetyClient;
+  @MockBean
+  KafkaTemplate<String, String> kafkaTemplate;
+  @Autowired
+  ProductService productService;
+
+  @Test
+  void createThrowsForUnsafeProduct() {
+    when(safetyClient.isSafe("upc-unsafe")).thenReturn(false);
+    ProductDto dto = new ProductDto("name", "upc-unsafe", "S", HOME);
+
+    assertThatThrownBy(() -> productService.createProduct(dto))
+        .isInstanceOf(IllegalStateException.class)
+        .hasMessage("Product is not safe!");
+  }
+
+  @Test
+  void createOk() {
+    when(supplierRepo.findByCode("S")).thenReturn(Optional.of(new Supplier().setCode("S")));
+    when(safetyClient.isSafe("upc-safe")).thenReturn(true);
+    ProductDto dto = new ProductDto("name", "upc-safe", "S", HOME);
+
+    // WHEN
+    productService.createProduct(dto);
+
+    ArgumentCaptor<Product> productCaptor = forClass(Product.class);
+    verify(productRepo).save(productCaptor.capture()); // as the mock the actual param value
+    Product product = productCaptor.getValue();
+    assertThat(product.getName()).isEqualTo("name");
+    assertThat(product.getUpc()).isEqualTo("upc-safe");
+    assertThat(product.getSupplier().getCode()).isEqualTo("S");
+    assertThat(product.getCategory()).isEqualTo(HOME);
+    //assertThat(product.getCreatedDate()).isToday(); // field set via Spring Magic @CreatedDate
+    //assertThat(product.getCreatedBy()).isEqualTo("user"); // field set via Spring Magic
+    verify(kafkaTemplate).send(ProductService.PRODUCT_CREATED_TOPIC, "k", "NAME");
+  }
+
+}
