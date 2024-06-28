2e822123257f829be530a2624741f2f12c67989c
Victor Rentea <victorrentea@gmail.com>
2024-06-28 14:17:18 +0300

    remove unused

--- src/main/java/victor/testing/design/roles/repo/ParcelRepo.java
+++
@@ -1,7 +0,0 @@
-package victor.testing.design.roles.repo;
-
-import victor.testing.design.roles.model.Parcel;
-
-public interface ParcelRepo {
-   Parcel findByBarcode(String barcode);
-}
--- src/main/java/victor/testing/filebased/export/PersonRepo.java
+++
@@ -1,6 +0,0 @@
-package victor.testing.filebased.export;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepo extends JpaRepository<Person, Long> {
-}
