73582b990581158157b1fc58321bd475ac6a4aaa
Victor Rentea <victorrentea@gmail.com>
2024-07-05 16:52:11 +0300

    test-fixture breakdown

---
+++ src/test/java/victor/testing/design/fixturecreep/FastFoodShawarmaTest.java
@@ -0,0 +1,44 @@
+package victor.testing.design.fixturecreep;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.when;
+import static victor.testing.design.fixturecreep.FeatureFlags.Feature.PORK_SHAWARMA;
+
+//@MockitoSettings(strictness = LENIENT) RAU!!!=PR reject
+@ExtendWith(MockitoExtension.class)
+class FastFoodShawarmaTest {
+   @Mock // strict mock; see other ways to get strict mocks in the 'strictstubs' package
+   Dependency dependency;
+   @Mock
+   FeatureFlags featureFlags;
+   @InjectMocks
+   FastFood fastFood;
+
+   @BeforeEach
+   final void before() {
+      when(dependency.isOnionAllowed()).thenReturn(true);
+      when(featureFlags.isActive(PORK_SHAWARMA)).thenReturn(true);
+   }
+
+   @Test
+   void shawarmaTest() { // + 7 more tests
+      // ... complex
+      fastFood.makeShawarma();
+   }
+   @Test
+   void shawarmaTest1() { // + 7 more tests
+      // ... complex
+      fastFood.makeShawarma();
+   }
+   @Test
+   void shawarmaTest2() { // + 7 more tests
+      // ... complex
+      fastFood.makeShawarma();
+   }
+}
--- src/test/java/victor/testing/design/fixturecreep/FastFoodTzatzikiTest.java
+++ src/test/java/victor/testing/design/fixturecreep/FastFoodTzatzikiTest.java
@@ -6,43 +6,21 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
 
 import static org.mockito.Mockito.*;
-import static org.mockito.quality.Strictness.LENIENT;
 import static victor.testing.design.fixturecreep.FeatureFlags.Feature.PORK_SHAWARMA;
 
 //@MockitoSettings(strictness = LENIENT) RAU!!!=PR reject
 @ExtendWith(MockitoExtension.class)
-class FastFoodTest {
+class FastFoodTzatzikiTest {
    @Mock // strict mock; see other ways to get strict mocks in the 'strictstubs' package
    Dependency dependency;
-   @Mock
-   FeatureFlags featureFlags;
    @InjectMocks
    FastFood fastFood;
 
    @BeforeEach
    final void before() {
-      when(dependency.isOnionAllowed()).thenReturn(true);
-      when(featureFlags.isActive(PORK_SHAWARMA)).thenReturn(true);
       when(dependency.isYogurt()).thenReturn(true);
-   }
-
-   @Test
-   void shawarmaTest() { // + 7 more tests
-      // ... complex
-      fastFood.makeShawarma();
-   }
-   @Test
-   void shawarmaTest1() { // + 7 more tests
-      // ... complex
-      fastFood.makeShawarma();
-   }
-   @Test
-   void shawarmaTest2() { // + 7 more tests
-      // ... complex
-      fastFood.makeShawarma();
    }
    @Test
    void tzatzikiTest() { // + 5 more tests
