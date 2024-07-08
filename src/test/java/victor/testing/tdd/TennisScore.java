0e12029fd9512ff3bca59d30701d664dac50037d
Victor Rentea <victorrentea@gmail.com>
2024-07-08 14:22:17 +0300

    cleanup

--- src/test/java/victor/testing/tdd/TennisScore.java
+++ src/test/java/victor/testing/tdd/TennisScore.java
@@ -3,8 +3,6 @@ package victor.testing.tdd;
 import java.util.Map;
 
 public class TennisScore {
-
-  private String string = "Love - Love";
   private int player1Points = 0;
 
   private static final String[] LABELS = {"Love", "Fifteen", "Thirty"};
@@ -16,10 +14,5 @@ public class TennisScore {
 
   public void addPointToPlayer1() {
     player1Points++;
-    if (string.equals("Love - Love")) {
-      string = "Fifteen - Love";
-    } else if (string.equals("Fifteen - Love")) {
-      string = "Thirty - Love";
-    }
   }
 }
