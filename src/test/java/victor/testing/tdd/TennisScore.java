1fae4677bacf8c6ce49200339f257db361997a75
Victor Rentea <victorrentea@gmail.com>
2024-07-08 14:20:02 +0300

    design options

--- src/test/java/victor/testing/tdd/TennisScore.java
+++ src/test/java/victor/testing/tdd/TennisScore.java
@@ -1,14 +1,25 @@
 package victor.testing.tdd;
 
+import java.util.Map;
+
 public class TennisScore {
 
   private String string = "Love - Love";
-
+  private int player1Points = 0;
+//  private static final Map<Integer, String> scores = Map.of(
+//      0, "Love",
+//      1, "Fifteen"
+//  );
+  private static final String[] LABELS = {"Love", "Fifteen", "Thirty"};
+//  enum Label{Love, Fifteen, Thirty}
   public String getScore() {
+//    Label.values()[0].name()
+    String decoded = LABELS[player1Points];
     return string;
   }
 
   public void addPointToPlayer1() {
+    player1Points++;
     if (string.equals("Love - Love")) {
       string = "Fifteen - Love";
     } else if (string.equals("Fifteen - Love")) {
