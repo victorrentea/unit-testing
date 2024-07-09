a5f97d468555c066f6714e6dca52ce2afcc4bfc0
Victor Rentea <victorrentea@gmail.com>
2024-07-09 15:06:21 +0300

    correct with before

--- src/test/java/victor/testing/spring/service/ProductServiceTest.java
+++ src/test/java/victor/testing/spring/service/ProductServiceTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import victor.testing.spring.api.dto.ProductDto;
 import victor.testing.spring.domain.Supplier;
@@ -17,9 +15,6 @@ import victor.testing.spring.repo.ProductRepo;
 import victor.testing.spring.repo.SupplierRepo;
 
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 
 // they conmfigure the instance of the test class
 //@RunWith(MockitoJUnitRunner.class)// Junit 4
@@ -34,13 +29,17 @@ class ProductServiceTest {
   @Mock
   KafkaTemplate<String, String> kafkaTemplateMock;
 
-  // field = new runs at constructor time (too early, before @Mock fields got created)
-  ProductService service = new ProductService(
-      supplierRepoMock,
-      productRepoMock,
-      safetyApiClientMock,
-      null,
-      kafkaTemplateMock);
+  ProductService service;
+
+  @BeforeEach
+  final void setup() { // now works as it runs later
+    service = new ProductService(
+        supplierRepoMock,
+        productRepoMock,
+        safetyApiClientMock,
+        null,
+        kafkaTemplateMock);
+  }
 
   @Test
   void createProduct() {
