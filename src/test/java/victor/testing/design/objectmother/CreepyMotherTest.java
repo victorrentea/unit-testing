eba591239ac9891f113e58b334b5a8fe42760156
Victor Rentea <victorrentea@gmail.com>
2024-07-10 14:13:42 +0300

    WRONG

--- src/test/java/victor/testing/design/objectmother/CreepyMotherTest.java
+++ src/test/java/victor/testing/design/objectmother/CreepyMotherTest.java
@@ -1,17 +1,22 @@
 package victor.testing.design.objectmother;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ShippingServiceTest {
+  @Test
   void generatesInvoiceWithValidCustomerAndOrder() {
-    Customer customer = Customer.builder()
-        .name("John Doe")
-        .billingAddress("123 Main St")
-        .shippingAddress("123 Main St")
-        .phoneNumber("WHY?!!!")
-        .build();
+//    Customer customer = Customer.builder()
+//        .name("John Doe")
+//        .billingAddress("123 Main St")
+//        .shippingAddress("123 Main St")
+//        .phoneNumber("WHY?!!!")
+//        .build();
+    Customer customer = Mockito.mock(Customer.class);
+    Mockito.when(customer.getName()).thenReturn("John Doe");
+    Mockito.when(customer.getBillingAddress()).thenReturn("123 Main St");
     String order = "Order123";
 
     InvoiceService invoiceService = new InvoiceService();
