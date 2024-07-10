cb6a3230d81499b080ddbc747fe7c03347fdcea2
Victor Rentea <victorrentea@gmail.com>
2024-07-10 14:17:02 +0300

    test#1 done.

--- src/test/java/victor/testing/design/objectmother/CreepyMotherTest.java
+++ src/test/java/victor/testing/design/objectmother/CreepyMotherTest.java
@@ -2,28 +2,30 @@ package victor.testing.design.objectmother;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import victor.testing.design.objectmother.Customer.CustomerBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ShippingServiceTest {
+  // WARNING: if you have setters on your class, DO NOT CREATE A BUILDER FOR IT. instead make setters fluent (returning this)
   @Test
   void generatesInvoiceWithValidCustomerAndOrder() {
-//    Customer customer = Customer.builder()
-//        .name("John Doe")
-//        .billingAddress("123 Main St")
-//        .shippingAddress("123 Main St")
-//        .phoneNumber("WHY?!!!")
-//        .build();
-    Customer customer = Mockito.mock(Customer.class);
-    Mockito.when(customer.getName()).thenReturn("John Doe");
-    Mockito.when(customer.getBillingAddress()).thenReturn("123 Main St");
+    Customer customer = validCustomer().build();
     String order = "Order123";
-
     InvoiceService invoiceService = new InvoiceService();
+
     String invoice = invoiceService.generateInvoice(customer, order);
 
     assertThat(invoice).contains("Buyer name: John Doe");
     assertThat(invoice).contains("Address: 123 Main St");
     assertThat(invoice).contains("For order Order123");
 }
+
+  private CustomerBuilder validCustomer() { // canned object
+    return Customer.builder()
+        .name("John Doe")
+        .billingAddress("123 Main St")
+        .shippingAddress("anything") // useless details that make the test harder to understand
+        .phoneNumber("WHY?!!!");
+  }
 }
