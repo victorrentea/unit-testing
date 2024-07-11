41d5290b326b2a6fe0a51d5c0b0d5f84aa672810
Victor Rentea <victorrentea@gmail.com>
2024-07-11 11:54:59 +0300

    @Sql to run scripts

--- src/test/java/victor/testing/spring/BaseIntegrationTest.java
+++ src/test/java/victor/testing/spring/BaseIntegrationTest.java
@@ -5,12 +5,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import victor.testing.spring.domain.Supplier;
 import victor.testing.spring.repo.ProductRepo;
 import victor.testing.spring.repo.SupplierRepo;
 
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
 @SpringBootTest
 @ActiveProfiles("test")
+// #2 for SQL-intensive projects, w/o JPA
+@Sql(scripts = "classpath:/sql/cleanup.sql",
+    executionPhase = BEFORE_TEST_METHOD)
 public abstract class BaseIntegrationTest {
   public static final String SUPPLIER_CODE = "S";
   @Autowired
@@ -18,16 +24,16 @@ public abstract class BaseIntegrationTest {
   @Autowired
   protected SupplierRepo supplierRepo;
 
-  @BeforeEach
-  final void insertRefData() {
-    productRepo.deleteAll();
-    supplierRepo.deleteAll();// now correct
-    supplierRepo.save(new Supplier().setCode(SUPPLIER_CODE)); // INSERT of data which is SELECTED by the testeed code
-  }
-
-  @AfterEach // clean before AND after to prevent leaking data to test classes running after you
-  final void cleanup() {
-    productRepo.deleteAll();
-    supplierRepo.deleteAll();// now correct
-  }
+//  @BeforeEach// #1 if using JPA = perfeect
+//  final void insertRefData() {
+//    productRepo.deleteAll();
+//    supplierRepo.deleteAll();// now correct
+//    supplierRepo.save(new Supplier().setCode(SUPPLIER_CODE)); // INSERT of data which is SELECTED by the testeed code
+//  }
+//
+//  @AfterEach // clean before AND after to prevent leaking data to test classes running after you
+//  final void cleanup() {
+//    productRepo.deleteAll();
+//    supplierRepo.deleteAll();// now correct
+//  }
 }
--- src/test/resources/sql/cleanup.sql
+++ src/test/resources/sql/cleanup.sql
@@ -4,3 +4,5 @@ DELETE FROM SUPPLIER;
 
 -- sequences are not rolledback by @Transcational
 ALTER SEQUENCE hibernate_sequence RESTART WITH 1;
+
+INSERT INTO SUPPLIER (id, name, code, active) VALUES (1, 'Supplier Name', 'S', 1);
