1e445db3fb3fdb3db1dcae97d27bc38a2b429b64
Victor Rentea <victorrentea@gmail.com>
2024-07-18 14:49:31 +0200

    UnnecessaryStubbingException: is a good exception because it keeps our subbings to a minimum. > avoid Mockito.mock, and avoid @MockitoSettings

--- src/test/java/victor/testing/spring/service/CreateProductTest.java
+++ src/test/java/victor/testing/spring/service/CreateProductTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.kafka.core.KafkaTemplate;
 import victor.testing.spring.infra.SafetyApiAdapter;
 import victor.testing.spring.repo.ProductRepo;
@@ -15,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+//@MockitoSettings(strictness = Strictness.LENIENT) // DO NOT USE THIS!±!
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
   public static final String BARCODE = "BARCODE";
@@ -22,8 +25,8 @@ class ProductServiceTest {
   SupplierRepo supplierRepo;
   @Mock
   ProductRepo productRepo;
-//  @Mock
-  SafetyApiAdapter safetyApiAdapter = mock(SafetyApiAdapter.class);
+  @Mock// always use @
+  SafetyApiAdapter safetyApiAdapter;// = mock(SafetyApiAdapter.class);
   @Mock
   KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
   @InjectMocks
