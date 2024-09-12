a94743269170f6eb1c357ef9314dff32f3a8c7c6
Victor Rentea <victorrentea@gmail.com>
2024-09-12 10:24:02 +0300

    wip

--- src/test/java/victor/testing/spring/service/ProductServiceTest.java
+++ src/test/java/victor/testing/spring/service/ProductServiceTest.java
@@ -4,13 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import victor.testing.spring.entity.Supplier;
 import victor.testing.spring.infra.SafetyApiAdapter;
+import victor.testing.spring.repo.SupplierRepo;
 import victor.testing.spring.rest.dto.ProductDto;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class) // asta interpreteaza @ din clasa
@@ -18,17 +21,33 @@ class ProductServiceTest {
   public static final String BARCODE = "#barcode#";
   @Mock
   SafetyApiAdapter apiAdapter;
+  @Mock
+  SupplierRepo supplierRepo;
   @InjectMocks
   ProductService target;
 
   @Test
-  void createProduct() {
+//  void whenProductIsNotSafe_createShouldThrowIllegalStateException() {
+//  void create_whenProductIsNotSafe_shouldThrowIllegalStateException() {
+  void createThrows_forUnsafeProduct() {
     ProductDto dto = new ProductDto();
     dto.setBarcode(BARCODE);
     when(apiAdapter.isSafe(BARCODE)).thenReturn(false);
-//    when(apiAdapter.isSafe(any())).thenReturn(false);
 
     assertThrows(IllegalStateException.class, () ->
         target.createProduct(dto));
+  }
+
+  @Test
+  void createProduct() {
+//    Supplier supplier = new Supplier(); // BINE
+    Supplier supplier = mock(Supplier.class); // RAU
+    ProductDto dto = new ProductDto();
+    dto.setBarcode(BARCODE);
+    when(apiAdapter.isSafe(BARCODE)).thenReturn(true);
+    when(supplierRepo.findByCode("#supplierCode#"))
+        .thenReturn(Optional.of(supplier));
+
+    target.createProduct(dto);
   }
 }
