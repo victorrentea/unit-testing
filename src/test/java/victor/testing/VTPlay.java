518e91bde88fee23f0f4f2358cf3ab8bd9329466
Victor Rentea <victorrentea@gmail.com>
2024-09-12 08:11:02 +0300

    wip

---
+++ src/test/java/victor/testing/VTPlay.java
@@ -0,0 +1,25 @@
+package victor.testing;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Executors;
+
+//public class VTPlay {
+//
+//  private VTPlay vtPlay = new VTPlay();
+//
+//  @Test
+//  void experiment() {
+//    Executors.newVirtualThreadExecutor().submit(() -> vtPlay.method());
+//    vtPlay.method();
+//  }
+//
+//  @SneakyThrows
+//  public void method() {
+//    synchronized (this) {
+//      System.out.println("Hello");
+//      Thread.sleep(1000);
+//    }
+//  }
+//}
