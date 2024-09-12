e52057ba9b50b483a22548e5b6b1635c4c843c92
Victor Rentea <victorrentea@gmail.com>
2024-09-12 09:18:47 +0300

    testul crapa acum pt ca stubbingul (when-then) de la :29 nu a fost uytilziat de codul testat. adica mincte cititorul!

--- src/test/java/victor/testing/spring/service/ProductServiceTest.java
+++ src/test/java/victor/testing/spring/service/ProductServiceTest.java
@@ -1,21 +1,28 @@
 package victor.testing.spring.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import victor.testing.spring.infra.SafetyApiAdapter;
 import victor.testing.spring.rest.dto.ProductDto;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class) // asta interpreteaza @ din clasa
 class ProductServiceTest {
-  SafetyApiAdapter apiAdapter = Mockito.mock(SafetyApiAdapter.class);
-  ProductService target = new ProductService(
+  @Mock
+  SafetyApiAdapter apiAdapter;// = Mockito.mock(SafetyApiAdapter.class);
+  @InjectMocks
+  ProductService target;/* = new ProductService(
       null,
       null,
       apiAdapter,
       null,
-      null);
+      null);*/
 
   @Test
   void createProduct() {
