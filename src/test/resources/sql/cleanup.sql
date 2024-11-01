5c0b00dc51a08ebd5e3c3709ad468f3c8908748e
Victor Rentea <victorrentea@gmail.com>
2024-11-01 12:19:22 +0200

    🍒

--- src/test/resources/sql/cleanup.sql
+++ src/test/resources/sql/cleanup.sql
@@ -3,4 +3,5 @@ DELETE FROM PRODUCT;
 DELETE FROM SUPPLIER;
 
 -- sequences are not rolledback by @Transcational
-ALTER SEQUENCE hibernate_sequence RESTART WITH 1;
+ALTER SEQUENCE product_seq RESTART WITH 1;
+ALTER SEQUENCE supplier_seq RESTART WITH 1;
