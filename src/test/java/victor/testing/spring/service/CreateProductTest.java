f614fbff23aea9e7cd1e71f800ed1ca8f125774f
Victor Rentea <victorrentea@gmail.com>
2024-11-01 15:19:09 +0200

    slice tests

--- src/test/java/victor/testing/spring/service/CreateProductTest.java
+++ src/test/java/victor/testing/spring/service/CreateProductTest.java
@@ -22,6 +22,7 @@ import org.springframework.test.annotation.DirtiesContext.MethodMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import victor.testing.spring.entity.Product;
 import victor.testing.spring.entity.Supplier;
 import victor.testing.spring.export.ProductExporter;
