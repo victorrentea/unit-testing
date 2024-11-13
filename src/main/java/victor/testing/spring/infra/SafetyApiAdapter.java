6cb5efd06ebda40d01c8d1407e504059ac2ad69b
Victor Rentea <victorrentea@gmail.com>
2024-11-13 10:41:55 +0200

    modalitati de a face REST API calluri

--- src/main/java/victor/testing/spring/infra/SafetyApiAdapter.java
+++ src/main/java/victor/testing/spring/infra/SafetyApiAdapter.java
@@ -3,8 +3,13 @@ package victor.testing.spring.infra;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
+import victor.testing.spring.infra.SafetyApiAdapter.SafetyResponse;
 
 import java.net.URL;
 
@@ -22,10 +27,32 @@ public class SafetyApiAdapter {
   }
 
   public boolean isSafe(String barcode) {
+    // foarte ok de folosit rest template
     SafetyResponse response = restTemplate.getForEntity(
             baseUrl + "/product/{barcode}/safety",
             SafetyResponse.class, barcode)
         .getBody();
+
+    // replacement pt RestTemplate (nu merita sa migrezi)
+//    var response = webClient // welcome to Reactive HELL☠️
+
+//    var response = RestClient.create() // nu-ti crea tu de mana ci foloseste o instanta injectata
+
+//    var response = restClient
+//        .get()
+//        .uri(baseUrl + "/product/{barcode}/safety", barcode)
+//        .retrieve()
+//        .body(SafetyResponse.class);
+
+//    var response = client.fetchSafety(barcode);
+
     return "SAFE".equals(response.category());
   }
+//  private final ProductApiClient client;
 }
+
+//@FeignClient("product-client")
+//interface ProductApiClient { // must-have pentru un eco de microservicii
+//  @GetMapping("/product/{barcode}/safety")
+//  SafetyResponse fetchSafety(@PathVariable String barcode);
+//}
