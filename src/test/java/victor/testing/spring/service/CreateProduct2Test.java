3606fe7e812f7d7e3f4eb242fb293635f3eb9d4e
Victor Rentea <victorrentea@gmail.com>
2024-11-13 12:20:20 +0200

    spring context cache

--- src/test/java/victor/testing/spring/service/CreateProduct2Test.java
+++ src/test/java/victor/testing/spring/service/CreateProduct2Test.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import victor.testing.spring.entity.Product;
 import victor.testing.spring.entity.Supplier;
@@ -24,39 +25,23 @@ import static org.mockito.Mockito.when;
 import static victor.testing.spring.entity.ProductCategory.HOME;
 import static victor.testing.spring.entity.ProductCategory.UNCATEGORIZED;
 
-//@TestPropertySource(properties = "spring.datasource.url=jdbc:h2:mem:test")
-@ActiveProfiles("test")// sa incarc application.test.properties
-@EmbeddedKafka // buteaza o kafka in memorie ~ H2
-@SpringBootTest // porneste springu in procesul JUNit
+//ce e comentat face contextul asta de test diferit=>spring porneste din nou +10-20 sec
 
-
-// #2 pt schema legacy (multe tabele)
-//@Sql(scripts = "/sql/cleanup.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
-
-// #3 auto-rollback: pt app noi cu JPA
-@Transactional // ruleaza fiecare @Test intr-o tranzactie care este ROLLBACKED dupa fiecare test automat, ca sa lase baza curatar
-
-//@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+/*@TestPropertySource(properties = "spring.datasource.url=jdbc:p6spy:h2:mem:test2")*/
+@ActiveProfiles({"test"/*,"doi"*/})
+@Transactional
 public class CreateProduct2Test {
   @Autowired
   SupplierRepo supplierRepo;
   @Autowired
   ProductRepo productRepo;
   @MockBean
+  /*@Autowired*/
   SafetyApiAdapter safetyApiAdapter;
-  @MockBean // inlocuieste in spring context instanta reala cu un mock mockito
+  @MockBean
   KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
   @Autowired
   ProductService productService;
-
-//  @BeforeEach // #1 programatic inainte si dupa test
-//  @AfterEach
-//  final void setup() {
-//    productRepo.deleteAll(); // cu grije in ordinea FKurilor
-//    supplierRepo.deleteAll();
-////    cacheManager.getCache()..clear()
-//    //wireMOck.reset();
-//  }
 
   @Test
   void createThrowsForUnsafeProduct() {
@@ -68,36 +53,4 @@ public class CreateProduct2Test {
         .hasMessage("Product is not safe!");
   }
 
-  @Test
-  void createOk() {
-    supplierRepo.save(new Supplier().setCode("S"));
-    when(safetyApiAdapter.isSafe("barcode-safe")).thenReturn(true);
-    ProductDto productDto = new ProductDto("name", "barcode-safe", "S", HOME);
-
-    // WHEN
-    var id = productService.createProduct(productDto);
-
-    Product product = productRepo.findById(id).orElseThrow();
-    assertThat(product.getName()).isEqualTo("name");
-    assertThat(product.getBarcode()).isEqualTo("barcode-safe");
-    assertThat(product.getSupplier().getCode()).isEqualTo("S");
-    assertThat(product.getCategory()).isEqualTo(HOME);
-    verify(kafkaTemplate).send(
-        eq(ProductService.PRODUCT_CREATED_TOPIC),
-        eq("k"),
-        argThat(e -> e.productId().equals(id)));
-  }
-  @Test
-  void defaultsToUncategorizedForWithMissingCategory() {
-    supplierRepo.save(new Supplier().setCode("S"));
-    when(safetyApiAdapter.isSafe("barcode-safe")).thenReturn(true);
-    ProductDto productDto = new ProductDto(
-        "name", "barcode-safe", "S", null);
-
-    // WHEN
-    var id = productService.createProduct(productDto);
-
-    Product product = productRepo.findById(id).orElseThrow();
-    assertThat(product.getCategory()).isEqualTo(UNCATEGORIZED);
-  }
 }
