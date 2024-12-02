5ac7e7f6bbbc602de43d3765926a80f77baf3fef
Victor Rentea <victorrentea@gmail.com>
2024-12-02 14:36:17 +0200

    Revert "Revert "polish""
    This reverts commit 3589ab175e0698cd742d947b8e12b61b580bf526.

--- src/test/java/victor/testing/design/time/TimeExtension.java
+++ src/test/java/victor/testing/design/time/TimeExtension.java
@@ -3,7 +3,6 @@ package victor.testing.design.time;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.InvocationInterceptor;
 import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
-import org.mockito.MockedStatic;
 
 import java.lang.reflect.Method;
 import java.time.LocalDate;
@@ -11,14 +10,17 @@ import java.time.LocalDate;
 import static org.mockito.Mockito.CALLS_REAL_METHODS;
 import static org.mockito.Mockito.mockStatic;
 
-// TO use this, add to your test class:
-// @RegisterExtension TimeExtension timeExtension = new TimeExtension("2019-09-29");
+/**
+ * Example usage: add this as a field of your test class
+ * {@code @RegisterExtension TimeExtension timeExtension = new TimeExtension("2019-09-29");}
+ */
 public class TimeExtension implements InvocationInterceptor {
-	private LocalDate fixedDate;
+  private LocalDate fixedDate;
 
   public TimeExtension(LocalDate fixedDate) {
     this.fixedDate = fixedDate;
   }
+
   public TimeExtension(String fixedDateIsoStr) {
     this.fixedDate = LocalDate.parse(fixedDateIsoStr);
   }
@@ -28,13 +30,11 @@ public class TimeExtension implements InvocationInterceptor {
   }
 
   @Override
-	public void interceptTestMethod(Invocation<Void> invocation, ReflectiveInvocationContext<Method> invocationContext, ExtensionContext extensionContext) throws Throwable {
-		try (MockedStatic<LocalDate> mock = mockStatic(LocalDate.class,
-          CALLS_REAL_METHODS)) { // all other methods will be real
-			mock.when(LocalDate::now)
-          .thenAnswer(call -> fixedDate); // = uses a -> to allow changes of date
+  public void interceptTestMethod(Invocation<Void> invocation, ReflectiveInvocationContext<Method> invocationContext, ExtensionContext extensionContext) throws Throwable {
+    try (var mocked = mockStatic(LocalDate.class, CALLS_REAL_METHODS)) { // other methods=untouched
+      mocked.when(LocalDate::now).thenAnswer(call -> fixedDate); // -> allows change of date during @Test
 
-			invocation.proceed();
-		}
-	}
+      invocation.proceed();
+    }
+  }
 }
