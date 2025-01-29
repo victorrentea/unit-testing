ac6348c1be2f69f2b33a378ff874e342d9f80ca4
Victor Rentea <victorrentea@gmail.com>
2025-01-29 15:33:40 +0200

    solutii pentru a curata SQLul

--- src/test/java/victor/testing/spring/service/CreateProductTest.java
+++ src/test/java/victor/testing/spring/service/CreateProductTest.java
@@ -1,6 +1,9 @@
 package victor.testing.spring.service;
 
 import org.assertj.core.api.SoftAssertions;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -15,7 +18,10 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import victor.testing.spring.IntegrationTest;
 import victor.testing.spring.entity.Product;
@@ -34,14 +40,17 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 import static victor.testing.spring.entity.ProductCategory.HOME;
 import static victor.testing.spring.entity.ProductCategory.UNCATEGORIZED;
 
-@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)//cea mai proasta idee dar e Vineri
+//@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)//cea mai proasta idee dar e Vineri
+// o idee proasta pt ca adauga +10..60 secunde / @Test #4 nu o face
 @ActiveProfiles("test")
 @EmbeddedKafka
 @SpringBootTest // porneste app spring in memoria JUnit
-//@Sql(value = "classpath:/sql/common-reference-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+//@Sql(value = "classpath:/sql/cleanup.sql", executionPhase = BEFORE_TEST_METHOD) #3
+@Transactional // daca pui in teste, face rollback la sfarsitul fiecarui test 💖 #1
 public class CreateProductTest /*extends IntegrationTest*/ {
   @Autowired
   SupplierRepo supplierRepo;
@@ -53,6 +62,13 @@ public class CreateProductTest /*extends IntegrationTest*/ {
   KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
   @Autowired
   ProductService productService;
+
+//  @BeforeEach // solutie foarte buna #2
+//  @AfterEach
+//  final void cleanup() {
+//      productRepo.deleteAll();
+//      supplierRepo.deleteAll();
+//  }
 
   @Test
   void createThrowsForUnsafeProduct() {
