ad63d7816e499ede8a2741e972843560c6a45050
Victor Rentea <victorrentea@gmail.com>
2025-05-12 16:46:24 +0300

    moved from file-based to programatic DSL configuration

--- src/test/java/victor/testing/spring/service/CreateProductTest.java
+++ src/test/java/victor/testing/spring/service/CreateProductTest.java
@@ -9,6 +9,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
@@ -21,6 +22,7 @@ import victor.testing.spring.rest.dto.ProductDto;
 
 import java.util.Optional;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentCaptor.forClass;
@@ -32,6 +34,7 @@ import static victor.testing.spring.entity.ProductCategory.HOME;
 @EmbeddedKafka
 @ActiveProfiles("test")
 @SpringBootTest
+@AutoConfigureWireMock(port = 0)
 public class CreateProductTest {
   @MockBean
   SupplierRepo supplierRepo;
@@ -46,7 +49,6 @@ public class CreateProductTest {
 
   @Test
   void createThrowsForUnsafeProduct() {
-//    when(safetyApiAdapter.isSafe("barcode-unsafe")).thenReturn(false);
     ProductDto productDto = new ProductDto("name", "barcode-unsafe", "S", HOME);
 
     assertThatThrownBy(() -> productService.createProduct(productDto))
@@ -56,8 +58,18 @@ public class CreateProductTest {
 
   @Test
   void createOk() {
+    stubFor(get(urlEqualTo("/product/barcode-safe/safety"))
+        .willReturn(aResponse()
+            .withStatus(200)
+            .withHeader("Content-Type", "application/json")
+            .withBody("""
+            {
+              "category": "SAFE",
+              "detailsUrl": "http://details.url/a/b"
+            }
+        """)));
+
     when(supplierRepo.findByCode("S")).thenReturn(Optional.of(new Supplier().setCode("S")));
-//    when(safetyApiAdapter.isSafe("barcode-safe")).thenReturn(true);
     when(productRepo.save(any())).thenReturn(new Product().setId(123L));
     ProductDto productDto = new ProductDto("name", "barcode-safe", "S", HOME);
 
