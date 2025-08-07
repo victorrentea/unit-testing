e98d680b87a08b1d776d548e3d5a6ff6106eb3d6
Victor Rentea <victorrentea@gmail.com>
2025-08-07 14:25:21 +0300

    param test methoud source static by default

---
+++ src/test/java/victor/testing/spring/repo/ProductRepoSearchPTest.java
@@ -0,0 +1,75 @@
+package victor.testing.spring.repo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import victor.testing.spring.IntegrationTest;
+import victor.testing.spring.entity.Product;
+import victor.testing.spring.entity.ProductCategory;
+import victor.testing.spring.entity.Supplier;
+import victor.testing.spring.rest.dto.ProductSearchCriteria;
+import victor.testing.spring.rest.dto.ProductSearchResult;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static victor.testing.spring.entity.ProductCategory.ELECTRONICS;
+import static victor.testing.spring.entity.ProductCategory.HOME;
+
+@Transactional
+class ProductRepoSearchPTest extends IntegrationTest {
+  @Autowired
+  ProductRepo productRepo;
+  @Autowired
+  SupplierRepo supplierRepo;
+
+  ProductSearchCriteria searchCriteria = new ProductSearchCriteria();
+
+  Product product;
+  Supplier supplier;
+
+  @BeforeEach
+  final void setup() {
+    supplier = supplierRepo.save(new Supplier());
+    product = productRepo.save(new Product()
+        .setName("NAme")
+        .setSupplier(supplier)
+        .setCategory(HOME));
+  }
+
+  public static Stream<Arguments> data() {
+    return Stream.of(
+        Arguments.arguments(new ProductSearchCriteria(), true),
+        Arguments.arguments(new ProductSearchCriteria().setName("aM"), true),
+        Arguments.arguments(new ProductSearchCriteria().setName("Ankit"), false),
+        Arguments.arguments(new ProductSearchCriteria().setCategory(HOME), true),
+        Arguments.arguments(new ProductSearchCriteria().setCategory(ELECTRONICS), false),
+//        Arguments.arguments(new ProductSearchCriteria().setSupplierId(supplier.getId()), true),
+        Arguments.arguments(new ProductSearchCriteria().setSupplierId(-1L), false)
+
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("data")
+  void oneTestToRuleThemAll(ProductSearchCriteria searchCriteria, boolean matches) {
+    List<ProductSearchResult> searchResults = productRepo.search(searchCriteria);
+
+    if (matches) {
+      assertThat(searchResults).hasSize(1)
+          .first()
+          .returns("NAme", ProductSearchResult::name)
+          .returns(product.getId(), ProductSearchResult::id);
+    } else {
+      assertThat(searchResults).isEmpty();
+    }
+  }
+
+
+}
+
