448e1ad09bafc612301d72aa6b4fdcbe104b08fd
Victor Rentea <victorrentea@gmail.com>
2025-09-17 15:34:42 +0300

    back to best practice

--- src/test/java/victor/testing/spring/service/ProductServiceCreateTest.java
+++ src/test/java/victor/testing/spring/service/ProductServiceCreateTest.java
@@ -1,5 +1,7 @@
 package victor.testing.spring.service;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -12,8 +14,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
 import victor.testing.spring.entity.Product;
 import victor.testing.spring.entity.Supplier;
 import victor.testing.spring.infra.SafetyApiAdapter;
@@ -29,12 +34,23 @@ import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
+import static org.springframework.test.annotation.DirtiesContext.MethodMode.AFTER_METHOD;
 import static victor.testing.spring.entity.ProductCategory.HOME;
 import static victor.testing.spring.entity.ProductCategory.UNCATEGORIZED;
 
 @SpringBootTest // pornesc app in-mem
 @ActiveProfiles("test")
 @EmbeddedKafka // porneste un emulator de K in-mem
+//@Sql(scripts = "classpath:/sql/cleanup.sql") // #2 pt scheme mari
+//@Transactional // in /src/main face commit la finalul metodei
+// da' cand il pui in /src/test face la final @Testului ROLLBACK #3
+
+// Springu tre sa moara dupa fiecare @Test = furi viata de la colegi
+// cat= count(@Test)*10..50s
+//@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD) // #RAU
+// merge pt ca springu cand moare moare si H2 din memorie
+// Dar daca intri in randul lumii si testezi cu DB in DOcker🐳, atunci nu mai moare DB cand moare Spring
 public class ProductServiceCreateTest {
   @Autowired // = @Mock+@Bean; adica inlocuieste beanul normal cu un mock configurabil
   SupplierRepo supplierRepo;
@@ -52,6 +68,16 @@ public class ProductServiceCreateTest {
       .supplierCode("S")
       .category(HOME)
       .build();
+
+  @BeforeEach
+  @AfterEach
+  final void before() { // #1 STERG inainte sa ma asez pe colac, da si dupa, sa-l las cum l-am gasit
+    productRepo.deleteAll(); // cu grija in ordinea FKurilor
+    supplierRepo.deleteAll();
+    // cache.clear
+    // singleton cu state.clear
+    // kafka.drain topic pt 1 sec
+  }
 
   @Test
   void createThrowsForUnsafeProduct() {
@@ -80,7 +106,7 @@ public class ProductServiceCreateTest {
     verify(kafkaTemplate).send(
         eq(ProductService.PRODUCT_CREATED_TOPIC),
         eq("k"),
-        assertArg(e-> assertThat(e.productId()).isEqualTo(newProductId)));
+        assertArg(e -> assertThat(e.productId()).isEqualTo(newProductId)));
     assertThat(product.getCreatedDate()).isToday(); // TODO can only integration-test as it requires Hibernate magic
   }
 
