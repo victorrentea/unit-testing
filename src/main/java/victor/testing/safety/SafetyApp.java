ca047b6608786d03448d99d7c803b077008fb690
Victor Rentea <victorrentea@gmail.com>
2025-10-15 09:37:03 +0300

    am scos ce nu aveati nevoie

--- src/main/java/victor/testing/safety/SafetyApp.java
+++ src/main/java/victor/testing/safety/SafetyApp.java
@@ -1,5 +1,6 @@
 package victor.testing.safety;
 
+import lombok.EqualsAndHashCode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EqualsAndHashCode
 public class SafetyApp {
    public static void main(String[] args) {
       SpringApplication.run(SafetyApp.class, "--server.port=8090");
