e97b892d925181b42122d2a092fa8cfc57fecf17
Victor Rentea <victorrentea@gmail.com>
2025-11-18 17:19:01 +0200

    Mockuri programatic

--- src/test/java/victor/testing/mocks/telemetry/DiagnosticTest.java
+++ src/test/java/victor/testing/mocks/telemetry/DiagnosticTest.java
@@ -1,28 +1,42 @@
 package victor.testing.mocks.telemetry;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class DiagnosticTest {
 
+  Diagnostic diagnostic = new Diagnostic();
+  Client clientMock = Mockito.mock(Client.class);
+  @BeforeEach
+  final void before() {
+    diagnostic.setTelemetryClient(clientMock);
+    when(clientMock.getVersion()).thenReturn("ver");
+  }
+
   @Test
   void checkTransmission() {
-    Diagnostic diagnostic = new Diagnostic();
-    // nu vrei sa incluzi in testul tau o clasa
-    // complexa sau care cere o DB/API extern
-    Client clientMock = Mockito.mock(Client.class); // o instanta surogat din clasa Client
-    // - pe care o pot invata ce sa returneze: when..then
-    // - intreba ce metode i s-au chemat: verify..
-    diagnostic.setTelemetryClient(clientMock);
-//    client.setOnlineStatus(true);// corect daca era @Entity sau DTO = ob cu date
     when(clientMock.getOnlineStatus()).thenReturn(true);
-    when(clientMock.getVersion()).thenReturn("ver");
 
     diagnostic.checkTransmission(true);
+
+    verify(clientMock).disconnect(true);
+  }
+
+  @Test
+  void checkTransmission_throwsWhenNotOnline() {
+    when(clientMock.getOnlineStatus()).thenReturn(false);
+
+    assertThatThrownBy(() -> // AssertJ
+        diagnostic.checkTransmission(true))
+          .isInstanceOf(IllegalStateException.class)
+          .hasMessage("Unable to connect.");
 
     //assert+verify
     verify(clientMock).disconnect(true);
